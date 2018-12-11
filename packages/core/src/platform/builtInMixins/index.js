6cdc2aa337d99904fe25af9f647f4f310ffb854c
pkingwa <pkinglvwa@163.com>
2018-12-11 14:10:28 +0800

    use Component create Page

--- packages/core/src/platform/builtInMixins/index.js
+++ packages/core/src/platform/builtInMixins/index.js
@@ -1,6 +1,5 @@
 import pageStatusMixin from './pageStatusMixin'
 import proxyEventMixin from './proxyEventMixin'
-import componentMixin from './componentMixin'
 import renderHelperMixin from './renderHelperMixin'
 import injectHelperMixin from './injectHelperMixin'
 
@@ -8,7 +7,6 @@ export default function getBuiltInMixins (type, options) {
   return [
     pageStatusMixin(type, options),
     proxyEventMixin(type, options),
-    componentMixin(type, options),
     renderHelperMixin(),
     injectHelperMixin()
   ].filter(item => item)
