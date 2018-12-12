af865d5b8e2ac1992834477e57884afa8f8cc428
hiyuki <674883329@qq.com>
2018-12-12 20:00:18 +0800

    components map ignore query

--- packages/webpack-plugin/lib/utils/strip-extention.js
+++ packages/webpack-plugin/lib/utils/strip-extention.js
@@ -5,14 +5,12 @@ function stripExtension (request) {
   if (typeof request !== 'string' || request === '') return request
   if (!seen[request]) {
     let queryIndex = request.indexOf('?')
-    let query = ''
     let resource = request
     if (queryIndex > -1) {
-      query = request.slice(queryIndex)
       resource = request.slice(0, queryIndex)
     }
     let parsed = path.parse(resource)
-    seen[request] = path.posix.join(parsed.dir, parsed.name) + query
+    seen[request] = path.posix.join(parsed.dir, parsed.name)
   }
   return seen[request]
 }
