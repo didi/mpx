ee3d58e76067d8f2f3b614e617454e6ae88d4b17
hiyuki <674883329@qq.com>
2018-12-14 20:18:04 +0800

    rm useless

--- packages/webpack-plugin/lib/template/WxsTemplatePlugin.js
+++ packages/webpack-plugin/lib/template/WxsTemplatePlugin.js
@@ -1,10 +1,6 @@
 const WxsMainTemplatePlugin = require('./WxsMainTemplatePlugin')
 
 class WxsTemplatePlugin {
-  constructor (options) {
-    options = options || {}
-  }
-
   apply (compiler) {
     compiler.hooks.thisCompilation.tap('WxsTemplatePlugin', compilation => {
       new WxsMainTemplatePlugin().apply(
