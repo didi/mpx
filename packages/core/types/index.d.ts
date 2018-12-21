98de7b01688d6516736da8f49b7d683472c97842
sky-admin <546485299@qq.com>
2018-12-21 19:08:43 +0800

    add types file

---
+++ packages/core/types/index.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for @mpxjs/core
+// Project: https://didi.github.io/mpx/api.html
+// Definitions by: skyadmin <https://github.com/sky-admin>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.8
+
+export function createStore (): any;
+
+declare class mpx {
+  createStore: Function
+}
+
+export default mpx
