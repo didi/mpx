3ee451b88c5b823b73f928ef77ec8fe8ea88b1e6
pkingwa <pkinglvwa@163.com>
2019-01-25 13:14:50 +0800

    修改createStore逻辑

--- packages/core/src/core/createStore.js
+++ packages/core/src/core/createStore.js
@@ -17,13 +17,9 @@ function transformGetters (getters, module, store) {
     if (key in store.getters) {
       console.warn(new Error(`duplicate getter type: ${key}`))
     }
-    if (typeof getters[key] === 'function') {
-      defineGetter(newGetters, key, function () {
-        return getters[key](module.state, store.getters, store.state)
-      })
-    } else {
-      newGetters[key] = getters[key]
-    }
+    defineGetter(newGetters, key, function () {
+      return getters[key](module.state, store.getters, store.state)
+    })
   }
   return newGetters
 }
@@ -34,9 +30,9 @@ function transformMutations (mutations, module, store) {
     if (store.mutations[key]) {
       console.warn(new Error(`duplicate mutation type: ${key}`))
     }
-    newMutations[key] = typeof mutations[key] === 'function' ? action(function (...payload) {
+    newMutations[key] = action(function (...payload) {
       return mutations[key](module.state, ...payload)
-    }) : mutations[key]
+    })
   }
   return newMutations
 }
@@ -47,7 +43,7 @@ function transformActions (actions, module, store) {
     if (store.actions[key]) {
       console.warn(new Error(`duplicate action type: ${key}`))
     }
-    newActions[key] = typeof actions[key] === 'function' ? function (...payload) {
+    newActions[key] = function (...payload) {
       const result = actions[key]({
         rootState: store.state,
         state: module.state,
@@ -61,33 +57,29 @@ function transformActions (actions, module, store) {
       } else {
         return Promise.resolve(result)
       }
-    } : actions[key]
+    }
   }
   return newActions
 }
 
-function mergeDeps (options) {
-  const stores = options.deps
-  if (!stores) return options
+function mergeDeps (module, deps, getterKeys) {
   const mergeProps = ['state', 'getters', 'mutations', 'actions']
-  Object.keys(stores).forEach(key => {
-    const store = stores[key]
+  Object.keys(deps).forEach(key => {
+    const store = deps[key]
     mergeProps.forEach(prop => {
-      if (options[prop] && (key in options[prop])) {
+      if (module[prop] && (key in module[prop])) {
         console.warn(new Error(`deps's name: [${key}] conflicts with ${prop}'s key in current options`))
       } else {
-        options[prop] = options[prop] || {}
-        options[prop][key] = store[prop]
+        module[prop] = module[prop] || {}
+        module[prop][key] = store[prop]
+        prop === 'getters' && getterKeys.push(key)
       }
     })
   })
-  delete options.deps
-  return options
 }
 
 class Store {
   constructor (options) {
-    options = mergeDeps(options)
     this.getters = {}
     this.mutations = {}
     this.actions = {}
@@ -114,23 +106,31 @@ class Store {
   }
 
   registerModule (module) {
-    const reactiveModuleOption = {
-      state: module.state || {}
+    const getterKeys = []
+    const reactiveModule = {
+      state: observable(module.state || {})
     }
-    const reactiveModule = observable(reactiveModuleOption)
     if (module.getters) {
-      extendObservable(reactiveModule, {
-        getters: transformGetters(module.getters, reactiveModule, this)
-      })
-      // 使用proxy，保证store.getters的属性是可观察的
-      proxy(this.getters, reactiveModule.getters, Object.keys(module.getters), true)
+      // mobx计算属性是不可枚举的，所以单独收集
+      getterKeys.push.apply(getterKeys, Object.keys(module.getters))
+      reactiveModule.getters = observable(transformGetters(module.getters, reactiveModule, this))
     }
     if (module.mutations) {
-      Object.assign(this.mutations, transformMutations(module.mutations, reactiveModule, this))
+      reactiveModule.mutations = transformMutations(module.mutations, reactiveModule, this)
     }
     if (module.actions) {
-      Object.assign(this.actions, transformActions(module.actions, reactiveModule, this))
+      reactiveModule.actions = transformActions(module.actions, reactiveModule, this)
     }
+    if (module.deps) {
+      mergeDeps(reactiveModule, module.deps, getterKeys)
+    }
+    // merge getters, 不能用Object.assign，会导致直接执行一次getter函数
+    reactiveModule.getters && proxy(this.getters, reactiveModule.getters, getterKeys, true)
+    // merge mutations
+    Object.assign(this.mutations, reactiveModule.mutations)
+    // merge actions
+    Object.assign(this.actions, reactiveModule.actions)
+    // 子module
     if (module.modules) {
       const childs = module.modules
       Object.keys(childs).forEach(key => {
