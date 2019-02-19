be91f76bb31e3110c3c3e49e0a91e2dc7c4159f9
sky-admin <546485299@qq.com>
2019-02-19 20:04:49 +0800

    add update-notifier for cli tool

--- packages/cli/lib/check-version.js
+++ packages/cli/lib/check-version.js
@@ -6,7 +6,7 @@ module.exports = done => {
   // Ensure minimum supported node version is used
   if (!semver.satisfies(process.version, packageConfig.engines.node)) {
     return console.log(chalk.red(
-      '  You must upgrade node to >=' + packageConfig.engines.node + '.x to use @mpx/cli'
+      '  You must upgrade node to >=' + packageConfig.engines.node + '.x to use @mpxjs/cli'
     ))
   }
   done()
