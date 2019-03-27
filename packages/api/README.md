08f6fe596c25cd7767ddb51ebda6b3c32c44a1a5
httpsxiao <day_clear@163.com>
2019-03-27 17:11:01 +0800

    extract api

---
+++ packages/api/README.md
@@ -0,0 +1,14 @@
+# `mpx-api`
+
+> convert miniprogram API at each end
+
+## Usage
+
+```js
+import mpx from '@mpxjs/core'
+import api from '@mpxjs/api'
+
+mpx.use(api)
+```
+
+**如果同时使用 `@mpxjs/promisify`，请在其前面引入 `@mpxjs/api`**
--- packages/promisify/src/index.js
+++ packages/promisify/src/index.js
@@ -1,7 +1,5 @@
 import getPromisifyList from './promisify'
-import { proxyApi } from './platform'
 
 export default function install (proxyMPX, whiteList) {
-  proxyApi()
   Object.assign(proxyMPX, getPromisifyList(whiteList))
 }
