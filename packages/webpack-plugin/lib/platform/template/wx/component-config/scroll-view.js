d7235220b818c71571eea89fe9c83f1dcfc936ea
xuehebinglan <785529534@qq.com>
2019-04-28 19:42:33 +0800

    scroll-view baidu

--- packages/webpack-plugin/lib/platform/template/wx/component-config/scroll-view.js
+++ packages/webpack-plugin/lib/platform/template/wx/component-config/scroll-view.js
@@ -5,9 +5,16 @@ module.exports = function ({ print }) {
    * @type {function(isError: (number|boolean|string)?): void} aliLog
    * @desc - 无法转换时告知用户的通用方法，接受0个或1个参数，意为是否error级别
    */
+  const baiduLog = print('baidu', TAG_NAME)
 
   return {
     test: TAG_NAME,
+    props: [
+      {
+        test: /^(enable-back-to-top)$/,
+        swan: baiduLog()
+      }
+    ],
     event: [
       {
         test: /^(scrolltoupper|scrolltolower|scroll)$/,
