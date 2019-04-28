e1a3a04868253941f4d56478acc618f3bac50409
xuehebinglan <785529534@qq.com>
2019-04-28 19:03:09 +0800

    canvas ali

--- packages/webpack-plugin/lib/platform/template/wx/component-config/canvas.js
+++ packages/webpack-plugin/lib/platform/template/wx/component-config/canvas.js
@@ -17,16 +17,25 @@ module.exports = function ({ print }) {
             value
           }
         }
-      },
-      {
-        test: /^(disable-scroll)$/,
-        ali: aliLog()
       }
     ],
     // 组件事件中的差异部分
     // 微信中基础事件有touchstart|touchmove|touchcancel|touchend|tap|longpress|longtap|transitionend|animationstart|animationiteration|animationend|touchforcechange
     // 支付宝中的基础事件有touchStart|touchMove|touchEnd|touchCancel|tap|longTap
     event: [
+      {
+        test: /^(touchstart|touchmove|touchend|touchcancel|longtap)$/,
+        ali (eventName) {
+          const eventMap = {
+            'touchstart': 'touchStart',
+            'touchmove': 'touchMove',
+            'touchend': 'touchEnd',
+            'touchcancel': 'touchCancel',
+            'longtap': 'longTap',
+          }
+          return eventMap[eventName]
+        }
+      },
       {
         test: /^(error)$/,
         ali: aliLog()
