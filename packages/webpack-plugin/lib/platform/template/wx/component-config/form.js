cfcec6b943784de0f82978cdf9b5e14684d8e557
xuehebinglan <785529534@qq.com>
2019-04-29 15:11:03 +0800

    form.js baidu

--- packages/webpack-plugin/lib/platform/template/wx/component-config/form.js
+++ packages/webpack-plugin/lib/platform/template/wx/component-config/form.js
@@ -6,12 +6,15 @@ module.exports = function ({ print }) {
    * @desc - 无法转换时告知用户的通用方法，接受0个或1个参数，意为是否error级别
    */
   const aliLog = print('ali', TAG_NAME)
+  const baiduLog = print('baidu', TAG_NAME)
+
   return {
     test: TAG_NAME,
     props: [
       {
         test: /^(report-submit-timeout)$/,
-        ali: aliLog()
+        ali: aliLog(),
+        swan: baiduLog()
       }
     ],
     event: [
