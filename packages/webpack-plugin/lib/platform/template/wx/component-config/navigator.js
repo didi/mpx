04ff16364c4b2e5d580401426537675ccfb8bd60
xuehebinglan <785529534@qq.com>
2019-04-29 20:29:36 +0800

    navigator.js for baidu

--- packages/webpack-plugin/lib/platform/template/wx/component-config/navigator.js
+++ packages/webpack-plugin/lib/platform/template/wx/component-config/navigator.js
@@ -10,7 +10,7 @@ module.exports = function ({ print }) {
     test: TAG_NAME,
     props: [
       {
-        test: /^(target|delta|app-id|path|extra-data|version)$/,
+        test: /^(target|delta|app-id|path|extra-data|version|hover-stop-propagation)$/,
         ali: aliPropLogError
       },
       {
