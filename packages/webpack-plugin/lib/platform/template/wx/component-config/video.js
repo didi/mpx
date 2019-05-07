62cde4e9da970dad36053b76d476ec3c6d178b80
xuehebinglan <785529534@qq.com>
2019-05-07 15:13:15 +0800

    <video> for qq

--- packages/webpack-plugin/lib/platform/template/wx/component-config/video.js
+++ packages/webpack-plugin/lib/platform/template/wx/component-config/video.js
@@ -3,6 +3,7 @@ const TAG_NAME = 'video'
 module.exports = function ({ print }) {
   const baiduPropLog = print({ platform: 'baidu', tag: TAG_NAME, isError: false })
   const baiduEventLogError = print({ platform: 'baidu', tag: TAG_NAME, isError: false, type: 'event' })
+  const qqPropLog = print({ platform: 'qq', tag: TAG_NAME, isError: false })
 
   return {
     test: TAG_NAME,
@@ -10,6 +11,20 @@ module.exports = function ({ print }) {
       {
         test: /^(direction|show-mute-btn|title|play-btn-position|enable-play-gesture|auto-pause-if-navigate|auto-pause-if-open-native|vslide-gesture|vslide-gesture-in-fullscreen)$/,
         swan: baiduPropLog
+      },
+      {
+        test: /^(vslide-gesture)$/,
+        qq (obj) {
+          const propsMap = {
+            'vslide-gesture': 'page-gesture'
+          }
+          obj.name = propsMap[obj.name]
+          return obj
+        }
+      },
+      {
+        test: /^(vslide-gesture-in-fullscreen)$/,
+        qq: qqPropLog
       }
     ],
     event: [
