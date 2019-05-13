396577150553814dad37f8e0fd2342094c049e17
xuehebinglan <785529534@qq.com>
2019-05-13 17:21:26 +0800

    unsupported 'show-menu-by-longpress' for <image> for ali && baidu && bytedance

--- packages/webpack-plugin/lib/platform/template/wx/component-config/image.js
+++ packages/webpack-plugin/lib/platform/template/wx/component-config/image.js
@@ -1,8 +1,20 @@
 const TAG_NAME = 'image'
 
 module.exports = function ({ print }) {
+  const aliPropLog = print({ platform: 'ali', tag: TAG_NAME, isError: false })
+  const baiduPropLog = print({ platform: 'baidu', tag: TAG_NAME, isError: false })
+  const ttPropLog = print({ platform: 'bytedance', tag: TAG_NAME, isError: false })
+
   return {
     test: TAG_NAME,
+    props: [
+      {
+        test: /^show-menu-by-longpress$/,
+        ali: aliPropLog,
+        swan: baiduPropLog,
+        tt: ttPropLog
+      }
+    ],
     event: [
       {
         test: /^(error|load)$/,
