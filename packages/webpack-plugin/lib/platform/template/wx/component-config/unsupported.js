a08c5aebe971e6cc35f73737182653c5c996ab00
xuehebinglan <785529534@qq.com>
2019-05-13 17:14:41 +0800

    unsupported components for bytedance

--- packages/webpack-plugin/lib/platform/template/wx/component-config/unsupported.js
+++ packages/webpack-plugin/lib/platform/template/wx/component-config/unsupported.js
@@ -5,7 +5,7 @@ const BAIDU_UNSUPPORTED_TAG_NAME_ARR = ['functional-page-navigator', 'live-pushe
 // QQ小程序不支持的标签集合
 const QQ_UNSUPPORTED_TAG_NAME_ARR = ['functional-page-navigator', 'live-player', 'live-pusher', 'map', 'official-account', 'editor']
 // 头条小程序不支持的标签集合
-const TT_UNSUPPORTED_TAG_NAME_ARR = ['movable-view', 'cover-image', 'cover-view', 'movable-area', 'open-data', 'official-account', 'ad', 'editor', 'functional-page-navigator']
+const TT_UNSUPPORTED_TAG_NAME_ARR = ['movable-view', 'cover-image', 'cover-view', 'movable-area', 'open-data', 'official-account', 'ad', 'editor', 'functional-page-navigator', 'audio', 'camera', 'live-player', 'live-pusher']
 
 /**
  * @param {function(object): function} print
