f85455f385173ff5035687875ad74278ce5adff9
xuehebinglan <785529534@qq.com>
2019-05-13 14:26:18 +0800

    unsupported of toutiao

--- packages/webpack-plugin/lib/platform/template/wx/component-config/unsupported.js
+++ packages/webpack-plugin/lib/platform/template/wx/component-config/unsupported.js
@@ -4,6 +4,8 @@ const ALI_UNSUPPORTED_AG_NAME_ARR = ['live-pusher', 'live-player', 'camera', 'vi
 const BAIDU_UNSUPPORTED_AG_NAME_ARR = ['functional-page-navigator', 'live-pusher']
 // QQ小程序不支持的标签集合
 const QQ_UNSUPPORTED_AG_NAME_ARR = ['functional-page-navigator', 'live-player', 'live-pusher', 'map', 'official-account']
+// 头条小程序不支持的标签集合
+const TOUTIAO_UNSUPPORTED_AG_NAME_ARR = ['movable-view', 'cover-image', 'cover-view', 'movable-area']
 
 /**
  * @param {function(object): function} print
@@ -13,10 +15,12 @@ module.exports = function ({ print }) {
   const aliUnsupportedTagError = print({ platform: 'ali', isError: true, type: 'tag' })
   const baiduUnsupportedTagError = print({ platform: 'baidu', isError: true, type: 'tag' })
   const qqUnsupportedTagError = print({ platform: 'qq', isError: true, type: 'tag' })
+  const ttUnsupportedTagError = print({ platform: 'toutiao', isError: true, type: 'tag' })
 
   const aliUnsupportedExp = new RegExp('^(' + ALI_UNSUPPORTED_AG_NAME_ARR.join('|') + ')$')
   const baiduUnsupportedExp = new RegExp('^(' + BAIDU_UNSUPPORTED_AG_NAME_ARR.join('|') + ')$')
   const qqUnsupportedExp = new RegExp('^(' + QQ_UNSUPPORTED_AG_NAME_ARR.join('|') + ')$')
+  const ttUnsupportedExp = new RegExp('^(' + TOUTIAO_UNSUPPORTED_AG_NAME_ARR.join('|') + ')$')
 
   return [
     {
@@ -33,6 +37,11 @@ module.exports = function ({ print }) {
       supportedTargets: ['qq'],
       test: qqUnsupportedExp,
       ali: qqUnsupportedTagError
+    },
+    {
+      supportedTargets: ['tt'],
+      test: ttUnsupportedExp,
+      ali: ttUnsupportedTagError
     }
   ]
 }
