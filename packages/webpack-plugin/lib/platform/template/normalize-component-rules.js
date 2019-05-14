3d04c5f7e2f2c52774f9bfe38bcc7198c588e5ed
hiyuki <674883329@qq.com>
2019-05-14 15:00:49 +0800

    add attrsMap

--- packages/webpack-plugin/lib/platform/template/normalize-component-rules.js
+++ packages/webpack-plugin/lib/platform/template/normalize-component-rules.js
@@ -9,7 +9,7 @@ module.exports = function normalizeComponentRules (cfgs, spec) {
     const supportedTargets = cfg.supportedTargets || spec.supportedTargets
     supportedTargets.forEach((target) => {
       result[target] = function (el) {
-        const rTag = cfg[target] && cfg[target].call(this, el.tag)
+        const rTag = cfg[target] && cfg[target].call(this, el.tag, { attrsMap: el.attrsMap })
         if (rTag) {
           el.tag = rTag
         }
