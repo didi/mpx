9e42006b33e3deab36cb66efbc28ecb5cdf0e511
sky-admin <546485299@qq.com>
2019-05-16 20:18:19 +0800

    remove a useless test case because inline wxs has been supported in ali env

--- packages/webpack-plugin/test/platform/wx/template.spec.js
+++ packages/webpack-plugin/test/platform/wx/template.spec.js
@@ -1,25 +1,6 @@
 const compiler = require('../../../lib/template-compiler/compiler')
 
 describe('template should transform correct', function () {
-  it('should report error when no src attr on wxs tag in ali env', function () {
-    const input = `<wxs>var a = 123</wxs>
-<view>123</view>
-    `
-
-    const errorFn = jest.fn()
-
-    const parsed = compiler.parse(input, {
-      usingComponents: [],
-      srcMode: 'wx',
-      mode: 'ali',
-      warn: console.log,
-      error: errorFn
-    })
-    const ast = parsed.root
-    compiler.serialize(ast)
-    expect(errorFn).toHaveBeenCalledWith(`<wxs> should have 'src' attr in ali environment!`)
-  })
-
   it('should transform normally in ali env', function () {
     const input = `<wxs module="m1" src="./test.wxs"></wxs>
 <view>123</view>
