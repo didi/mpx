e35d0a56fa1e9cc1d609ee6c4f2146f08cbc9d64
sky-admin <546485299@qq.com>
2019-05-24 13:53:29 +0800

    add rollup as build tools and make dist file

--- packages/webview-bridge/src/index.esm.js
+++ packages/webview-bridge/src/index.esm.js
@@ -242,5 +242,16 @@ const bridgeFunction = {
   wxsdkConfig
 }
 
+const { navigateTo, navigateBack, switchTab, reLaunch, redirectTo, getEnv, postMessage } = webviewApiList
+const { getLocation, chooseImage, openLocation, getNetworkType, previewImage } = exportApiList
+
 // 此处导出的对象包含所有的api
 export default bridgeFunction
+
+export {
+  // 此处导出的为3个平台均可使用的api
+  navigateTo, navigateBack, switchTab, reLaunch, redirectTo, getEnv, postMessage,
+  getLocation, chooseImage, openLocation, getNetworkType, previewImage,
+  // 微信特有的一个配置sdk的方法
+  wxsdkConfig
+}
