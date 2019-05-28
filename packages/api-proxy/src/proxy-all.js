39218ef4be05cc51acd5f1af010a2e562fa3922b
sky-admin <546485299@qq.com>
2019-05-28 09:17:58 +0800

    fix lint

--- packages/api-proxy/src/proxy-all.js
+++ packages/api-proxy/src/proxy-all.js
@@ -4,7 +4,7 @@ import promisify from './promisify'
 function proxyAll (target, usePromise, whiteList) {
   const envObj = getEnvObj()
   const list = promisify(envObj, usePromise, whiteList)
-  
+
   Object.keys(list).forEach(key => {
     try {
       target[key] = list[key]
