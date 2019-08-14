79969123cfc648f18cbfc3e24961a6b001500ae4
sky-admin <546485299@qq.com>
2019-08-14 19:55:35 +0800

    add example about cloud function

---
+++ examples/mpx-cloud/README.md
@@ -0,0 +1,16 @@
+# mpx-cloud
+
+> A mpx project
+
+## Dev
+
+```bash
+# install dep
+npm i
+
+# for dev
+npm run watch
+
+# for online
+npm run product
+```
---
+++ examples/mpx-cloud/build/build.js
@@ -0,0 +1,134 @@
+const ora = require('ora')
+const rm = require('rimraf')
+const path = require('path')
+const chalk = require('chalk')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const program = require('commander')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MpxWebpackPlugin = require('@mpxjs/webpack-plugin')
+const mpxWebpackPluginConfig = require('./mpx.webpack.conf')
+
+let webpackMainConfig = require('./webpack.conf')
+
+var prodEnv = require('../config/prod.env')
+var devEnv = require('../config/dev.env')
+
+const mainSubDir = 'miniprogram'
+function resolveDist (file, subPathStr = mainSubDir) {
+  return path.resolve(__dirname, '../dist', subPathStr, file || '')
+}
+
+const webpackConfigArr = []
+const userSelectedMode = 'wx'
+
+// 微信小程序需要拷贝project.config.json，如果npm script参数里有--wx，拷贝到/dist下，如果指定--wx，拷贝到/dist/wx下
+const webpackWxConfig = merge(webpackMainConfig, {
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, '../project.config.json'),
+        to: path.resolve(__dirname, '../dist/project.config.json')
+      },
+      {
+        from: path.resolve(__dirname, '../functions'),
+        to: path.resolve(__dirname, '../dist/functions')
+      }
+])
+  ]
+})
+
+webpackConfigArr.push(merge(webpackWxConfig, {
+  output: {
+    path: resolveDist()
+  },
+  plugins: [
+    new MpxWebpackPlugin(Object.assign({mode: userSelectedMode}, mpxWebpackPluginConfig))
+  ]
+}))
+
+program
+  .option('-w, --watch', 'watch mode')
+  .option('-p, --production', 'production release')
+  .parse(process.argv)
+
+function runWebpack (cfg) {
+  // env
+  if (Array.isArray(cfg)) {
+    cfg.forEach(item => item.plugins.unshift(new webpack.DefinePlugin(program.production ? prodEnv : devEnv)))
+  } else {
+    cfg.plugins.unshift(new webpack.DefinePlugin(program.production ? prodEnv : devEnv))
+  }
+
+  if (program.production || program.watch) {
+    const extendCfg = program.production ? { mode: 'production' } : { cache: true }
+    if (Array.isArray(cfg)) {
+      cfg = cfg.map(item => merge(item, extendCfg))
+    } else {
+      cfg = merge(cfg, extendCfg)
+    }
+  }
+  if (process.env.npm_config_report) {
+    var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+    var mainCfg = Array.isArray(cfg) ? cfg[0] : cfg
+    mainCfg.plugins.push(new BundleAnalyzerPlugin())
+  }
+  if (program.watch) {
+    webpack(cfg).watch({}, callback)
+  } else {
+    webpack(cfg, callback)
+  }
+}
+
+function callback (err, stats) {
+  spinner.stop()
+  if (err) return console.error(err)
+  if (Array.isArray(stats.stats)) {
+    stats.stats.forEach(item => {
+      console.log(item.compilation.name + '打包结果：')
+      process.stdout.write(item.toString({
+        colors: true,
+        modules: false,
+        children: false,
+        chunks: false,
+        chunkModules: false,
+        entrypoints: false
+      }) + '\n\n')
+    })
+  } else {
+    process.stdout.write(stats.toString({
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false,
+      entrypoints: false
+    }) + '\n\n')
+  }
+
+  if (!program.watch && stats.hasErrors()) {
+    console.log(chalk.red('  Build failed with errors.\n'))
+    process.exit(1)
+  }
+
+  console.log(chalk.cyan('  Build complete.\n'))
+  if (program.watch) {
+    console.log(chalk.cyan(`  ${new Date()} build finished.\n  Still watching...\n`))
+  }
+}
+
+var spinner = ora('building...')
+spinner.start()
+
+try {
+  rm.sync(path.resolve(__dirname, `../dist/*`))
+} catch (e) {
+  console.error(e)
+  console.log('\n\n删除dist文件夹遇到了一些问题，如果遇到问题请手工删除dist重来\n\n')
+}
+
+if (webpackConfigArr.length === 1) {
+  runWebpack(webpackConfigArr[0])
+} else {
+  runWebpack(webpackConfigArr)
+}
