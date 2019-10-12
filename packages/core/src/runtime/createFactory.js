09f840035950a150a88de4d9dfbd06e98b7ba7fb
httpsxiao <day_clear@163.com>
2019-10-12 14:54:14 +0800

    update createFactory

--- packages/core/src/runtime/createFactory.js
+++ packages/core/src/runtime/createFactory.js
@@ -2,8 +2,15 @@ const mpx = require('../index')
 
 module.exports = (type) => (...args) => {
   if (type === 'Behavior') {
-    // eslint-disable-next-line
-    args[0].__mpx_behaviors_to_mixins__ = true
+    if (args[0]) {
+      Object.defineProperty(args[0], '__mpx_behaviors_to_mixins__', {
+        configurable: true,
+        enumerable: false,
+        get () {
+          return true
+        }
+      })
+    }
     return args[0]
   }
   return mpx[`create${type}`] && mpx[`create${type}`].apply(null, args.concat({ isNative: true }))
