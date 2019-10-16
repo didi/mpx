7e30b4904699045f36bef81851b6ada825aca3ca
hiyuki <674883329@qq.com>
2019-10-16 20:57:11 +0800

    temp

--- packages/webpack-plugin/lib/platform/template/wx/component-config/text.js
+++ packages/webpack-plugin/lib/platform/template/wx/component-config/text.js
@@ -2,13 +2,30 @@ const TAG_NAME = 'text'
 
 module.exports = function ({ print }) {
   const baiduPropLog = print({ platform: 'baidu', tag: TAG_NAME, isError: false })
+  const webPropLog = print({ platform: 'web', tag: TAG_NAME, isError: false })
 
   return {
     test: TAG_NAME,
+    web (tag, { transWebMode, el }) {
+      if (transWebMode === 'simple') {
+        return 'span'
+      } else {
+        el.isBuiltIn = true
+        return 'mpx-text'
+      }
+    },
     props: [
       {
         test: /^(decode)$/,
         swan: baiduPropLog
+      },
+      {
+        test: /^(selectable|space|decode)$/,
+        web (prop, { transWebMode }) {
+          if (transWebMode === 'simple') {
+            webPropLog(prop)
+          }
+        }
       }
     ]
   }
