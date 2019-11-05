1b3f650b65372d17fa7f63d770962de4e427d14e
httpsxiao <day_clear@163.com>
2019-11-05 19:45:50 +0800

    add test

---
+++ packages/api-h5/test/global.js
@@ -0,0 +1,5 @@
+import install from '../src/index.js'
+const mpx = {}
+install(mpx)
+
+export default mpx
---
+++ packages/api-h5/test/storage.spec.js
@@ -0,0 +1,115 @@
+import mpx from './global'
+
+const storageKey = 'storage key'
+const storageValue = 'storage value'
+const storageSyncKey = 'storage sync key'
+const storageSyncValue = {
+  'test key': 'storage sync value'
+}
+
+describe('test storage', () => {
+  beforeAll(() => {
+    window.localStorage.clear()
+  })
+
+  test('setStorageSync & getStorageSync', () => {
+    mpx.setStorageSync(storageKey, storageValue)
+    const getValue = mpx.getStorageSync(storageKey)
+    expect(getValue).toBe(storageValue)
+  })
+
+  test('setStorage', () => {
+    const success = jest.fn()
+    const fail = jest.fn()
+    const complete = jest.fn()
+
+    mpx.setStorage({
+      key: storageSyncKey,
+      data: storageSyncValue,
+      success,
+      fail,
+      complete
+    })
+
+    expect(success.mock.calls.length).toBe(1)
+    expect(fail.mock.calls.length).toBe(0)
+    expect(complete.mock.calls.length).toBe(1)
+  })
+
+  test('getStorage', () => {
+    const success = jest.fn()
+    const fail = jest.fn()
+    const complete = jest.fn()
+
+    mpx.getStorage({
+      key: storageSyncKey,
+      success,
+      fail,
+      complete
+    })
+
+    expect(success.mock.calls.length).toBe(1)
+    expect(success.mock.calls[0][0]['data']).toEqual(storageSyncValue)
+    expect(fail.mock.calls.length).toBe(0)
+    expect(complete.mock.calls.length).toBe(1)
+  })
+
+  test('getStorageInfo', () => {
+    const success = jest.fn()
+    const fail = jest.fn()
+    const complete = jest.fn()
+
+    mpx.getStorageInfo({
+      success,
+      fail,
+      complete
+    })
+
+    expect(success.mock.calls.length).toBe(1)
+    expect(success.mock.calls[0][0]['keys']).toEqual([storageKey, storageSyncKey])
+    expect(fail.mock.calls.length).toBe(0)
+    expect(complete.mock.calls.length).toBe(1)
+  })
+
+  test('removeStorage', () => {
+    const success = jest.fn()
+    const fail = jest.fn()
+    const complete = jest.fn()
+
+    mpx.removeStorage({
+      key: storageKey,
+      success,
+      fail,
+      complete
+    })
+
+    expect(success.mock.calls.length).toBe(1)
+    expect(fail.mock.calls.length).toBe(0)
+    expect(complete.mock.calls.length).toBe(1)
+    expect(mpx.getStorageInfoSync()).toEqual({ keys: [storageSyncKey], limitSize: null, currentSize: null })
+  })
+
+  test('clearStorage', () => {
+    const success = jest.fn()
+    const fail = jest.fn()
+    const complete = jest.fn()
+
+    mpx.clearStorage({
+      success,
+      fail,
+      complete
+    })
+
+    expect(success.mock.calls.length).toBe(1)
+    expect(fail.mock.calls.length).toBe(0)
+    expect(complete.mock.calls.length).toBe(1)
+    expect(Object.keys(window.localStorage)).toEqual([])
+  })
+
+  test('clearStorageSync', () => {
+    mpx.setStorageSync(storageKey, storageValue)
+    expect(Object.keys(window.localStorage)).toEqual([storageKey])
+    mpx.clearStorageSync()
+    expect(Object.keys(window.localStorage)).toEqual([])
+  })
+})
