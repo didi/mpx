4be88979c684b77951c22b93d5d8e8000f827adf
wangxiaokou <w-cuijuan@163.com>
2019-11-13 17:36:40 +0800

    add export getLoadError

--- packages/webview-bridge/src/index.esm.js
+++ packages/webview-bridge/src/index.esm.js
@@ -1,6 +1,6 @@
 import { bridgeFunction, webviewApiList } from './common'
 
-const { navigateTo, navigateBack, switchTab, reLaunch, redirectTo, getEnv, postMessage } = webviewApiList
+const { navigateTo, navigateBack, switchTab, reLaunch, redirectTo, getEnv, postMessage, getLoadError } = webviewApiList
 const { getAdvancedApi } = bridgeFunction
 
 // 此处导出的对象包含所有的api
@@ -8,7 +8,7 @@ export default bridgeFunction
 
 export {
   // 此处导出的为3个平台均可使用的小程序api
-  navigateTo, navigateBack, switchTab, reLaunch, redirectTo, getEnv, postMessage,
+  navigateTo, navigateBack, switchTab, reLaunch, redirectTo, getEnv, postMessage, getLoadError,
   // 使用native的能力，微信需要配置一个config，其他平台不需要
   getAdvancedApi
 }
