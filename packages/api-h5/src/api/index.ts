a082e967905c476997b3c52b73ef4cd8c1c9916e
httpsxiao <day_clear@163.com>
2019-11-14 14:58:18 +0800

    update

--- packages/api-h5/src/api/index.ts
+++ packages/api-h5/src/api/index.ts
@@ -1,10 +1,33 @@
 /// <reference types="miniprogram-api-typings" />
 
+// base64ToArrayBuffer, arrayBufferToBase64
+export * from './base'
+
+// createSelectorQuery
 export * from './create-selector-query'
+
+// showActionSheet
 export * from './action-sheet'
+
+// showToast, hideToast, showLoading, hideLoading
 export * from './toast'
+
+// showModal
 export * from './modal'
+
+// setStorage, setStorageSync, getStorage, getStorageSync
+// getStorageInfo, getStorageInfoSync, removeStorage, removeStorageSync
+// clearStorage, clearStorageSync
 export * from './storage'
+
+// getSystemInfo, getSystemInfoSync
 export * from './system'
+
+// request
 export * from './request'
+
+// redirectTo, navigateTo, navigateBack, reLaunch, switchTab
 export * from './route'
+
+// connectSocket
+export * from './socket'
--- packages/api-h5/src/index.ts
+++ packages/api-h5/src/index.ts
@@ -7,7 +7,7 @@ export default function install (target: any) {
   window.EventChannel = new EventChannel()
 
   Object.keys(allApi).forEach(api => {
-    target[api] = function (...args: any[]) {
+    target[api] = function (...args) {
       return allApi[api].apply(this, args)
     }
   })
