962b675856be5815862890ce732dec8c2150c519
httpsxiao <day_clear@163.com>
2019-12-04 20:07:07 +0800

    remove src_mode

--- packages/api-proxy/src/index.web.js
+++ packages/api-proxy/src/index.web.js
@@ -1,11 +1,19 @@
 import * as allApi from './web/api'
 import { EventChannel } from './web/api/event-channel'
+import { genFromMap } from './common/js'
 
 export default function install (target) {
+  const fromMap = genFromMap()
+
   window.EventChannel = new EventChannel()
 
   Object.keys(allApi).forEach(api => {
     target[api] = function (...args) {
+      const from = args.pop()
+      if (typeof from !== 'string' || !fromMap[from]) {
+        args.push(from)
+      }
+
       return allApi[api].apply(this, args)
     }
   })
