577c9851f79ddbaa09361c9e57e7bb1e3e57017a
hiyuki <674883329@qq.com>
2019-12-10 20:59:58 +0800

    support navigator

--- packages/webpack-plugin/lib/platform/template/wx/component-config/navigator.js
+++ packages/webpack-plugin/lib/platform/template/wx/component-config/navigator.js
@@ -7,9 +7,17 @@ module.exports = function ({ print }) {
   const aliEventLog = print({ platform: 'ali', tag: TAG_NAME, isError: false, type: 'event' })
   const ttPropLog = print({ platform: 'bytedance', tag: TAG_NAME, isError: false })
   const ttEventLog = print({ platform: 'bytedance', tag: TAG_NAME, isError: false, type: 'event' })
+  const webPropLog = print({ platform: 'web', tag: TAG_NAME, isError: false })
+  const webEventLog = print({ platform: 'web', tag: TAG_NAME, isError: false })
+  const webValueLogError = print({ platform: 'web', tag: TAG_NAME, isError: true, type: 'value' })
+
 
   return {
     test: TAG_NAME,
+    web (tag, { el }) {
+      el.isBuiltIn = true
+      return 'mpx-navigator'
+    },
     props: [
       {
         test: /^(target|delta|app-id|path|extra-data|version|hover-stop-propagation)$/,
@@ -22,6 +30,12 @@ module.exports = function ({ print }) {
           if (supportedList.indexOf(attr.value) === -1) {
             aliValueLogError(attr)
           }
+        },
+        web (attr) {
+          let supportedList = ['navigate', 'redirect', 'navigateBack', 'reLaunch']
+          if (supportedList.indexOf(attr.value) === -1) {
+            webValueLogError(attr)
+          }
         }
       },
       {
@@ -31,13 +45,18 @@ module.exports = function ({ print }) {
       {
         test: /^(target|app-id|path|extra-data|version)$/,
         tt: ttPropLog
+      },
+      {
+        test: /^(target|app-id|path|extra-data|version)$/,
+        web: webPropLog
       }
     ],
     event: [
       {
         test: /^(success|fail|complete)$/,
         ali: aliEventLog,
-        tt: ttEventLog
+        tt: ttEventLog,
+        web: webEventLog
       }
     ]
   }
