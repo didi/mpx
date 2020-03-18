819c24065b2f3a917fea3282fa1891d6322136da
hiyuki <674883329@qq.com>
2020-03-18 11:25:02 +0800

    adjust wxs pre loader

--- packages/webpack-plugin/lib/index.js
+++ packages/webpack-plugin/lib/index.js
@@ -584,11 +584,6 @@ class MpxWebpackPlugin {
             packageName
           })
           data.request = `!!${pathLoader}!${resource}`
-        } else if (queryObj.wxsModule) {
-          const wxsPreLoader = normalize.lib('wxs/wxs-pre-loader')
-          if (!/wxs-loader/.test(request)) {
-            data.request = `!!${wxsPreLoader}!${resource}`
-          }
         }
         callback(null, data)
       })
