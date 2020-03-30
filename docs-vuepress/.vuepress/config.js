a9f6dd9a3d1b38185bd967fdd64b908350aaddce
lj0812 <xoyljlj@163.com>
2020-03-30 11:43:53 +0800

    add dosc using vuepress

---
+++ docs-vuepress/.vuepress/config.js
@@ -0,0 +1,72 @@
+const sidebar = {
+  '/guide/': [
+    {
+      title: '基础',
+      collapsable: false,
+      children: [
+        'start',
+        'what-is-single-file',
+        'template-enhance',
+        'script-enhance',
+        'style-enhance',
+        'json-enhance'
+      ]
+    },
+    {
+      title: '进阶',
+      collapsable: false,
+      children: [
+        'store',
+        'compilationEnhance',
+        'platform',
+        'ts',
+        'i18n',
+        'progressive',
+        'understanding',
+        'resource'
+      ]
+    }
+  ],
+  '/api/': [
+    'intro',
+    'instance-api'
+  ],
+  '/extends/': [
+    'intro'
+  ],
+  '/articles/': [
+    {
+      title: '相关文章',
+      collapsable: false,
+      children: [
+        '1.0',
+        '2.0',
+        'mpx1'
+      ]
+    }
+  ],
+  '/version/': [
+    'migrate'
+  ]
+}
+
+const nav = [
+  { text: '指南', link: '/guide/' },
+  { text: 'API', link: '/api/intro' },
+  { text: '拓展', link: '/extends/intro' },
+  { text: '2.0迁移', link: '/version/' },
+  { text: '相关文章', link: '/articles/' }
+]
+
+module.exports = {
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+  // title: 'MPX',
+  themeConfig: {
+    logo: '/logo.png',
+    sidebar,
+    nav,
+    displayAllHeaders: true
+  }
+}
---
+++ docs-vuepress/README.md
@@ -0,0 +1,25 @@
+# 文档指南
+
+该文档使用vuepress生成
+
+## 该项目指南
+
+已安装依赖vuepress，并已经在package.json中新增写作和部署脚本
+
+``` sh
+# 写作时
+npm run docs:dev
+
+# 部署时
+npm run docs:build
+# 将在docs-vuepress/.vuepress/dist下生成静态文件，部署到相应服务器即可，部署参考下方链接
+```
+
+## TODO
+
+需更新 `docs-vuepress/index.md` 中 `footer: MIT Licensed` 一行（不一定是MIT协议）
+
+## 参考
+
+- [部署指南](https://www.vuepress.cn/guide/deploy.html)
+- [vuepress文档](https://www.vuepress.cn/)
