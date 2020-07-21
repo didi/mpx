fe1e0e22568c298bf37d893c85ea993043338928
wangzhanz <onlyprobie@gmail.com>
2020-07-21 13:22:34 +0800

    update

--- packages/api-proxy/src/web/api/create-selector-query/SelectQuery.js
+++ packages/api-proxy/src/web/api/create-selector-query/SelectQuery.js
@@ -104,9 +104,16 @@ class SelectQuery {
       res.node = el
       // 如果是canvas节点，需要做特殊处理
       if (isCanvas(el)) {
+        // 设置canvas的画布大小，避免直接使用style标签导致canvas的绘制出现拉伸效果
+        canvas.width = res.width
+        canvas.height = res.height
+        // 避免lint检查报错
+        /* eslint-disable */
         el.createImage = function () {
           return new Image()
         }
+        /* eslint-disable */
+
         el.requestAnimationFrame = function (callback) {
           return window.requestAnimationFrame(callback)
         }
