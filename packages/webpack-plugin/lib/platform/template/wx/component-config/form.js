00d1509f587493f71c00b2ae4b9ce7f4c0d89457
Light Leung <lsycxyj@gmail.com>
2020-08-04 23:06:32 +0800

    fx lint

--- packages/webpack-plugin/lib/platform/template/wx/component-config/form.js
+++ packages/webpack-plugin/lib/platform/template/wx/component-config/form.js
@@ -4,7 +4,7 @@ module.exports = function ({ print }) {
   const aliPropLog = print({ platform: 'ali', tag: TAG_NAME, isError: false })
   const baiduPropLog = print({ platform: 'baidu', tag: TAG_NAME, isError: false })
   const qqPropLog = print({ platform: 'qq', tag: TAG_NAME, isError: false })
-  const ttPropLog = print({ platform: 'bytedance', tag: TAG_NAME, isError: false })
+  // const ttPropLog = print({ platform: 'bytedance', tag: TAG_NAME, isError: false })
   const webPropLog = print({ platform: 'web', tag: TAG_NAME, isError: false })
 
   return {
