c1d4ee83fd1aaa57250ae6c1cb3575a1a648917b
Maoerbuxiong <anniechan_up@126.com>
2020-08-06 19:40:29 +0800

    Support api system

---
+++ packages/api-proxy/src/qa/system/index.js
@@ -0,0 +1,37 @@
+/** 
+*@file 兼容微信系统信息相关API
+*@export getSystemInfo、getSystemInfoSync
+**/
+
+// 此处引入先用webpack externals处理
+import app from 'qApp'
+import { successHandler, failHandler } from '../../common/js'
+
+function getSystemInfoSync(options = {}) {
+  let systemInfo
+  try{
+    systemInfo = app.getInfo()
+    const response = Object.assign({ msg: 'getSystemInfo:ok' }, systemInfo)
+    successHandler(response, options.success, options.complete)
+    return systemInfo
+  } catch (e) {
+    const error = Object.assign({ errMsg: 'getSystemInfo:error' }, e)
+    failHandler(error, options.fail, options.complete)
+  }
+}
+
+function getSystemInfo(options = {}) {
+  try {
+    const systemInfo = getSystemInfoSync(options)
+    return Promise.resolve(systemInfo)
+  } catch (e) {
+    return Promise.reject(e)
+  }
+}
+
+export {
+  getSystemInfoSync,
+  getSystemInfo
+}
+
+
