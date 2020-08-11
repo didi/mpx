3e01e5704f96948350be6bfc7e4b1d267c7cd658
wangxiaokou <w-cuijuan@163.com>
2020-08-11 15:59:45 +0800

    fix lint + 3

--- packages/webpack-plugin/lib/runtime/components/web/filterTag.js
+++ packages/webpack-plugin/lib/runtime/components/web/filterTag.js
@@ -1,11 +1,14 @@
+//eslint-disable-next-line
 const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/ // useless escape
 const ncname = `[a-zA-Z_][\\w\\-\\.]*`
 const qname = `((?:${ncname}\\:)?${ncname})`
 const startTagOpen = new RegExp(`^<${qname}`)
 const startTagClose = /^\s*(\/?)>/
 const endTag = new RegExp(`^<\\/${qname}[^>]*>`)
-const comment = /^<!\--/ // useless escape
-const invalidAttributeRE = /[\s"'<>\/=]/ // useless escape
+//eslint-disable-next-line
+const comment = /^<!\--/
+//eslint-disable-next-line
+const invalidAttributeRE = /[\s"'<>\/=]/
 let currentParent
 
 function makeMap (str, expectsLowerCase) {
