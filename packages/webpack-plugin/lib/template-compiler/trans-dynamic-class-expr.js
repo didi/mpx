a7e321c31a2671c597737995feb9160119ec8c75
hiyuki <674883329@qq.com>
2020-08-28 18:17:31 +0800

    a huge bugfix

--- packages/webpack-plugin/lib/template-compiler/trans-dynamic-class-expr.js
+++ packages/webpack-plugin/lib/template-compiler/trans-dynamic-class-expr.js
@@ -3,14 +3,20 @@ const t = require('babel-types')
 const generate = require('babel-generator').default
 const dash2hump = require('../utils/hump-dash').dash2hump
 
-export default function transDynamicClassExpr (expr) {
-  expr = babylon.parseExpression(expr)
+module.exports = function transDynamicClassExpr (expr) {
+  expr = babylon.parseExpression(expr, {
+    plugins: [
+      'objectRestSpread'
+    ]
+  })
   if (t.isObjectExpression(expr)) {
     expr.properties.forEach((property) => {
       if (t.isObjectProperty(property) && !property.computed) {
         const propertyName = property.key.name || property.key.value
         if (/-/.test(propertyName)) {
           property.key = t.identifier(dash2hump(propertyName) + 'MpxDash')
+        } else {
+          property.key = t.identifier(propertyName)
         }
       }
     })
