26ef6351453f2147583841cfd7a270dd49e3638c
sky-admin <546485299@qq.com>
2020-09-01 15:12:08 +0800

    fix spell

--- packages/webpack-plugin/lib/platform/template/wx/component-config/index.js
+++ packages/webpack-plugin/lib/platform/template/wx/component-config/index.js
@@ -31,8 +31,8 @@ const wxs = require('./wxs')
 const template = require('./template')
 const block = require('./block')
 const icon = require('./icon')
-const Nonsupport = require('./unsupported')
-const HyphenTagName = require('./hypen-tag-name')
+const unsupported = require('./unsupported')
+const hyphenTagName = require('./hypen-tag-name')
 
 module.exports = function getComponentConfigs ({ warn, error }) {
   /**
@@ -70,7 +70,7 @@ module.exports = function getComponentConfigs ({ warn, error }) {
 
   // 转换规则只需以微信为基准配置微信和支付宝的差异部分，比如微信和支付宝都支持但是写法不一致，或者微信支持而支付宝不支持的部分(抛出错误或警告)
   return [
-    ...Nonsupport({ print }),
+    ...unsupported({ print }),
     ad({ print }),
     view({ print }),
     scrollView({ print }),
@@ -104,6 +104,6 @@ module.exports = function getComponentConfigs ({ warn, error }) {
     template(),
     block(),
     icon(),
-    HyphenTagName({ print })
+    hyphenTagName({ print })
   ]
 }
