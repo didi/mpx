c5a977113ea85a0d9d84f0863d469d1016625d4c
Maoerbuxiong <anniechan_up@126.com>
2020-09-01 18:36:17 +0800

    Handle pages when user define options in mpx plugin config file

--- packages/webpack-plugin/lib/qaHelper/genRouter.js
+++ packages/webpack-plugin/lib/qaHelper/genRouter.js
@@ -8,7 +8,9 @@ module.exports = function genRouter(projectEntry, pagesMapArray, defineRouter) {
   let entryComp = projectEntry.slice(lastIndex + 1)
   let prefix = projectEntry.slice(0, lastIndex)
   let subpackages= `[`
-
+  let routers = `
+        "entry": "${prefix}",
+        "errorPage": "${defineRouter.errorPage || ''}",`
   /* 
   **** @todo需处理用户在 mpx.plugin.config.js 文件中自定义部分pages
   **** 用户自定义pages部分，用的是mpx项目中相对应组件的name和path,
@@ -22,26 +24,60 @@ module.exports = function genRouter(projectEntry, pagesMapArray, defineRouter) {
       }
     }
   }
-
-  let routers = `
-        "entry": "${prefix}",
-        "errorPage": "${defineRouter.errorPage || ''}",
+  
+  // 首页用户自定义pages规则时，合并规则
+  let index = needProcessPages.findIndex(item => {
+    return item.path.slice(1) === projectEntry
+  })
+  if (index >= 0) {
+    routers += `
+        "pages": {
+          "${prefix}": {
+            "component": "${entryComp}",
+            "path": "/${projectEntry}",
+            "filter": ${JSON.stringify(needProcessPages[index].filter) || {}},
+            "launchMode": "${needProcessPages[index].launchMode || 'standard'}"
+          }`
+  } else {
+    routers += `
         "pages": {
           "${prefix}": {
             "component": "${entryComp}"
           }`
+  }
+  
   for (let i = 0; i < pagesMapArray.length; i++) {
+    let isSubpackage = pagesMapArray[i].split('/')[0] !== 'pages'
+
+    // 处理pages
     if (pagesMapArray[i] !== projectEntry) {
       let lastIndex = pagesMapArray[i].lastIndexOf('/')
       let compName = pagesMapArray[i].slice(lastIndex + 1)
       let prefix = pagesMapArray[i].slice(0, lastIndex)
 
-      routers += `,
+      // 与用户自定义pages/options合并
+      let index = -1
+      if(!isSubpackage) {
+        index = needProcessPages.findIndex(item => {
+          return item.path.slice(1) === pagesMapArray[i]
+        })
+      }
+      if (index !== -1) {
+        routers +=`,
+        "${prefix}": {
+          "component": "${compName}",
+          "path": "/${compName}",
+          "filter": ${JSON.stringify(needProcessPages[index].filter) || {}},
+          "launchMode": "${needProcessPages[index].launchMode || 'standard'}"
+        }`
+      } else {
+        routers += `,
           "${prefix}": {
             "component": "${compName}"
           }`
+      }
     }
-    let isSubpackage = pagesMapArray[i].split('/')[0] !== 'pages'
+    // 处理分包
     if (isSubpackage) {
       let name = pagesMapArray[i].split('/')[0]
       if (subpackages !== `[`) {
