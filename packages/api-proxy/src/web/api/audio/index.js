fdfd00f770648bcfeecc7936761be5fbc409522c
jiangzongyu <jiangzongyu@didiglobal.com>
2020-09-07 22:12:26 +0800

    优化

--- packages/api-proxy/src/web/api/audio/index.js
+++ packages/api-proxy/src/web/api/audio/index.js
@@ -1,182 +1,64 @@
-/* eslint-disable no-undef */
-/* eslint-disable new-parens */
-const audio = new Audio
+export const createInnerAudioContext = () => {
+  // eslint-disable-next-line no-undef
+  const audio = new Audio()
+  const __audio = {}
 
-audio.stop = function () {
-  this.currentTime = 0
-  this.pause()
-}
+  __audio.play = () => !/mpxFalse/.test(audio.src) ? audio.play() : ''
 
-audio.seek = function (value) {
-  this.currentTime = value
-}
+  __audio.pause = () => audio.pause()
 
-audio.destroy = function () {
-  // audio = null //  Error in v-on handler: "Error: "audio" is read-only"
-  this.src = ''
-}
-
-let onCanplay = true
-audio.onCanplay = function (callback) {
-  this.addEventListener('play', function (event) {
-    if (event.type === 'play') {
-      this.addEventListener('canplay', function (e) {
-        if (onCanplay) {
-          callback(e)
-        }
-      })
-    }
-  }, { once: true })
-}
-
-audio.offCanplay = function (callback) {
-  onCanplay = false
-  if (callback) {
-    callback()
+  __audio.stop = () => {
+    __audio.pause()
+    __audio.seek(0)
   }
-}
 
-audio.onPlay = function (callback) {
-  this.addEventListener('play', function (e) {
-    callback(e)
-  })
-}
-
-let onPause = true
-audio.onPause = function (callback) {
-  this.addEventListener('pause', function (e) {
-    if (onPause) {
-      callback(e)
-    }
-  })
-}
-
-audio.offPause = function (callback) {
-  onPause = false
-  if (callback) {
-    callback()
+  __audio.seek = value => {
+    audio.currentTime = value
   }
-}
 
-let onStop = true
-audio.onStop = function (callback) {
-  this.addEventListener('pause', function (e) {
-    if (audio.currentTime === 0 && e.type === 'pause' && onStop) {
-      callback(e)
-    }
-  })
-}
-
-audio.offStop = function (callback) {
-  onStop = false
-  if (callback) {
-    callback()
+  __audio.destroy = () => {
+    audio.src = 'mpxFalse'
   }
-}
 
-let onEnded = true
-audio.onEnded = function (callback) {
-  this.addEventListener('timeupdate', function () {
-    // 监听播放完成，ended 事件不准确
-    if (audio.currentTime >= audio.duration) {
-      if (onEnded) {
-        callback()
+  const parameter = ['src', 'autoplay', 'loop', 'volume', 'duration', 'currentTime', 'buffered', 'paused']
+  parameter.forEach(item => {
+    Object.defineProperty(__audio, item, {
+      get: () => audio[item],
+      set (value) {
+        audio[item] = value
       }
-    }
+    })
   })
-}
-
-audio.offEnded = function (callback) {
-  onEnded = false
-  if (callback) {
-    callback()
-  }
-}
-
-let onTimeUpdate = true
-audio.onTimeUpdate = function (callback) {
-  audio.addEventListener('play', function (e) {
-    if (e.type === 'play') {
-      audio.addEventListener('timeupdate', () => {
-        if (onTimeUpdate) {
-          callback()
+  Object.defineProperty(__audio, 'startTime', {
+    value: 0
+  })
+  Object.defineProperty(__audio, 'obeyMuteSwitch', {
+    value: true
+  })
+  const simpleEvents = [
+    'Canplay',
+    'Ended',
+    'Pause',
+    'Play',
+    'Seeked',
+    'Seeking',
+    'TimeUpdate',
+    'Waiting',
+    'Stop',
+    'Error'
+  ]
+  const simpleListenerTuples = [
+    ['on', audio.addEventListener],
+    ['off', audio.removeEventListener]
+  ]
+  simpleEvents.forEach(eventName => {
+    simpleListenerTuples.forEach(([eventNamePrefix, listenerFunc]) => {
+      Object.defineProperty(__audio, `${eventNamePrefix}${eventName}`, {
+        get () {
+          return callback => listenerFunc.call(audio, eventName.toLowerCase(), callback)
         }
       })
-    }
-  }, { once: true })
-}
-
-audio.offTimeUpdate = function (callback) {
-  onTimeUpdate = false
-  if (callback) {
-    callback()
-  }
-}
-
-let onError = true
-audio.onError = function (callback) {
-  this.addEventListener('error', function (e) {
-    if (onError) {
-      callback(e)
-    }
+    })
   })
-}
-
-audio.offError = function (callback) {
-  onError = false
-  if (callback) {
-    callback()
-  }
-}
-
-let onWaiting = true
-audio.onWaiting = function (callback) {
-  this.addEventListener('waiting', function (e) {
-    if (onWaiting) {
-      callback(e)
-    }
-  })
-}
-
-audio.offWaiting = function (callback) {
-  onWaiting = false
-  if (callback) {
-    callback()
-  }
-}
-
-let onSeeking = true
-audio.onSeeking = function (callback) {
-  this.addEventListener('seeking', function (e) {
-    if (onSeeking) {
-      callback(e)
-    }
-  })
-}
-
-audio.offSeeking = function (callback) {
-  onSeeking = false
-  if (callback) {
-    callback()
-  }
-}
-
-let onSeeked = true
-audio.onSeeked = function (callback) {
-  this.addEventListener('seeked', function (e) {
-    if (onSeeked) {
-      callback(e)
-    }
-  })
-}
-
-audio.offSeeked = function (callback) {
-  onSeeked = false
-  if (callback) {
-    callback()
-  }
-}
-
-export function createInnerAudioContext () {
-  return audio
+  return __audio
 }
