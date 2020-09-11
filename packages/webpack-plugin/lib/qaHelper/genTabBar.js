28202b232840bc147ddb6b568bb237d5c54aad20
xiao19880917lu <xiao19880917lu@126.com>
2020-09-11 19:42:09 +0800

    tabBar not custom

--- packages/webpack-plugin/lib/qaHelper/genTabBar.js
+++ packages/webpack-plugin/lib/qaHelper/genTabBar.js
@@ -2,12 +2,29 @@ const fs = require('fs')
 const ConcatSource = require('webpack-sources').ConcatSource
 const parseComponent = require('../parser')
 
+const genName = (item) => {
+  return item.pagePath.replace(/[.\/]/g, '')
+}
+
+const genEntryTabContent = (tabBar) => {
+  let content = new ConcatSource()
+  tabBar.forEach((item) => {
+    item.key = genName(item)
+  })
+  content.add('<import name="tab-bar-main" src="./tabBar.ux"></import>')
+  content.add(`<template><div class="tabbar-wrapper"><tab-bar-main data='${JSON.stringify(tabBar)}'></tab-bar-main></div></template>`)
+  return content
+}
+
 module.exports = function (tabBar, compilation, options) {
   if (tabBar) {    
     let content = new ConcatSource()
     tabBar.list.forEach((item) => {
-      const name = item.pagePath.replace(/.\//g, '')
-      const targetPath = '../' + item.pagePath.replace(/^(.\/)/, '')
+      const name = genName(item)
+      let targetPath = item.pagePath.replace(/(.\/pages)/, '..')
+      if (targetPath.indexOf('.ux') === -1) {
+        targetPath += '.ux'
+      }
       content.add(`<import src="${targetPath}" name="${name}"></import>\n`)
     })
     const filePath = __dirname + '/tabBar.ux'
@@ -19,7 +36,9 @@ module.exports = function (tabBar, compilation, options) {
         content.add('<style>' + parts.styles[0].content + '</style>\n')
         content.add('<script>' + parts.script.content + '</script>\n')
 
-        compilation.assets['pages/tabBar/index' + '.ux'] = content
+        compilation.assets['pages/tabBar/tabBar' + '.ux'] = content
+        const entryTabContent = genEntryTabContent(tabBar.list)
+        compilation.assets['pages/tabBar/index' + '.ux'] = entryTabContent
 
       } catch (err) {
         console.log(err)
