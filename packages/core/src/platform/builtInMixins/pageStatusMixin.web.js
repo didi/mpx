aac78a73b1e1529c744d94dd6d856f65eceab095
anotherso1a <1181581742@qq.com>
2020-10-12 15:39:15 +0800

    feat(core/platform): supported show & hide pageLifeTimes

--- packages/core/src/platform/builtInMixins/pageStatusMixin.web.js
+++ packages/core/src/platform/builtInMixins/pageStatusMixin.web.js
@@ -1,3 +1,5 @@
+import { CREATED } from '../../core/innerLifecycle'
+
 export default function pageStatusMixin (mixinType) {
   if (mixinType === 'page') {
     return {
@@ -7,6 +9,27 @@ export default function pageStatusMixin (mixinType) {
       deactivated () {
         this.onHide && this.onHide()
       }
+    }
+  }
+  return {
+    [CREATED] () {
+      let vnode = window.__mpxRouter.__mpxActiveVnode
+      let pageInstance
+      if (vnode && vnode.componentInstance) {
+        pageInstance = vnode.tag.endsWith('mpx-tabbar-container') ? vnode.componentInstance.$children[1] : vnode.componentInstance
+      }
+      if(!pageInstance) return
+      this.$watch(
+        () => pageInstance.mpxPageStatus,
+        status => {
+          if (!status) return
+          const pageLifetimes = this.$rawOptions && this.$rawOptions.pageLifetimes || {}
+          // show & hide
+          if (status in pageLifetimes && typeof pageLifetimes[status] === 'function') {
+            pageLifetimes[status].call(this)
+          }
+        }
+      )
     }
   }
 }
