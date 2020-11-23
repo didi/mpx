cd700a3b76dcdd923d16492b572b9169c6d4cffd
Maoerbuxiong <anniechan_up@126.com>
2020-11-23 15:27:39 +0800

    Add more on storage

--- packages/api-proxy/src/qa/storage/index.js
+++ packages/api-proxy/src/qa/storage/index.js
@@ -1,6 +1,5 @@
 /**
 *@file 本地缓存相关API
-*@export 
 **/
 import storage from 'qStorage'
 import { successHandler, failHandler } from '../../common/js'
@@ -47,9 +46,96 @@ function removeStorage(options) {
   }).then(res => {
     successHandler(res, options.success, options.complete)
     resolve()
-  }).catch(err => {
-    failHandler(err, options.fail, options.complete)
+  }).catch(e => {
+    const error = Object.assign({ errMsg: 'removeStorage:error' }, e)
+    failHandler(error, options.fail, options.complete)
     reject()
   })
+}
+
+function getStorageSync(key) {
+  let obj = Object.create(null)
+  obj['key'] = key
+
+  try {
+    storage.get(obj)
+  } catch (e) {
+    const error = Object.assign({ errMsg: 'getStorageSync:error' }, e)
+    console.log(error)
+  }
+}
+
+function getStorage(options) {
+  return new Promise((resolve, reject) => {
+    storage.get(options)
+  }).then(res => {
+    successHandler(res, options.success, options.complete)
+    resolve()
+  }).catch(e => {
+    const error = Object.assign({ errMsg: 'getStorage:error' }, e)
+    failHandler(error, options.fail, options.complete)
+    reject()
+  })
+}
+
+function getStorageInfo() {
+  let storageInfo = Object.create(null)
+  let keys = []
+
+  try {
+    let length = storage.length
+    for (let i = 0; i < length; i++) {
+      storage.key({
+        index: i,
+        success: function(data) {
+          keys.push(data)
+        }
+      })
+    }
+    storageInfo['keys'] = keys
+    storageInfo = Object.assign({}, storageInfo, {
+      currentSize: 'It does not support for getting storage limitSize on quick app',
+      limitSize: 'It does not support for getting storage limitSize on quick app'
+    })
+    successHandler(storageInfo, options.success, options.complete)
+    return storageInfo
+  } catch(e) {
+    const error = Object.assign({ errMsg: 'getStorageInfo:error' }, e)
+    failHandler(error, options.fail, options.complete)
+  }
+}
+
+function clearStorageSync() {
+  try {
+    storage.clear({})
+  } catch (e) {
+    const error = Object.assign({ errMsg: 'removeStorageSync:error' }, e)
+    console.log(error)
+  }
+}
+
+function clearStorage() {
+  return new Promise((resolve, reject) => {
+    storage.clear(options)
+  }).then(res => {
+    successHandler(res, options.success, options.complete)
+    resolve()
+  }).catch(e => {
+    const error = Object.assign({ errMsg: 'clearStorage:error' }, e)
+    failHandler(error, options.fail, options.complete)
+    reject()
+  })
+}
+
+export {
+  setStorageSync,
+  setStorage,
+  removeStorageSync,
+  removeStorage,
+  getStorageSync,
+  getStorage,
+  getStorageInfo,
+  clearStorageSync,
+  clearStorage
 }
 
