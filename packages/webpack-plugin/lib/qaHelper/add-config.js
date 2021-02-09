59255180e3a66167c41cd9df88bb7acf25871625
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-02-09 12:26:40 +0800

    fix eslint

--- packages/webpack-plugin/lib/qaHelper/add-config.js
+++ packages/webpack-plugin/lib/qaHelper/add-config.js
@@ -4,11 +4,11 @@ module.exports = function (cnt, options) {
   const appJson = JSON.parse(cnt.children)
   const qa = options.quickapp
   let config = {
-    package: qa && qa.package || '',
-    name: qa && qa.name || '',
-    versionName: qa && qa.versionName || '',
-    versionCode: qa && qa.versionCode || 1,
-    minPlatformVersion: qa && qa.minPlatformVersion || 1080
+    package: qa ? qa.package : '',
+    name: qa ? qa.name : '',
+    versionName: qa ? qa.versionName : '',
+    versionCode: qa ? qa.versionCode : 1,
+    minPlatformVersion: qa ? qa.minPlatformVersion : 1080
   }
   let finalConfig = JSON.stringify(Object.assign({}, appJson, config), null, 2)
   let content = new ConcatSource(finalConfig)
