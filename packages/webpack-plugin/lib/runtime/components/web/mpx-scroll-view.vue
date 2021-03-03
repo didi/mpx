218a2ffe1722d321b1913cb883fab4c0e6ec8cd1
yandadaFreedom <525966780@qq.com>
2021-03-03 15:05:52 +0800

    update doc

--- packages/webpack-plugin/lib/runtime/components/web/mpx-scroll-view.vue
+++ packages/webpack-plugin/lib/runtime/components/web/mpx-scroll-view.vue
@@ -88,9 +88,7 @@
           scrollY: this.scrollY,
           probeType: 3,
           bounce: false,
-          observeDOM: this.observeDOM,
-          stopPropagation: true,
-          bindToWrapper: true,
+          observeDOM: this.observeDOM
         }
         const bsOptions = Object.assign({}, originBsOptions, this.scrollOptions)
         this.bs = new BScroll(this.$refs.wrapper, bsOptions)
