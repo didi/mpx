26d0615ec742b856fdcf77c9d01035da5979be0a
anotherso1a <1181581742@qq.com>
2021-06-29 15:33:14 +0800

    fix: remove types that user will never use

--- packages/core/@types/node.d.ts
+++ packages/core/@types/node.d.ts
@@ -1,3 +1,11 @@
-/// <reference path="./process.d.ts" />
-
 declare let global: Record<string, any> // in web, we use global varible to do some things, here to declare
+
+type Dict<T> = {
+  [k: string]: T | undefined
+}
+
+type EnvType = Dict<string>
+
+declare let process: {
+  env: EnvType
+}
