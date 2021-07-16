909f9416c80c77fb7737134771e0b77ab06501ca
yandadaFreedom <525966780@qq.com>
2021-07-16 15:04:33 +0800

    暂时不处理 ➜  mpx git:(feature-transRpx-rules) ✗ git add . ➜  mpx git:(feature-transRpx-rules) ✗ git commit -m 暂时不处理

--- packages/webpack-plugin/lib/web/processTemplate.js
+++ packages/webpack-plugin/lib/web/processTemplate.js
@@ -57,78 +57,69 @@ module.exports = function (template, options, callback) {
       return callback(new Error('[mpx loader][' + loaderContext.resource + ']: ' + 'template lang is not supported in trans web mode temporarily, we will support it in the future!'))
     }
 
-    output += genComponentTag(template, {
-      // attrs (template) {
-      //   const attrs = Object.assign({}, template.attrs)
-      //   if (hasScoped) {
-      //     attrs.mpxScoped = true
-      //   }
-      //   return attrs
-      // },
-      content (template) {
-        if (ctorType === 'app') {
-          return template.content
+    output += genComponentTag(template, template => {
+      if (ctorType === 'app') {
+        return template.content
+      }
+      if (template.content) {
+        const templateSrcMode = template.mode || srcMode
+        const parsed = templateCompiler.parse(template.content, {
+          warn: (msg) => {
+            loaderContext.emitWarning(
+              new Error('[template compiler][' + loaderContext.resource + ']: ' + msg)
+            )
+          },
+          error: (msg) => {
+            loaderContext.emitError(
+              new Error('[template compiler][' + loaderContext.resource + ']: ' + msg)
+            )
+          },
+          usingComponents: options.usingComponents,
+          hasComment: options.hasComment,
+          isNative: options.isNative,
+          basename: path.basename(resourcePath),
+          isComponent: ctorType === 'component',
+          mode,
+          srcMode: templateSrcMode,
+          defs,
+          decodeHTMLText: options.decodeHTMLText,
+          externalClasses: options.externalClasses,
+          hasScoped: false,
+          moduleId,
+          filePath: loaderContext.resourcePath,
+          i18n: null,
+          checkUsingComponents: options.checkUsingComponents,
+          // web模式下全局组件不会被合入usingComponents中，故globalComponents可以传空
+          globalComponents: [],
+          // web模式下实现抽象组件
+          componentGenerics: options.componentGenerics
+        })
+        if (parsed.meta.wxsModuleMap) {
+          wxsModuleMap = parsed.meta.wxsModuleMap
         }
-        if (template.content) {
-          const templateSrcMode = template.mode || srcMode
-          const parsed = templateCompiler.parse(template.content, {
-            warn: (msg) => {
-              loaderContext.emitWarning(
-                new Error('[template compiler][' + loaderContext.resource + ']: ' + msg)
-              )
-            },
-            error: (msg) => {
-              loaderContext.emitError(
-                new Error('[template compiler][' + loaderContext.resource + ']: ' + msg)
-              )
-            },
-            usingComponents: options.usingComponents,
-            hasComment: options.hasComment,
-            isNative: options.isNative,
-            basename: path.basename(resourcePath),
-            isComponent: ctorType === 'component',
-            mode,
-            srcMode: templateSrcMode,
-            defs,
-            decodeHTMLText: options.decodeHTMLText,
-            externalClasses: options.externalClasses,
-            hasScoped: false,
-            moduleId,
-            filePath: loaderContext.resourcePath,
-            i18n: null,
-            checkUsingComponents: options.checkUsingComponents,
-            // web模式下全局组件不会被合入usingComponents中，故globalComponents可以传空
-            globalComponents: [],
-            // web模式下实现抽象组件
-            componentGenerics: options.componentGenerics
+        if (parsed.meta.wxsContentMap) {
+          for (let module in parsed.meta.wxsContentMap) {
+            wxsContentMap[`${resourcePath}~${module}`] = parsed.meta.wxsContentMap[module]
+          }
+        }
+        if (parsed.meta.builtInComponentsMap) {
+          Object.keys(parsed.meta.builtInComponentsMap).forEach((name) => {
+            builtInComponentsMap[name] = {
+              resource: addQuery(parsed.meta.builtInComponentsMap[name], { component: true })
+            }
           })
-          if (parsed.meta.wxsModuleMap) {
-            wxsModuleMap = parsed.meta.wxsModuleMap
-          }
-          if (parsed.meta.wxsContentMap) {
-            for (let module in parsed.meta.wxsContentMap) {
-              wxsContentMap[`${resourcePath}~${module}`] = parsed.meta.wxsContentMap[module]
-            }
-          }
-          if (parsed.meta.builtInComponentsMap) {
-            Object.keys(parsed.meta.builtInComponentsMap).forEach((name) => {
-              builtInComponentsMap[name] = {
-                resource: addQuery(parsed.meta.builtInComponentsMap[name], { component: true })
-              }
-            })
-          }
-          if (parsed.meta.genericsInfo) {
-            genericsInfo = parsed.meta.genericsInfo
-          }
-          // 输出H5有多个root element时, 使用div标签包裹
-          if (parsed.root.tag === 'temp-node') {
-            const childLen = calculateRootEleChild(parsed.root.children)
-            if (childLen >= 2) {
-              parsed.root.tag = 'div'
-            }
-          }
-          return templateCompiler.serialize(parsed.root)
         }
+        if (parsed.meta.genericsInfo) {
+          genericsInfo = parsed.meta.genericsInfo
+        }
+        // 输出H5有多个root element时, 使用div标签包裹
+        if (parsed.root.tag === 'temp-node') {
+          const childLen = calculateRootEleChild(parsed.root.children)
+          if (childLen >= 2) {
+            parsed.root.tag = 'div'
+          }
+        }
+        return templateCompiler.serialize(parsed.root)
       }
     })
     output += '\n\n'
