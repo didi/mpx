798f265c0a0eb56bc786ffc2a5f9e966d198ec57
hiyuki <674883329@qq.com>
2021-07-19 18:28:28 +0800

    little adjust

--- packages/webpack-plugin/lib/runtime/components/web/mpx-video.vue
+++ packages/webpack-plugin/lib/runtime/components/web/mpx-video.vue
@@ -15,6 +15,7 @@
 
 <script>
   import { inheritEvent, extendEvent } from './getInnerListeners'
+
   export default {
     name: 'mpx-video',
     props: {
@@ -154,59 +155,58 @@
         const videoNode = this.$refs['_mpx_video_ref']
 
         videoNode.addEventListener('play', (e) => {
-          extendEvent(e, {detail: {}})
+          extendEvent(e, { detail: {} })
           this.$emit('play', e)
         })
 
         videoNode.addEventListener('pause', (e) => {
-          extendEvent(e, {detail: {}})
+          extendEvent(e, { detail: {} })
           this.$emit('pause', e)
         })
 
         videoNode.addEventListener('ended', (e) => {
-          extendEvent(e, {detail: {}})
+          extendEvent(e, { detail: {} })
           this.$emit('ended', e)
         })
 
         videoNode.addEventListener('timeupdate', (e) => {
           const eNode = e.target
-          extendEvent(e, {detail: {currentTime: eNode.currentTime, duration: eNode.duration}})
+          extendEvent(e, { detail: { currentTime: eNode.currentTime, duration: eNode.duration } })
           this.$emit('timeupdate', e)
         })
 
         videoNode.addEventListener('error', (e) => {
-          extendEvent(e, {detail: {}})
+          extendEvent(e, { detail: {} })
           this.$emit('error', e)
         })
 
         videoNode.addEventListener('waiting', (e) => {
-          extendEvent(e, {detail: {}})
+          extendEvent(e, { detail: {} })
           this.$emit('waiting', e)
         })
 
         videoNode.addEventListener('loadedmetadata', (e) => {
           const eNode = e.target
-          extendEvent(e, {detail: {width: eNode.videoWidth, height: eNode.videoHeight, duration: eNode.duration}})
+          extendEvent(e, { detail: { width: eNode.videoWidth, height: eNode.videoHeight, duration: eNode.duration } })
           this.$emit('loadedmetadata', e)
         })
 
         videoNode.addEventListener('progress', (e) => {
           const eNode = e.target
           const buffered = (eNode?.buffered?.end(0)) / (eNode?.duration)
-          extendEvent(e, {detail: {buffered: buffered * 100}})
+          extendEvent(e, { detail: { buffered: buffered * 100 } })
           this.$emit('progress', e)
         })
 
         videoNode.addEventListener('seeked', (e) => {
           const eNode = e.target
-          const NewEvent = inheritEvent('seekcomplete', e, {position: eNode.currentTime})
-          this.$emit('seekcomplete', NewEvent)
+          const ne = inheritEvent('seekcomplete', e, { position: eNode.currentTime })
+          this.$emit('seekcomplete', ne)
         })
 
         videoNode.addEventListener('fullscreenchange', (e) => {
-          const eNode = e.target
           //  TODO direction
-          extendEvent(e, {detail: {fullScreen: false}})
+          extendEvent(e, { detail: { fullScreen: false } })
           if (document.isFullScreen) {
             e.detail.fullScreen = true
             this.$emit('fullscreenchange', e)
@@ -217,12 +217,12 @@
         })
 
         videoNode.addEventListener('enterpictureinpicture', (e) => {
-          extendEvent(e, {detail: {}})
+          extendEvent(e, { detail: {} })
           this.$emit('enterpictureinpicture', e)
         })
 
         videoNode.addEventListener('leavepictureinpicture', (e) => {
-          extendEvent(e, {detail: {}})
+          extendEvent(e, { detail: {} })
           this.$emit('leavepictureinpicture', e)
         })
       }
@@ -235,15 +235,19 @@
     .mpx-no-show_progress
       &::-webkit-media-controls-timeline
         display none !important
+
     .mpx-no-show_fullscreen_btn
       &::-webkit-media-controls-fullscreen-button
         display none !important
+
     .mpx-no-show_play_btn
       &::-webkit-media-controls-play-button
         display none !important
+
     .mpx-no-show_center_play_btn
       &::-webkit-media-controls-start-playback-button
         display none !important
+
     .mpx-no-show_mute_btn
       &::-webkit-media-controls-mute-button
         display none !important
