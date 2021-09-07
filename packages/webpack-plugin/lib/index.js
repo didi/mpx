d396bbf51b5d8f256230052e41fad041a9defcc3
hiyuki <674883329@qq.com>
2021-09-07 18:04:15 +0800

    little fix

--- packages/webpack-plugin/lib/index.js
+++ packages/webpack-plugin/lib/index.js
@@ -549,8 +549,7 @@ class MpxWebpackPlugin {
               } else {
                 currentResourceMap[resourcePath] = outputPath
               }
-            } else {
-              // todo 可能导致watch后无法正确获取资源路径
+            } else if (!currentResourceMap[resourcePath]) {
               currentResourceMap[resourcePath] = true
             }
 
