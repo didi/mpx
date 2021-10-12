e320253e15feb7855327a74f39d7ec7856b5efd1
xuegan <xuegan@didiglobal.com>
2021-10-12 20:39:17 +0800

    tt mode下不删除pageLifetimes 中的 resize 生命周期

--- packages/core/src/convertor/wxToTt.js
+++ packages/core/src/convertor/wxToTt.js
@@ -10,9 +10,5 @@ export default {
         }
       })
     }
-    if (options.pageLifetimes && options.pageLifetimes.resize) {
-      error(`Options.pageLifetimes.resize is not supported in tt environment!`, global.currentResource)
-      delete options.pageLifetimes.resize
-    }
   }
 }
