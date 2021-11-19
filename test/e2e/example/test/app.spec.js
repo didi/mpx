e38af4db68d8bf3e1c2c203118f2eafd086538a3
xuegan <xuegan@didiglobal.com>
2021-11-19 17:54:11 +0800

    抽离 readFromDist 方法

--- test/e2e/example/test/app.spec.js
+++ test/e2e/example/test/app.spec.js
@@ -2,6 +2,11 @@ const fs = require('fs')
 const json5 = require('json5')
 const path = require('path')
 
+const readFileSyncInDist = (filePath, options) => {
+  const realPath = path.join('test/e2e/example/', filePath)
+  return fs.readFileSync(realPath, options)
+}
+
 describe('test App instance', () => {
   const subpackage = [
     {
@@ -13,17 +18,16 @@ describe('test App instance', () => {
   ]
 
   it('should wx App instance json is correct', function () {
-    console.log('=========', path.resolve('.'))
-    const wxAppJsonStr = fs.readFileSync('dist/wx/app.json', 'utf-8')
+    const wxAppJsonStr = readFileSyncInDist('dist/wx/app.json', 'utf-8')
     const wxAppJsonObj = json5.parse(wxAppJsonStr)
     const wxPages = wxAppJsonObj.pages
     const wxSubPackages = wxAppJsonObj.subPackages
-    expect(wxPages).toEqual(['pages/index', "pages/mode", 'pages/alias', 'pages/someEnv'])
+    expect(wxPages).toEqual(["pages/index", "pages/mode", "pages/alias", "pages/someEnv"])
     expect(wxSubPackages).toEqual(subpackage)
   })
 
   it('should ali App instance json is correct', function () {
-    const aliAppJsonStr = fs.readFileSync('dist/ali/app.json', 'utf-8')
+    const aliAppJsonStr = readFileSyncInDist('dist/ali/app.json', 'utf-8')
     const aliAppJsonObj = json5.parse(aliAppJsonStr)
     const aliPages = aliAppJsonObj.pages
     const aliSubPackages = aliAppJsonObj.subPackages
@@ -32,7 +36,7 @@ describe('test App instance', () => {
   })
 
   it('should tt App instance json is correct', function () {
-    const ttAppJsonStr = fs.readFileSync('dist/tt/app.json', 'utf-8')
+    const ttAppJsonStr = readFileSyncInDist('dist/tt/app.json', 'utf-8')
     const ttAppJsonObj = json5.parse(ttAppJsonStr)
     const ttPages = ttAppJsonObj.pages
     const ttSubPackages = ttAppJsonObj.subPackages
@@ -41,7 +45,7 @@ describe('test App instance', () => {
   })
 
   it('should swan App instance json is correct', function () {
-    const swanAppJsonStr = fs.readFileSync('dist/swan/app.json', 'utf-8')
+    const swanAppJsonStr = readFileSyncInDist('dist/swan/app.json', 'utf-8')
     const swanAppJsonObj = json5.parse(swanAppJsonStr)
     const swanPages = swanAppJsonObj.pages
     const swanSubPackages = swanAppJsonObj.subPackages
@@ -51,10 +55,10 @@ describe('test App instance', () => {
 
   it('should App mode and env display correct', function () {
     // test page resolve mode
-    const swanPageEnvStr = fs.readFileSync('dist/swan/pages/mode.swan', 'utf-8')
-    const aliPageEnvStr = fs.readFileSync('dist/ali/pages/mode.axml', 'utf-8')
-    const ttPageEnvStr = fs.readFileSync('dist/tt/pages/mode.ttml', 'utf-8')
-    const wxPageEnvStr = fs.readFileSync('dist/wx/pages/mode.wxml', 'utf-8')
+    const swanPageEnvStr = readFileSyncInDist('dist/swan/pages/mode.swan', 'utf-8')
+    const aliPageEnvStr = readFileSyncInDist('dist/ali/pages/mode.axml', 'utf-8')
+    const ttPageEnvStr = readFileSyncInDist('dist/tt/pages/mode.ttml', 'utf-8')
+    const wxPageEnvStr = readFileSyncInDist('dist/wx/pages/mode.wxml', 'utf-8')
     expect(swanPageEnvStr).toMatch(/<view>\{\{\("swan"\)}}<\/view>/)
     expect(aliPageEnvStr).toMatch(/<view>\{\{\("ali"\)}}<\/view>/)
     expect(ttPageEnvStr).toMatch(/<view>\{\{\("tt"\)}}<\/view>/)
