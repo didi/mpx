4438c97bd3ff8d63e5b832c00ccf416f80e8f732
yandadaFreedom <525966780@qq.com>
2021-11-22 20:26:07 +0800

    修复重复切换tabbar页面时，swiper停止自动播放

--- packages/webpack-plugin/lib/runtime/components/web/mpx-swiper.vue
+++ packages/webpack-plugin/lib/runtime/components/web/mpx-swiper.vue
@@ -93,9 +93,16 @@
     beforeCreate () {
       this.itemIds = []
     },
-    activated() {
-    	this.refresh()
-    },
+	  activated () {
+    	if (this.bs && this.autoplay) {
+		    this.bs.startPlay()
+      }
+	  },
+	  deactivated () {
+		  if (this.bs && this.autoplay) {
+			  this.bs.pausePlay()
+		  }
+	  },
     mounted () {
       this.bs = new BScroll(this.$refs.wrapper, {
         scrollX: !this.vertical,
