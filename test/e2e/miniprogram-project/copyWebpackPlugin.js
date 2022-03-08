e87b66148d1d32c419aa22b9ea2f27a76da02e55
xuegan <xuegan@didiglobal.com>
2022-03-08 16:52:16 +0800

    add script run

--- test/e2e/miniprogram-project/copyWebpackPlugin.js
+++ test/e2e/miniprogram-project/copyWebpackPlugin.js
@@ -1,2 +1,2 @@
 const { execSync } = require('child_process')
-execSync('scp -r ../../../packages/webpack-plugin/lib/ ./node_modules/@mpxjs/webpack-plugin/lib/')
+execSync('scp -r ../../../packages/webpack-plugin/lib/web/ ./node_modules/@mpxjs/webpack-plugin/lib/')
--- test/e2e/miniprogram-project/ls-plugin.js
+++
@@ -1,2 +0,0 @@
-const { execSync } = require('child_process')
-execSync('cd ./dist/wx && ls')
