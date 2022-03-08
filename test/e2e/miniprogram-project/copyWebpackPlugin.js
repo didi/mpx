d1be15c0d4a223f25e8b193ba507889fade835c0
xuegan <xuegan@didiglobal.com>
2022-03-08 16:29:35 +0800

    验证单元测试问题

--- test/e2e/miniprogram-project/copyWebpackPlugin.js
+++ test/e2e/miniprogram-project/copyWebpackPlugin.js
@@ -1,2 +1,2 @@
-const {execSync} = require('child_process')
+const { execSync } = require('child_process')
 execSync('scp -r ../../../packages/webpack-plugin/lib/ ./node_modules/@mpxjs/webpack-plugin/lib/')
---
+++ test/e2e/miniprogram-project/ls-plugin.js
@@ -0,0 +1,2 @@
+const { execSync } = require('child_process')
+execSync('cd ./dist/wx && ls')
