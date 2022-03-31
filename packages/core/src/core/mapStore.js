ba3f00371406a5933481cc3a08791b2447971bff
luyongfang <luyongfang@didichuxing.com>
2022-03-31 10:23:38 +0800

    async store

--- packages/core/src/core/mapStore.js
+++ packages/core/src/core/mapStore.js
@@ -30,6 +30,20 @@ function mapFactory (type, store) {
   }
 }
 
+function checkMapInstance (maps, context, errorStr) {
+  if (!context) {
+    context = maps
+    maps = null
+  }
+  if (Object.prototype.toString.call(context) != '[object Object]') {
+    console.error(`调用${errorStr}时请传入当前component实例`)
+  }
+  return {
+    context,
+    maps
+  }
+}
+
 export default function (store) {
   return {
     mapGetters: mapFactory('getters', store),
@@ -49,6 +63,43 @@ export default function (store) {
         }
       })
       return result
+    },
+    // 以下map**ToInstance用于异步store
+    mapStateToInstance: (depPath, mapsNs, contextComp) => {
+      let { context, maps } = checkMapInstance(mapsNs, contextComp, 'mapStateToInstance')
+      maps = normalizeMap(depPath, maps)
+      const result = {}
+      Object.keys(maps).forEach(key => {
+        const value = maps[key]
+        result[key] = function () {
+          if (typeof value === 'function') {
+            return value.call(this, store.state, store.getters)
+          } else if (typeof value === 'string') {
+            return getByPath(store.state, value)
+          }
+        }
+      })
+      const mpxProxyIns = context.__mpxProxy.options.computed || {}
+      Object.assign(mpxProxyIns, result)
+    },
+    mapGettersToInstance: (depPath, mapsNs, contextComp) => {
+      const { context, maps } = checkMapInstance(mapsNs, contextComp, 'mapGettersToInstance')
+      const mapGet = mapFactory('getters', store)
+      const result = mapGet(depPath, maps)
+      const mpxProxyIns = context.__mpxProxy.options.computed || {}
+      Object.assign(mpxProxyIns, result)
+    },
+    mapMutationsToInstance: (depPath, mapsNs, contextComp) => {
+      const { context, maps } = checkMapInstance(mapsNs, contextComp, 'mapMutationsToInstance')
+      const mapMutation = mapFactory('mutations', store)
+      const result = mapMutation(depPath, maps)
+      Object.assign(context, result)
+    },
+    mapActionsToInstance: (depPath, mapsNs, contextComp) => {
+      const { context, maps } = checkMapInstance(mapsNs, contextComp, 'mapActionsToInstance')
+      const mapAction = mapFactory('actions', store)
+      const result = mapAction(depPath, maps)
+      Object.assign(context, result)
     }
   }
 }
