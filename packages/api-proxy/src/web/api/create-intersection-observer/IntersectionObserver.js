6535be596d099a1545995feab3da43fa06bdc7f4
yandadaFreedom <525966780@qq.com>
2022-05-25 18:36:38 +0800

    fix: add async

--- packages/api-proxy/src/web/api/create-intersection-observer/IntersectionObserver.js
+++ packages/api-proxy/src/web/api/create-intersection-observer/IntersectionObserver.js
@@ -6,17 +6,21 @@ class WebIntersectionObserver {
   constructor (_component, options) {
     this._component = _component
     this._options = options || {}
-    this._disconnected = false
     this._relativeInfo = []
-    this.callback = null
+    this._callback = null
+    this._observer = null
+    this._root = null
+    this._rootMargin = ''
+    this._disconnected = false
   }
 
-  initObserver (root, rootMargin) {
-    if (this.observer) {
-      this.observer = null
+  initObserver () {
+    if (this._observer) {
+      this._observer = null
     }
+    this._disconnected = false
     // eslint-disable-next-line no-undef
-    this.observer = new IntersectionObserver((entries, observer) => {
+    return new IntersectionObserver((entries, observer) => {
       const initialRatio = this._options.initialRatio || 0
       const thresholds = this._options.thresholds || [0]
       const thresholdsSortArr = thresholds.sort((a, b) => {
@@ -51,30 +55,25 @@ class WebIntersectionObserver {
               configurable: true
             }
           })
-          this.callback && this.callback(entry)
+          this._callback && this._callback(entry)
         }
       })
       isInit = false
     }, {
-      root: root || null,
-      rootMargin,
+      root: this._root || null,
+      rootMargin: this._rootMargin,
       threshold: this._options.thresholds || [0]
     })
-    this._disconnected = false
-  }
-
-  disconnect () {
-    this._disconnected = true
-    this.observer.disconnect()
   }
 
   observe (targetSelector, callback) {
-    nextTick(() => {
+    nextTick(async () => {
       if (!targetSelector) {
         const res = { errMsg: 'observe:targetSelector can not be empty' }
         return Promise.reject(res)
       }
-      this.callback = callback
+      this._observer = await this.initObserver()
+      this._callback = callback
       let targetElement = []
       if (this._options.observeAll) {
         targetElement = document.querySelectorAll(targetSelector)
@@ -82,40 +81,44 @@ class WebIntersectionObserver {
         targetElement = [document.querySelector(targetSelector)]
       }
       targetElement.forEach((element) => {
-        this.observer.observe(element)
+        this._observer && this._observer.observe(element)
       })
     })
   }
 
-  relativeTo (selector, margins = {}) {
+  relativeTo (selector, margins) {
     nextTick(() => {
-      const { left = 0, right = 0, top = 0, bottom = 0 } = margins
-      const root = document.querySelector(selector)
-      const rootMargin = `${top}px ${right}px ${bottom}px ${left}px`
-      this._relativeInfo.push({selector, margins})
-      this.initObserver(root, rootMargin)
+      const marginsTemp = margins || {}
+      const { left = 0, right = 0, top = 0, bottom = 0 } = marginsTemp
+      this._root = document.querySelector(selector)
+      this._rootMargin = `${top}px ${right}px ${bottom}px ${left}px`
+      this._relativeInfo.push({ selector, margins })
     })
     return this
   }
 
-  relativeToViewport (margins = {}) {
+  relativeToViewport (margins) {
     nextTick(() => {
-      const { left = 0, right = 0, top = 0, bottom = 0 } = margins
-      const root = document.querySelector('.app')
+      const marginsTemp = margins || {}
+      const { left = 0, right = 0, top = 0, bottom = 0 } = marginsTemp
+      this._root = document.querySelector('html')
       const viewportWidth = window.innerWidth || document.documentElement.clientWidth
       const viewportHeight = window.innerHeight || document.documentElement.clientHeight
-      const rootWidth = root.offsetWidth || 0
-      const rootHeight = root.offsetHeight || 0
-      let rootMargin = ''
+      const rootWidth = this._root.offsetWidth || 0
+      const rootHeight = this._root.offsetHeight || 0
       if (rootHeight >= viewportHeight) {
-        rootMargin = `${top}px ${viewportWidth - rootWidth + right}px ${viewportHeight - rootHeight + bottom}px ${left}px`
+        this._rootMargin = `${top}px ${viewportWidth - rootWidth + right}px ${viewportHeight - rootHeight + bottom}px ${left}px`
       } else {
-        rootMargin = `${top}px ${right}px ${bottom}px ${left}px`
+        this._rootMargin = `${top}px ${right}px ${bottom}px ${left}px`
       }
       this._relativeInfo.push({ selector: null, margins })
-      this.initObserver(root, rootMargin)
     })
     return this
+  }
+
+  disconnect () {
+    this._disconnected = true
+    this._observer.disconnect()
   }
 }
 
