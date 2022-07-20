506f616b50f949669fd350da14ed923ecef8f2c9
luyongfang <luyongfang@didichuxing.com>
2022-07-20 12:10:43 +0800

    fix eslint

--- packages/core/src/platform/patch/web/getDefaultOptions.js
+++ packages/core/src/platform/patch/web/getDefaultOptions.js
@@ -1,8 +1,8 @@
 import builtInKeysMap from '../builtInKeysMap'
 import mergeOptions from '../../../core/mergeOptions'
 import { diffAndCloneA } from '../../../helper/utils'
-import { getCurrentInstance as getVueCurrentInstance } from '../../export/index'
-import { MpxProxy, setCurrentInstance, unsetCurrentInstance } from '../../../core/proxy'
+import { getCurrentInstance as getVueCurrentInstance} from '../../export/index'
+import MpxProxy, { setCurrentInstance, unsetCurrentInstance } from '../../../core/proxy'
 
 function filterOptions (options) {
   const newOptions = {}
