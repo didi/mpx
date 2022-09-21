6d0d7e118ec8ee26f18a30addc5ce21554f58f2c
yandadaFreedom <525966780@qq.com>
2022-09-21 14:48:35 +0800

    update: 合并d,ts

--- packages/web-plugin/src/types/patch.d.ts
+++ packages/web-plugin/src/types/patch.d.ts
@@ -1,3 +1,99 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module 'webpack/lib/InitFragment' {
+  export default class InitFragment {
+    static STAGE_CONSTANTS: any
+    constructor(...args: any[])
+  }
+}
+declare module 'webpack/lib/util/makeSerializable' {
+  export default function makeSerializable(...args: any[]): any
+}
+declare module 'webpack/lib/dependencies/ModuleDependency' {}
+declare module '@mpxjs/compiler/template-compiler/parser' {
+  export default function parser(...args: any[]): any
+}
+declare module 'webpack/lib/NullFactory' {
+  export default class NullFactory {
+    constructor()
+  }
+}
+declare module 'webpack/lib/dependencies/HarmonyImportDependencyParserPlugin' {}
+declare module 'webpack/lib/FlagEntryExportAsUsedPlugin' {
+  export default class FlagEntryExportAsUsedPlugin {
+    constructor(nsObjectUsed:boolean, explanation:string)
+    apply(compiler: any):this
+  }
+}
+declare module 'webpack/lib/FileSystemInfo' {
+  export default function FileSystemInfo(): any
+}
+
+declare module '@mpxjs/webpack-plugin/lib/dependencies/ResolveDependency' {
+  export default class ResolveDependency{
+    static Template: typeof ResolveDependencyTemplate
+    constructor(resource:string, packageName:string, issuerResource:string, range: number[])
+  }
+}
+declare class InjectDependencyTemplate {
+  constructor();
+}
+declare module '@mpxjs/webpack-plugin/lib/dependencies/InjectDependency' {
+  export default class InjectDependency {
+    static Template: typeof InjectDependencyTemplate
+    constructor(options : { content: string; index: number } )
+  }
+}
+declare class CommonJsVariableDependencyTemplate {
+  constructor();
+}
+declare module '@mpxjs/webpack-plugin/lib/dependencies/CommonJsVariableDependency' {
+  export default class CommonJsVariableDependency {
+    name: string
+    static Template: typeof CommonJsVariableDependencyTemplate
+    constructor(request: string, name: string)
+  }
+}
+declare class ReplaceDependencyTemplate {
+  constructor();
+}
+declare module '@mpxjs/webpack-plugin/lib/dependencies/ReplaceDependency' {
+  export default class ReplaceDependency {
+    static Template: typeof ReplaceDependencyTemplate
+    constructor(replacement: string, range: number[])
+  }
+}
+declare class RecordResourceMapDependencyTemplate {
+  constructor();
+}
+declare module '@mpxjs/webpack-plugin/lib/dependencies/RecordResourceMapDependency' {
+  export default class RecordResourceMapDependency {
+    static Template: typeof RecordResourceMapDependencyTemplate
+    constructor(resourcePath: string, resourceType: string, outputPath: string, packageRoot: string)
+  }
+}
+declare class RecordVueContentDependencyTemplate {
+  constructor();
+}
+declare module '@mpxjs/webpack-plugin/lib/dependencies/RecordVueContentDependency' {
+  export default class RecordVueContentDependency {
+    static Template: typeof RecordVueContentDependencyTemplate
+    constructor(resourcePath: string, content: string)
+  }
+}
+
+declare interface DepConstructor {
+  new (...args: any[]): any;
+}
+
+declare class DependencyTemplate {
+  constructor();
+  apply(...args: any[]): void;
+}
+
+declare abstract class ModuleFactory {
+  create(...args: any[]): void;
+}
+
 declare module 'lru-cache' {
   class LruCache {
     constructor(cache: number);
