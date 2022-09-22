791dbb11b99edef71fbad8cd48b726faad0ea932
Maoerbuxiong <anniechan_up@126.com>
2022-09-22 20:23:47 +0800

    Modify storeToRefs

--- packages/pinia/src/index.web.js
+++ packages/pinia/src/index.web.js
@@ -10,9 +10,10 @@ import {
   mapState,
   mapGetters,
   mapActions,
-  mapWritableState,
-  storeToRefs
+  mapWritableState
 } from 'pinia'
+import { isRef, isReactive, toRef } from '@mpxjs/core'
+import { isFunction } from '@mpxjs/utils'
 
 vue.use(PiniaVuePlugin)
 
@@ -20,6 +21,20 @@ function createPinia () {
   const webPinia = webCreatePinia()
   global.__mpx_pinia = webPinia
   return webPinia
+}
+
+function storeToRefs (store) {
+  let refs = {}
+  const isNotNativeProps = prop => {
+    return typeof prop === 'string' && prop[0] !== '$' && prop[0] !== '_'
+  }
+  for (let key in store) {
+    const value = store[key]
+    if (isNotNativeProps(key) && !isRef(value) && !isReactive(value) && !isFunction(value)) {
+      refs[key] = toRef(store, key)
+    }
+  }
+  return refs
 }
 
 export {
--- packages/pinia/src/storeToRefs.js
+++ packages/pinia/src/storeToRefs.js
@@ -1,9 +1,21 @@
-import { toRefs } from '@mpxjs/core'
+import { isRef, isReactive, toRef } from '@mpxjs/core'
+import { isFunction } from '@mpxjs/utils'
 /**
  * * @description: all the props extracted are reactive
  * * @param: store
  * @return {*} store
  */
 export function storeToRefs (store) {
-  return toRefs(store)
+  // return toRefs(store)
+  let refs = {}
+  const isNotNativeProps = prop => {
+    return typeof prop === 'string' && prop[0] !== '$' && prop[0] !== '_'
+  }
+  for (let key in store) {
+    const value = store[key]
+    if (isNotNativeProps(key) && !isRef(value) && !isReactive(value) && !isFunction(value)) {
+      refs[key] = toRef(store, key)
+    }
+  }
+  return refs
 }
