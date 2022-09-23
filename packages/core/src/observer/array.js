733941c200cc8a2d0d0caeabf4e2e8a2228252a3
Maoerbuxiong <anniechan_up@126.com>
2022-09-23 23:04:37 +0800

    Fix issue on mp

--- packages/core/src/observer/array.js
+++ packages/core/src/observer/array.js
@@ -1,7 +1,15 @@
-import { defProp } from '@mpxjs/utils'
 import { getObserver } from './reactive'
 
 const arrayProto = Array.prototype
+// TODO 从mpxjs/utils引入defProp，项目会报错找不到，先这样处理，后续看下解决方案
+function def (obj, key, val, enumerable) {
+  Object.defineProperty(obj, key, {
+    value: val,
+    enumerable: !!enumerable,
+    writable: true,
+    configurable: true
+  })
+}
 export const arrayMethods = Object.create(arrayProto)
 
 ;[
@@ -15,7 +23,7 @@ export const arrayMethods = Object.create(arrayProto)
 ].forEach(function (method) {
   // cache original method
   const original = arrayProto[method]
-  defProp(arrayMethods, method, function mutator (...args) {
+  def(arrayMethods, method, function mutator (...args) {
     const result = original.apply(this, args)
     const ob = getObserver(this)
     if (ob) {
