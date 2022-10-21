83a06ab32107afef0fc2fd9e2dd9ac6001055523
xuegan <xuegan@didiglobal.com>
2022-10-21 17:33:02 +0800

    删除不必要的behavior string 判断

--- packages/core/src/convertor/wxToTt.js
+++ packages/core/src/convertor/wxToTt.js
@@ -11,7 +11,7 @@ export default {
   convert (options) {
     if (options.behaviors) {
       options.behaviors.forEach((behavior, idx) => {
-        if (typeof behavior === 'string' && BEHAVIORS_MAP.includes(behavior)) {
+        if (BEHAVIORS_MAP.includes(behavior)) {
           error(`Built-in behavior "${behavior}" is not supported in tt environment!`, global.currentResource)
           options.behaviors.splice(idx, 1)
         }
