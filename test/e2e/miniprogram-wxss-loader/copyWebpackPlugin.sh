9b84a081188f18fca71ad395a528c1d4a1314e27
xuegan <xuegan@didiglobal.com>
2022-12-20 21:56:41 +0800

    fix:修复e2e相关配置

--- test/e2e/miniprogram-wxss-loader/copyWebpackPlugin.sh
+++ test/e2e/miniprogram-wxss-loader/copyWebpackPlugin.sh
@@ -1,7 +1,13 @@
 echo "start copy webpackPlugin"
 
-rm -rf ./node_modules/@mpxjs/webpack-plugin/lib
+rm -rf ./node_modules/@mpxjs/webpack-plugin/lib ./node_modules/@mpxjs/utils/src ./node_modules/@mpxjs/store/src ./node_modules/@mpxjs/pinia/src ./node_modules/@mpxjs/core/src ./node_modules/@mpxjs/fetch/src ./node_modules/@mpxjs/api-proxy/src
 
-scp -r ../../../packages/webpack-plugin/lib/ ./node_modules/@mpxjs/webpack-plugin/lib/
+cp -r ../../../packages/webpack-plugin/lib/ ./node_modules/@mpxjs/webpack-plugin/lib/
+cp -r ../../../packages/utils/src/ ./node_modules/@mpxjs/utils/src/
+cp -r ../../../packages/store/src/ ./node_modules/@mpxjs/store/src/
+cp -r ../../../packages/pinia/src/ ./node_modules/@mpxjs/pinia/src/
+cp -r ../../../packages/core/src/ ./node_modules/@mpxjs/core/src/
+cp -r ../../../packages/fetch/src/ ./node_modules/@mpxjs/fetch/src/
+cp -r ../../../packages/api-proxy/src/ ./node_modules/@mpxjs/api-proxy/src/
 
 echo "end copy webpackPlugin"
