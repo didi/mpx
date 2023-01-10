274bcf8baa789e27e49df202bc85826867107102
xuegan <xuegan@didiglobal.com>
2023-01-10 20:43:34 +0800

    fix:code lint error

--- packages/webpack-plugin/lib/utils/ts-loader-watch-run-loader-filter.js
+++ packages/webpack-plugin/lib/utils/ts-loader-watch-run-loader-filter.js
@@ -4,22 +4,20 @@ const scriptSetupPath = normalize.lib('script-setup-compiler/index.js')
 const mpxLoaderPath = normalize.lib('loader.js')
 const { has } = require('./set')
 
-const tsLoaderWatchRunFilterLoaders = [
+const tsLoaderWatchRunFilterLoaders = new Set([
   selectorPath,
   scriptSetupPath,
   mpxLoaderPath,
   'node_modules/vue-loader/lib/index.js'
-]
+])
 
 module.exports = (loaders, loaderIndex) => {
   for (let len = loaders.length; len > 0; --len) {
     const currentLoader = loaders[len - 1]
-    if (!has(tsLoaderWatchRunFilterLoaders, (filterLoaderPath) => {
-      return currentLoader.path.endsWith(filterLoaderPath)
-    })) {
+    if (!has(tsLoaderWatchRunFilterLoaders, filterLoaderPath => currentLoader.path.endsWith(filterLoaderPath))) {
       break
     }
-    loaderIndex -= 1
+    loaderIndex--
   }
   return loaderIndex
 }
