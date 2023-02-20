6123a37ba011116b93b82215daaf1f5ca5874f29
sarahliu <sarahliu@didiglobal.com>
2023-02-20 16:35:39 +0800

    修改

--- packages/utility-first-css/index.js
+++ packages/utility-first-css/index.js
@@ -1,30 +1,30 @@
 const path = require('path')
-var windicss = require("windicss");
+let windicss = require('windicss')
 const hash = require('hash-sum')
-const windiConfigPath = path.join(process.cwd(),'./windi.config.js')
+const windiConfigPath = path.join(process.cwd(), './windi.config.js')
 const windiConfig = require(windiConfigPath)
-var import_style = require("windicss/utils/style");
-var import_utils3 = require("@antfu/utils");
-const regexHtmlTag = /<(\w[\w-]*)([\S\s]*?)\/?>/mg;
-var regexClassSplitter = /[\s'"`{}]/g;
-var regexClassCheck1 = /^!?[a-z\d@<>.+-](?:\([\w,.%#\(\)+-]*\)|[\w:/\\,%#\[\].$-])*$/;
-var regexAttributifyItem = /(?:\s|^)([\w+:_/-]+)\s?=\s?(['"{])((?:\\\2|\\\\|\n|\r|.)*?)(?:\2|\})/gm;
-var regexClassCheck2 = /[a-z].*[\w)\]]$/;
-var regexClassChecks = [
+let import_style = require('windicss/utils/style')
+let import_utils3 = require('@antfu/utils')
+const regexHtmlTag = /<(\w[\w-]*)([\S\s]*?)\/?>/mg
+let regexClassSplitter = /[\s'"`{}]/g
+let regexClassCheck1 = /^!?[a-z\d@<>.+-](?:\([\w,.%#\(\)+-]*\)|[\w:/\\,%#\[\].$-])*$/
+let regexAttributifyItem = /(?:\s|^)([\w+:_/-]+)\s?=\s?(['"{])((?:\\\2|\\\\|\n|\r|.)*?)(?:\2|\})/gm
+let regexClassCheck2 = /[a-z].*[\w)\]]$/
+let regexClassChecks = [
   regexClassCheck1,
   regexClassCheck2
-];
-const { RawSource } = require("webpack-sources");
-let classesPending = /* @__PURE__ */ {};
-const classesGenerated = /* @__PURE__ */ {};
-const layerStylesMap = /* @__PURE__ */ {};
+]
+const { RawSource } = require("webpack-sources")
+let classesPending = /* @__PURE__ */ {}
+const classesGenerated = /* @__PURE__ */ {}
+const layerStylesMap = /* @__PURE__ */ {}
 const layers = {
     base: {},
     utilities: {},
     components: {}
-  };
+  }
 const commonDir = []
-const attributes = [];
+const attributes = []
 const OUTPUTCSSFILENAME = 'index'
 const MAINDIR = 'main'
 const CSSFILEEXTMAP = {
@@ -33,68 +33,68 @@ const CSSFILEEXTMAP = {
 }
 const FILESPLIT = path.sep
 function validClassName(i) {
-    return regexClassChecks.every((r) => i.length > 2 && i.match(r));
+    return regexClassChecks.every((r) => i.length > 2 && i.match(r))
 }
 
 function include(set, v) {
     for (const i of v)
-      set.add(i);
+      set.add(i)
 }
 function DefaultExtractor(code) {
-    const tags = Array.from(code.matchAll(regexHtmlTag));
-    const tagNames = tags.map((i) => i[1]);
+    const tags = Array.from(code.matchAll(regexHtmlTag))
+    const tagNames = tags.map((i) => i[1])
     return {
       tags: tagNames,
       get classes() {
-        return code.split(regexClassSplitter).filter(validClassName);
+        return code.split(regexClassSplitter).filter(validClassName)
       },
       get attributes() {
-        const attrRanges = [];
+        const attrRanges = []
         const attributes = {
           names: [],
           values: []
-        };
-        const attributesBlocklist = ["class", "className"];
-        const tagsBlocklist = ["meta", "script", "style", "link"];
+        }
+        const attributesBlocklist = ["class", "className"]
+        const tagsBlocklist = ["meta", "script", "style", "link"]
         tags.filter((i) => !tagsBlocklist.includes(i[1])).forEach((i) => {
           return Array.from(i[2].matchAll(regexAttributifyItem) || []).forEach((match) => {
-            let name = match[1];
-            const [full, , , value] = match;
-            name = name.replace(/^(:|v-bind:)/, "");
+            let name = match[1]
+            const [full, , , value] = match
+            name = name.replace(/^(:|v-bind:)/, "")
             if (attributesBlocklist.includes(name))
-              return;
-            attributes.names.push(name);
-            attributes.values.push(value);
+              return
+            attributes.names.push(name)
+            attributes.values.push(value)
             if (match.index != null)
-              attrRanges.push([match.index, match.index + full.length]);
-          });
-        });
-        return attributes;
+              attrRanges.push([match.index, match.index + full.length])
+          })
+        })
+        return attributes
       }
-    };
+    }
 }
 function applyExtractors(code) {
   const results = [DefaultExtractor].map((extractor) => extractor(code))
   const attributesNames = results.flatMap((v) => {
-  var _a, _b;
-  return (_b = (_a = v.attributes) == null ? void 0 : _a.names) != null ? _b : [];
-  });
+  let _a, _b
+  return (_b = (_a = v.attributes) == null ? void 0 : _a.names) != null ? _b : []
+  })
   const attributesValues = results.flatMap((v) => {
-  var _a, _b;
-  return (_b = (_a = v.attributes) == null ? void 0 : _a.values) != null ? _b : [];
-  });
+  let _a, _b
+  return (_b = (_a = v.attributes) == null ? void 0 : _a.values) != null ? _b : []
+  })
   return {
     tags: (0, import_utils3.uniq)(results.flatMap((v) => {
-        var _a;
-        return (_a = v.tags) != null ? _a : [];
+        let _a
+        return (_a = v.tags) != null ? _a : []
     })),
     ids: (0, import_utils3.uniq)(results.flatMap((v) => {
-        var _a;
-        return (_a = v.ids) != null ? _a : [];
+        let _a
+        return (_a = v.ids) != null ? _a : []
     })),
     classes: (0, import_utils3.uniq)(results.flatMap((v) => {
-        var _a;
-        return (_a = v.classes) != null ? _a : [];
+        let _a
+        return (_a = v.classes) != null ? _a : []
     })),
     attributes: attributesNames.length || attributesValues.length ? {
         names: attributesNames,
@@ -109,19 +109,19 @@ function addClasses(classes, dir) {
   }
   classes.forEach((i) => {
       if (!i || classesGenerated[dir].has(i) || classesPending[dir].has(i))
-      return;
-      classesPending[dir].add(i);
-  });
-  return classesPending;
+      return
+      classesPending[dir].add(i)
+  })
+  return classesPending
 }
 function extractFileLoader(code, dir) {
-  const extractResult = applyExtractors(code);
+  const extractResult = applyExtractors(code)
   if(windiConfig.attributify) {
-    const extractedAttrs = extractResult.attributes;
+    const extractedAttrs = extractResult.attributes
     if (extractedAttrs == null ? void 0 : extractedAttrs.names.length) {
       extractedAttrs.names.forEach((name2, i) => {
-        attributes.push([name2, extractedAttrs.values[i]]);
-      });
+        attributes.push([name2, extractedAttrs.values[i]])
+      })
     }
     return addClasses((extractedAttrs == null ? void 0 : extractedAttrs.classes) || extractResult.classes || [], dir)
   } else {
@@ -129,62 +129,60 @@ function extractFileLoader(code, dir) {
   }
 }
 function buildLayerCss(layer,dir) {
-    var _a;
-    const style = new import_style.StyleSheet(Array.from(layerStylesMap[dir].values()).flatMap((i) => i).filter((i) => i.meta.type === layer));
-    // style.prefixer = (_a = windiConfigMap[dir].prefixer) != null ? _a : true;
-    style.prefixer = (_a = windiConfig.prefixer) != null ? _a : true;
+    let _a
+    const style = new import_style.StyleSheet(Array.from(layerStylesMap[dir].values()).flatMap((i) => i).filter((i) => i.meta.type === layer))
+    style.prefixer = (_a = windiConfig.prefixer) != null ? _a : true
     return `${style.build()}`
 }
 function buildPendingStyles(dir) {
-  // const processor = new windicss(windiConfigMap[dir]);
-  const processor = new windicss(windiConfig);
+  const processor = new windicss(windiConfig)
   if(!classesPending[dir]){
     classesPending[dir] = new Set()
   }
   if (classesPending[dir].size) {
-    const result = processor.interpret(Array.from(classesPending[dir]).join(" "));
+    const result = processor.interpret(Array.from(classesPending[dir]).join(" "))
     if (result.success.length) {
-      updateLayers(result.styleSheet.children, "__classes", dir, false);
-      include(classesGenerated[dir], result.success);
-      classesPending[dir].clear();
+      updateLayers(result.styleSheet.children, "__classes", dir, false)
+      include(classesGenerated[dir], result.success)
+      classesPending[dir].clear()
     }
   }
   function updateLayers(styles, filepath, dir, replace = true) {
-    var _a;
-    const timestamp = +Date.now();
-    const changedLayers = /* @__PURE__ */ new Set();
-    styles.forEach((i) => changedLayers.add(i.meta.type));
+    let _a
+    const timestamp = +Date.now()
+    const changedLayers = /* @__PURE__ */ new Set()
+    styles.forEach((i) => changedLayers.add(i.meta.type))
     if (replace) {
-      (_a = layerStylesMap[dir].get(filepath)) == null ? void 0 : _a.forEach((i) => changedLayers.add(i.meta.type));
-      layerStylesMap[dir].set(filepath, styles);
+      (_a = layerStylesMap[dir].get(filepath)) == null ? void 0 : _a.forEach((i) => changedLayers.add(i.meta.type))
+      layerStylesMap[dir].set(filepath, styles)
     } else {
-      const prevStyles = layerStylesMap[dir].get(filepath) || [];
-      layerStylesMap[dir].set(filepath, prevStyles.concat(styles));
+      const prevStyles = layerStylesMap[dir].get(filepath) || []
+      layerStylesMap[dir].set(filepath, prevStyles.concat(styles))
     }
     for (const name2 of changedLayers) {
-      const layer = layers[name2];
+      const layer = layers[name2]
       if (layer) {
-        layer.timestamp = timestamp;
-        layer.cssCache = void 0;
+        layer.timestamp = timestamp
+        layer.cssCache = void 0
       }
     }
   }
   if (windiConfig.attributify) {
     if (attributes.length) {
-      const attributesObject = {};
+      const attributesObject = {}
       attributes.filter((i) => i[0] && i[1]).forEach(([name2, value]) => {
         if (!attributesObject[name2])
-          attributesObject[name2] = [];
-        attributesObject[name2].push(...String(value).split(regexClassSplitter).filter(Boolean));
-      });
-      const attributifyStyle = processor.attributify(attributesObject);
-      updateLayers(attributifyStyle.styleSheet.children, "__attributify", dir, false);
-      attributes.length = 0;
+          attributesObject[name2] = []
+        attributesObject[name2].push(...String(value).split(regexClassSplitter).filter(Boolean))
+      })
+      const attributifyStyle = processor.attributify(attributesObject)
+      updateLayers(attributifyStyle.styleSheet.children, "__attributify", dir, false)
+      attributes.length = 0
     }
   }
 }
 function generateCSS(layer,dir) {
-  buildPendingStyles(dir);
+  buildPendingStyles(dir)
   return buildLayerCss(layer,dir)
 }
 function getCommonClass(){
@@ -214,7 +212,7 @@ function getDirs(defaultSubpackages) {
     windiConfig?.extract?.include.forEach(item => {
       item.replace(/subpackage\/(\w+)/g,(str,$1) => {
         if($1) dirs.push($1)
-      });
+      })
     })
   } else {
     dirs = Object.keys(defaultSubpackages)
