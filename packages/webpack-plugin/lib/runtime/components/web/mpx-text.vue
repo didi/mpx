dea6d06f146d45377db78c8dfde0d08b2515d810
yandadaFreedom <525966780@qq.com>
2023-06-09 19:34:10 +0800

    fix: 节点丢失

--- packages/webpack-plugin/lib/runtime/components/web/mpx-text.vue
+++ packages/webpack-plugin/lib/runtime/components/web/mpx-text.vue
@@ -58,7 +58,7 @@
           newSlots.push(createElement('mpx-text', {
             on: componentOptions.listeners,
             props:componentOptions.propsData
-          }, item.componentOptions && item.componentOptions.children))
+          }, item.children || (item.componentOptions && item.componentOptions.children)))
         }
       })
       if (this.selectable) {
