817cc42fc267ea874278ebb480955a0803fb4f3b
zhuzhh <zhuzhh666@163.com>
2023-06-15 14:41:10 +0800

    优化代码(二)

--- packages/webpack-plugin/lib/template-compiler/bind-this.js
+++ packages/webpack-plugin/lib/template-compiler/bind-this.js
@@ -2,7 +2,7 @@ const babylon = require('@babel/parser')
 const traverse = require('@babel/traverse').default
 const t = require('@babel/types')
 const generate = require('@babel/generator').default
-const { MultiNode, clearCache } = require('../utils/multi-node.js')
+// const { MultiNode, clearCache } = require('../utils/multi-node.js')
 
 const names = 'Infinity,undefined,NaN,isFinite,isNaN,' +
   'parseFloat,parseInt,decodeURI,decodeURIComponent,encodeURI,encodeURIComponent,' +
@@ -85,7 +85,7 @@ function checkDelAndGetPath (path) {
     delPath = current
   }
 
-  // case: Number(a); this._p(a); this.test(this._p(a))
+  // case: Number(a); this._p(a); this._p(wxs.test(a))
   while (current) {
     if (t.isBlockStatement(current)) break // block 即可退出循环
 
@@ -95,11 +95,9 @@ function checkDelAndGetPath (path) {
       if (args.length === 1) {
         const callee = parent.callee
         const name = callee.property && callee.property.name // 确认是否要考虑 this['_p'](a)
-        if (name === '_p') { // 收集props
-          isPros = true
-        }
-        propsPath = parent
+        if (name === '_p') isPros = true // 收集props
         current = current.parentPath
+        propsPath = current
         continue
       }
     }
@@ -111,9 +109,9 @@ function checkDelAndGetPath (path) {
 
     const { key, computed, node, container } = current
     if (
-      computed ||
-      key === 'property' ||
-      (node.computed && !t.isStringLiteral(node.property)) ||
+      computed || // a[b] => a
+      key === 'property' || // a[b] => b
+      (node.computed && !t.isStringLiteral(node.property)) || // a.b[c] => a.b TODO 注释补全
       t.isLogicalExpression(container) ||
       (t.isIfStatement(container) && key === 'test')
     ) return result({ canDel: false })
@@ -140,18 +138,21 @@ function checkDelAndGetPath (path) {
 
 function checkPrefix (keys, key) {
   if (keys.length === 0) return false
-
   const temp = key.split('.')
   if (temp.length === 1) return false
 
   keys.forEach(str => {
     if (key.startsWith(str)) return true
   })
-
   return false
 }
 
-function dealRemove (path) {
+function dealRemove (path, replace) {
+  if (replace) { // TODO 优化，看是否可以挪到下面
+    path.replaceWith(t.stringLiteral(''))
+    return
+  }
+
   while (path.key === 'expression' && t.isExpressionStatement(path.parentPath)) {
     path = path.parentPath
   }
@@ -176,33 +177,36 @@ module.exports = {
       ]
     })
 
-    const propKeys = []
-
-    // 纪录每个作用域下的变量，以便回溯删除
-    let blockTree = null
     let currentBlock = null
+    const bindingsMap = new Map()
+
+    const propKeys = []
 
     const collectVisitor = {
       Program: {
         enter (path) {
-          blockTree = new MultiNode(null, {
-            path,
-            bindings: {}
-          }, [])
+          bindingsMap.set(path, {
+            parent: null,
+            bindings: {},
+            pBindings: {}
+          })
           currentBlock = path
         }
       },
       BlockStatement: {
-        enter (path) {
-          const scope = blockTree.get(currentBlock)
-          scope.add(new MultiNode(currentBlock, {
-            path,
-            bindings: {}
-          }, []))
+        enter (path) { // 收集作用域下所有变量(keyPath)
+          const parent = bindingsMap.get(currentBlock)
+          bindingsMap.set(path, {
+            parent,
+            bindings: {},
+            pBindings: {}
+          })
           currentBlock = path
         },
         exit (path) {
-          const scope = blockTree.get(path)
+          const scope = bindingsMap.get(path)
+          const parentScope = bindingsMap.get(scope.parent)
+          scope.pBindings = Object.assign({}, parentScope.bindings, parentScope.pBindings)
           currentBlock = scope.parent
         }
       },
@@ -210,35 +214,35 @@ module.exports = {
         if (
           checkBindThis(path) &&
           !path.scope.hasBinding(path.node.name) &&
-          !ignoreMap[path.node.name]
+          !ignoreMap[path.node.name] &&
+          needCollect
         ) {
-          path.shouldBindThis = true
+          const { last, keyPath, hasDangerous } = calPropName(path)
+          path.keyPath = keyPath
+          last.collectPath = t.stringLiteral(keyPath)
 
-          if (needCollect) {
-            const { last, keyPath, hasDangerous } = calPropName(path)
-            path.keyPath = keyPath
-            path.lastPath = last
+          if (!renderReduce) return
 
-            if (!renderReduce) return
-
-            const { isPros, propsPath, ignore, canDel, delPath, replace } = checkDelAndGetPath(hasDangerous ? last.parentPath : last)
-            if (isPros) {
-              propKeys.push(path.node.name)
-              propsPath.replaceWith(propsPath.arguments[0])
-            }
-            if (ignore) return
-
-            path.canDel = canDel
-            path.deleteLastPath = delPath
-
-            const current = blockTree.get(currentBlock)
-            const target = current.data.bindings[keyPath] || []
-            target.push({
-              path,
-              canDel
-            })
-            current.data.bindings[keyPath] = target
+          const {isPros, propsPath, ignore, delPath, canDel, replace} = checkDelAndGetPath(hasDangerous ? last.parentPath : last)
+          if (isPros) {
+            propKeys.push(path.node.name)
+            propsPath.isPros = true
           }
+          if (ignore) return
+
+          delPath.delInfo = {
+            keyPath,
+            canDel,
+            replace
+          }
+
+          const { bindings } = bindingsMap.get(currentBlock)
+          const target = bindings[keyPath] || []
+          target.push({
+            path,
+            canDel
+          })
+          bindings[keyPath] = target
         }
       }
     }
@@ -246,57 +250,84 @@ module.exports = {
     const bindThisVisitor = {
       BlockStatement: {
         enter (path) {
-          const scope = blockTree.get(path)
-          const { data } = blockTree.get(scope.parent)
-          scope.data.pBindings = Object.assign({}, data.bindings, data.pBindings)
           currentBlock = path
         },
         exit (path) {
-          const scope = blockTree.get(path)
-          currentBlock = scope.parent
+          currentBlock = bindingsMap.get(path).parent
         }
       },
-      Identifier (path) {
-        if (path.shouldBindThis) {
-          delete path.shouldBindThis
+      enter (path) {
+        // replace _p
+        if (path.isPros) {
+          path.replaceWith(path.node.arguments[0])
+          delete path.isPros
+        }
 
-          if (path.keyPath) {
-            const keyPath = path.keyPath
-            const last = path.lastPath
-            last.collectPath = t.stringLiteral(keyPath)
+        // 删除重复变量
+        if (path.delInfo) {
+          const { keyPath, canDel, replace } = path.delInfo
+          delete path.delInfo
 
-            delete path.keyPath
-            delete path.lastPath
+          if (canDel) {
+            const data = bindingsMap.get(currentBlock)
+            const { bindings, pBindings } = data
+            const allBindings = Object.assign({}, pBindings, bindings)
 
-            if (path.canDel) {
-              delete path.canDel
-              const { data } = blockTree.get(currentBlock)
-              const { bindings, pBindings } = data
-              const allBindings = Object.assign({}, pBindings, bindings)
-
-              const doDelete = () => {
-                dealRemove(path.deleteLastPath)
-                delete path.deleteLastPath
-              }
-
-              if (checkPrefix(Object.keys(allBindings), keyPath) || pBindings[keyPath]) {
-                doDelete()
-                return
-              } else {
-                const currentBlockVars = bindings[keyPath]
-                if (currentBlockVars.length > 1) {
-                  const index = currentBlockVars.findIndex(item => !item.canDel)
-                  if (index !== -1 || currentBlockVars[0].path !== path) { // 当前block中存在不可删除的变量或不是第一个可删除变量，即可删除该变量
-                    doDelete()
-                    return
-                  }
+            // 优先判断前缀，在判断全等
+            if (checkPrefix(Object.keys(allBindings), keyPath) || pBindings[keyPath]) {
+              dealRemove(path)
+              return
+            } else {
+              const currentBlockVars = bindings[keyPath]
+              if (currentBlockVars.length > 1) {
+                const index = currentBlockVars.findIndex(item => !item.canDel)
+                if (index !== -1 || currentBlockVars[0].path !== path) { // 当前block中存在不可删除的变量 || 不是第一个可删除变量，即可删除该变量
+                  dealRemove(path)
+                  return
                 }
               }
             }
-
-            // bind this
-            path.replaceWith(t.memberExpression(t.thisExpression(), path.node))
           }
+        }
+
+        if (path.keyPath) {
+          // bind this
+          path.replaceWith(t.memberExpression(t.thisExpression(), path.node))
+        }
+      },
+      Identifier (path) {
+        return
+        if (path.keyPath) {
+          const keyPath = path.keyPath
+
+          delete path.keyPath
+
+          if (path.canDel || path.replace) {
+            delete path.canDel
+            const { data } = blockTree.get(currentBlock)
+            const { bindings, pBindings } = data
+            const allBindings = Object.assign({}, pBindings, bindings)
+
+            const doDelete = () => {
+              dealRemove(path.deleteLastPath, path.replace)
+              delete path.deleteLastPath
+            }
+
+            if (checkPrefix(Object.keys(allBindings), keyPath) || pBindings[keyPath]) {
+              doDelete()
+              return
+            } else {
+              const currentBlockVars = bindings[keyPath]
+              if (currentBlockVars.length > 1) {
+                const index = currentBlockVars.findIndex(item => !item.canDel)
+                if (index !== -1 || currentBlockVars[0].path !== path) { // 当前block中存在不可删除的变量或不是第一个可删除变量，即可删除该变量
+                  doDelete()
+                  return
+                }
+              }
+            }
+          }
+
         }
       },
       MemberExpression: {
@@ -311,8 +342,6 @@ module.exports = {
 
     traverse(ast, collectVisitor)
     traverse(ast, bindThisVisitor)
-
-    clearCache()
 
     return {
       code: generate(ast).code,
