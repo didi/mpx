860c48d1a8ad650da00d840c2188ca2c77121f98
yandadaFreedom <525966780@qq.com>
2023-06-30 18:46:33 +0800

    feat: ssr阶段屏蔽pageScrollTo

--- packages/api-proxy/src/web/api/page-scroll-to/index.js
+++ packages/api-proxy/src/web/api/page-scroll-to/index.js
@@ -1,20 +1,22 @@
-import { webHandleSuccess, webHandleFail } from '../../../common/js'
+import { webHandleSuccess, webHandleFail, isBrowser } from '../../../common/js'
 import { nextTick } from '../next-tick'
 
 export function pageScrollTo (options) {
-  nextTick(() => {
-    const ms = global.__ms
-    const { success, fail, complete } = options
+  if (isBrowser) {
+    nextTick(() => {
+      const ms = global.__ms
+      const { success, fail, complete } = options
 
-    if (!ms) {
-      return webHandleFail({
-        errMsg: 'pageScrollTo:fail'
-      }, fail, complete)
-    }
+      if (!ms) {
+        return webHandleFail({
+          errMsg: 'pageScrollTo:fail'
+        }, fail, complete)
+      }
 
-    ms.pageScrollTo(options)
-    webHandleSuccess({
-      errMsg: 'pageScrollTo:ok'
-    }, success, complete)
-  })
+      ms.pageScrollTo(options)
+      webHandleSuccess({
+        errMsg: 'pageScrollTo:ok'
+      }, success, complete)
+    })
+  }
 }
