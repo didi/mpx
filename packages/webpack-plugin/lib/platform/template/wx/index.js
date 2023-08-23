e76ee5a8c77e88d384ebfa994825309ec9c43c97
anotherso1a <1181581742@qq.com>
2023-08-23 19:27:39 +0800

    improvement(web): improve web style attr's parsing speed

--- packages/webpack-plugin/lib/platform/template/wx/index.js
+++ packages/webpack-plugin/lib/platform/template/wx/index.js
@@ -190,16 +190,16 @@ module.exports = function getSpec ({ warn, error }) {
           }
           const styleBinding = []
           el.isStyleParsed = true
-          el.attrsList.forEach((item) => {
+          // 不过滤的话每一个属性都要 parse
+          el.attrsList.filter(item => /^(style|wx:style)$/.test(item.name)).forEach((item) => {
             const parsed = parseMustache(item.value)
-            if (item.name === 'style') {
-              if (parsed.hasBinding || parsed.result.indexOf('rpx') > -1) {
-                styleBinding.push(parsed.result)
-              } else {
-                styleBinding.push(JSON.stringify(item.value))
-              }
-            } else if (item.name === 'wx:style') {
+            if (item.name === 'wx:style') {
               styleBinding.push(parsed.result)
+            // item.name === 'style'
+            } else if (parsed.hasBinding || parsed.result.indexOf('rpx') > -1) {
+              styleBinding.push(parsed.result)
+            } else {
+              styleBinding.push(JSON.stringify(item.value))
             }
           })
           return {
