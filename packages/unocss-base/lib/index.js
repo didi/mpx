e57f0638cfc1af0c07d18a6c61bdab3b8764a642
pagnkelly <632228198@qq.com>
2023-09-11 11:30:03 +0800

    feat: base 替换remtorpx方式

--- packages/unocss-base/lib/index.js
+++ packages/unocss-base/lib/index.js
@@ -1,14 +1,19 @@
 const { presetUno } = require('@unocss/preset-uno')
-const theme = require('./theme')
 
-module.exports = function presetMpx (options = { preflight: false }) {
+const remRE = /(-?[\.\d]+)rem/g
+
+module.exports = function presetMpx (options = { preflight: false, baseFontSize: 37.5 }) {
   const uno = presetUno(options)
+  const { baseFontSize } = options
   return {
     ...uno,
     name: '@mpxjs/unocss-base',
-    theme: {
-      ...uno.theme,
-      ...theme
+    postprocess: (util) => {
+      util.entries.forEach((i) => {
+        const value = i[1]
+        if (typeof value === 'string' && remRE.test(value))
+          i[1] = value.replace(remRE, (_, p1) => `${p1 * baseFontSize}rpx`)
+      })
     }
   }
 }
