a0f19c4fd59c491d6c28985e0f82a33cd3775195
yandadaFreedom <525966780@qq.com>
2023-09-28 14:51:19 +0800

    chore: 删除组件名称校验

--- packages/webpack-plugin/lib/utils/check-component-name.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (name) {
-  const componentsNameList = ['button', 'checkbox', 'checkbox-group', 'form', 'icon', 'image', 'input', 'movable-are', 'movable-view',
-    'navigator', 'picker', 'picker-view', 'picker-view-column', 'progress', 'radio', 'radio-group', 'rich-text', 'scroll-view',
-    'slider', 'swiper', 'swiper-item', 'switch', 'text', 'textarea', 'video', 'view', 'web-view'
-  ]
-  return componentsNameList.includes(name)
-}
