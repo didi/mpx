5585009a6d898a995147b6b02011069e78434337
xuegan <xuegan@didiglobal.com>
2023-10-20 12:59:12 +0800

    fix: 修复输出ali/web root-view srcMode判断问题

--- packages/webpack-plugin/lib/template-compiler/compiler.js
+++ packages/webpack-plugin/lib/template-compiler/compiler.js
@@ -1795,7 +1795,7 @@ function processRootViewStyleClassHack (el, options, root) {
 
 // 有virtualHost情况wx组件注入virtualHost。无virtualHost阿里组件注入root-view。其他跳过。
 function getVirtualHostRoot (options, meta) {
-  if (options.isComponent) {
+  if (srcMode === 'wx' && options.isComponent) {
     // 处理组件时
     if (mode === 'wx' && options.hasVirtualHost) {
       // wx组件注入virtualHost配置
