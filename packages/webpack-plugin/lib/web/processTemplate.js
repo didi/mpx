f84b6cb22b4f752c05b9bd298b5e0989aa270c71
yandadaFreedom <525966780@qq.com>
2023-10-27 15:23:41 +0800

    chore: 修改web容器dom节点

--- packages/webpack-plugin/lib/web/processTemplate.js
+++ packages/webpack-plugin/lib/web/processTemplate.js
@@ -53,7 +53,7 @@ module.exports = function (template, {
     const idName = el?.match(/#(.*)/)?.[1] || 'app'
     template = {
       tag: 'template',
-      content: `<page id="${idName}"><mpx-keep-alive><router-view></router-view></mpx-keep-alive></page>`
+      content: `<div id="${idName}" class="app"><mpx-keep-alive><router-view></router-view></mpx-keep-alive></div>`
     }
     builtInComponentsMap['mpx-keep-alive'] = {
       resource: addQuery('@mpxjs/webpack-plugin/lib/runtime/components/web/mpx-keep-alive.vue', { isComponent: true })
