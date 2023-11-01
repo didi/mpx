c35ac8843295df63f32b601f15fd62ff7643aa00
Luobinf <1096422441@qq.com>
2023-11-01 09:52:05 +0800

    feat(reactivity): add onScopeDispose、getCurrentScope && tests

--- packages/reactivity/__tests__/effectScope.test.js
+++ packages/reactivity/__tests__/effectScope.test.js
@@ -1,6 +1,6 @@
 import { computed } from '../src/computed'
 import { effect, ref } from '../src'
-import { effectScope, EffectScope } from '../src/effectScope'
+import { onScopeDispose, EffectScope, getCurrentScope } from '../src/effectScope'
 import { reactive } from '../src/reactive'
 
 describe('reactivity/effectScope', () => {
@@ -64,5 +64,215 @@ describe('reactivity/effectScope', () => {
     counter.num = 6
     expect(dummy).toBe(7)
     expect(doubled).toBe(14)
+  })
+
+  it('should collect nested scope', () => {
+    let dummy, doubled
+    const counter = reactive({ num: 0 })
+
+    const scope = new EffectScope()
+    scope.run(() => {
+      effect(() => (dummy = counter.num))
+      // nested scope
+      new EffectScope().run(() => {
+        effect(() => (doubled = counter.num * 2))
+      })
+    })
+
+    expect(scope.effects.length).toBe(1)
+    // 用于记录嵌套作用域
+    expect(scope.scopes.length).toBe(1)
+    expect(scope.scopes[0]).toBeInstanceOf(EffectScope)
+
+    expect(dummy).toBe(0)
+    counter.num = 7
+    expect(dummy).toBe(7)
+    expect(doubled).toBe(14)
+
+    // stop the nested scope as well
+    scope.stop()
+    expect(scope.scopes.length).toBe(1)
+
+    counter.num = 6
+    expect(dummy).toBe(7)
+    expect(doubled).toBe(14)
+  })
+
+  it('nested scope can be escaped', () => {
+    let dummy, doubled
+    const counter = reactive({ num: 0 })
+
+    const scope = new EffectScope()
+    scope.run(() => {
+      effect(() => (dummy = counter.num))
+      // nested scope
+      new EffectScope(true).run(() => {
+        effect(() => (doubled = counter.num * 2))
+      })
+    })
+
+    expect(scope.effects.length).toBe(1)
+
+    expect(dummy).toBe(0)
+    counter.num = 7
+    expect(dummy).toBe(7)
+    expect(doubled).toBe(14)
+
+    scope.stop()
+
+    counter.num = 6
+    expect(dummy).toBe(7)
+
+    // nested scope should not be stopped
+    expect(doubled).toBe(12)
+  })
+
+  it('able to run the scope', () => {
+    let dummy, doubled
+    const counter = reactive({ num: 0 })
+
+    const scope = new EffectScope()
+    scope.run(() => {
+      effect(() => (dummy = counter.num))
+    })
+
+    expect(scope.effects.length).toBe(1)
+
+    scope.run(() => {
+      effect(() => (doubled = counter.num * 2))
+    })
+
+    expect(scope.effects.length).toBe(2)
+
+    counter.num = 7
+    expect(dummy).toBe(7)
+    expect(doubled).toBe(14)
+
+    scope.stop()
+  })
+
+  it('can not run an inactive scope', () => {
+    let dummy, doubled
+    const counter = reactive({ num: 0 })
+
+    const scope = new EffectScope()
+    scope.run(() => {
+      effect(() => (dummy = counter.num))
+    })
+
+    expect(scope.effects.length).toBe(1)
+
+    scope.stop()
+
+    scope.run(() => {
+      effect(() => (doubled = counter.num * 2))
+    })
+
+    expect('cannot run an inactive effect scope.').toHaveBeenWarned()
+
+    expect(scope.effects.length).toBe(1)
+
+    counter.num = 7
+    expect(dummy).toBe(0)
+    expect(doubled).toBe(undefined)
+  })
+
+  it('should fire onScopeDispose hook', () => {
+    let dummy = 0
+
+    const scope = new EffectScope()
+    scope.run(() => {
+      onScopeDispose(() => (dummy += 1))
+      onScopeDispose(() => (dummy += 2))
+    })
+
+    scope.run(() => {
+      onScopeDispose(() => (dummy += 4))
+    })
+
+    expect(dummy).toBe(0)
+
+    scope.stop()
+    expect(dummy).toBe(7)
+  })
+
+  it('should warn onScopeDispose() is called when there is no active effect scope', () => {
+    const spy = jest.fn()
+    const scope = new EffectScope()
+    scope.run(() => {
+      onScopeDispose(spy)
+    })
+
+    expect(spy).toHaveBeenCalledTimes(0)
+
+    onScopeDispose(spy)
+
+    expect(
+      'onScopeDispose() is called when there is no active effect scope to be associated with.'
+    ).toHaveBeenWarned()
+
+    scope.stop()
+    expect(spy).toHaveBeenCalledTimes(1)
+  })
+
+  it('should dereference child scope from parent scope after stopping child scope (no memleaks)', () => {
+    const parent = new EffectScope()
+    const child = parent.run(() => new EffectScope())
+    expect(parent.scopes.includes(child)).toBe(true)
+    child.stop()
+    expect(parent.scopes.includes(child)).toBe(false)
+  })
+
+  it('test with higher level APIs', async () => {
+    const r = ref(1)
+
+    const computedSpy = jest.fn()
+
+    let c
+    const scope = new EffectScope()
+    scope.run(() => {
+      c = computed(() => {
+        computedSpy()
+        return r.value + 1
+      })
+    })
+
+    c.value // computed is lazy so trigger collection
+    expect(computedSpy).toHaveBeenCalledTimes(1)
+
+    r.value++
+    c.value
+    expect(computedSpy).toHaveBeenCalledTimes(2)
+
+    scope.stop()
+
+    r.value++
+    c.value
+    // should not trigger anymore
+    expect(computedSpy).toHaveBeenCalledTimes(2)
+  })
+
+  it('getCurrentScope() stays valid when running a detached nested EffectScope', () => {
+    const parentScope = new EffectScope()
+
+    parentScope.run(() => {
+      const currentScope = getCurrentScope()
+      expect(currentScope).toBeDefined()
+      const detachedScope = new EffectScope(true)
+      detachedScope.run(() => {})
+
+      expect(getCurrentScope()).toBe(currentScope)
+    })
+  })
+
+  it('calling .off() of a detached scope inside an active scope should not break currentScope', () => {
+    const parentScope = new EffectScope()
+
+    parentScope.run(() => {
+      const childScope = new EffectScope(true)
+      childScope.on()
+      childScope.off()
+      expect(getCurrentScope()).toBe(parentScope)
+    })
   })
 })
--- packages/reactivity/src/effectScope.js
+++ packages/reactivity/src/effectScope.js
@@ -1,10 +1,35 @@
 let activeEffectScope
 
 export class EffectScope {
-  active = true
-  effects = []
-  constructor (detached) {
+  active = true;
+  effects = [];
+  cleanups = []
+  /**
+   * only assigned by undetached scope
+   * @internal
+   */
+  parent;
+  /**
+   * record undetached scopes
+   * @internal
+   */
+  scopes;
+  /**
+   * track a child scope's index in its parent's scopes array for optimized
+   * removal
+   * @internal
+   */
+  index;
+  // eslint-disable-next-line no-useless-constructor
+  constructor (detached = false) {
     this.detached = detached
+    this.parent = activeEffectScope
+    if (!detached && activeEffectScope) {
+      this.index =
+        (activeEffectScope.scopes || (activeEffectScope.scopes = [])).push(
+          this
+        ) - 1
+    }
   }
 
   run (fn) {
@@ -21,27 +46,90 @@ export class EffectScope {
     }
   }
 
-  stop () {
+  /**
+   * This should only be called on non-detached scopes
+   * @internal
+   */
+  on () {
+    activeEffectScope = this
+  }
+
+  /**
+    * This should only be called on non-detached scopes
+    * @internal
+   */
+  off () {
+    activeEffectScope = this.parent
+  }
+
+  stop (fromParent) {
     if (this.active) {
       let i, l
       for (i = 0, l = this.effects.length; i < l; i++) {
         this.effects[i].stop()
       }
-
+      if (this.scopes) {
+        for (i = 0, l = this.scopes.length; i < l; i++) {
+          this.scopes[i].stop(true)
+        }
+      }
+      for (i = 0, l = this.cleanups.length; i < l; i++) {
+        this.cleanups[i]()
+      }
+      // nested scope, should dereference child scope from parent scope after stopping child scope
+      if (!this.detached && this.parent && !fromParent) {
+        // optimized O(1) removal
+        if (this.parent.scopes) {
+          const last = this.parent.scopes.pop()
+          if (last && last !== this) {
+            this.parent.scopes[this.index] = last
+            last.index = this.index
+          }
+        }
+      }
+      this.parent = undefined
       this.active = false
     }
   }
 }
 
+/**
+ * Creates an effect scope object which can capture the reactive effects (i.e.
+ * computed and watchers) created within it so that these effects can be
+ * disposed together.
+ *
+ * @param detached - Can be used to create a "detached" effect scope.
+ */
 export function effectScope (detached) {
   return new EffectScope(detached)
 }
 
-export function recordEffectScope (
-  effect,
-  scope = activeEffectScope
-) {
+export function recordEffectScope (effect, scope = activeEffectScope) {
   if (scope && scope.active) {
     scope.effects.push(effect)
+  }
+}
+
+/**
+ * Returns the current active effect scope if there is one.
+ */
+export function getCurrentScope () {
+  return activeEffectScope
+}
+
+/**
+ * Registers a dispose callback on the current active effect scope. The
+ * callback will be invoked when the associated effect scope is stopped.
+ *
+ * @param fn - The callback function to attach to the scope's cleanup.
+ */
+export function onScopeDispose (fn) {
+  if (activeEffectScope) {
+    activeEffectScope.cleanups.push(fn)
+  } else if (__DEV__) {
+    console.warn(
+      'onScopeDispose() is called when there is no active effect scope' +
+        ' to be associated with.'
+    )
   }
 }
