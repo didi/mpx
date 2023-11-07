2ce4a7c8c69da7bcb912fc44865f2f4da789fbeb
wangxiaokou <w-cuijuan@163.com>
2023-11-07 17:35:14 +0800

    补充custom逻辑

--- packages/api-proxy/src/install.js
+++ packages/api-proxy/src/install.js
@@ -1,15 +1,18 @@
 import * as platformApi from './platform'
-import { getEnvObj } from './common/js'
+import { getEnvObj, getEnvStr } from './common/js'
 import promisify from './common/js/promisify'
 
 export default function install (target, options = {}) {
   const {
     usePromise = false, // 是否转为 promise 格式
     whiteList = [], // 强制变成 promise 格式的 api
-    blackList = [] // 强制不变成 promise 格式的 api
+    blackList = [], // 强制不变成 promise 格式的 api
+    custom = {}, // 自定义转化规则
+    fallbackMap = {} // 对于不支持的API，允许配置一个映射表，接管不存在的API
   } = options
 
   let transedApi = {}
+  const envStr = getEnvStr()
 
   if (__mpx_env__ === 'web') {
     transedApi = platformApi
@@ -35,4 +38,19 @@ export default function install (target, options = {}) {
     } catch (e) {
     } // 支付宝不支持重写 call 方法
   })
+  if (custom[envStr]) {
+    Object.keys(custom[envStr])
+      .forEach(k => {
+        target[k] = (...args) => {
+          return custom[envStr][k].apply(target, args)
+        }
+      })
+  }
+  // Fallback Map option
+  Object.keys(fallbackMap)
+    .forEach(k => {
+      if (!target[k]) {
+        target[k] = fallbackMap[k]
+      }
+    })
 }
