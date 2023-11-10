32a63f598b37da207542f03274cf8a8941a42e8e
anotherso1a <1181581742@qq.com>
2023-11-10 17:12:30 +0800

    revert: revert getCurrentVueInstance

--- packages/core/src/platform/patch/web/getDefaultOptions.js
+++ packages/core/src/platform/patch/web/getDefaultOptions.js
@@ -41,7 +41,7 @@ export function getDefaultOptions (type, { rawOptions = {} }) {
   const rawSetup = rawOptions.setup
   if (rawSetup) {
     rawOptions.setup = (props) => {
-      const instance = getCurrentVueInstance()
+      const instance = getCurrentVueInstance().proxy
       initProxy(instance, rawOptions)
       setCurrentInstance(instance.__mpxProxy)
       const newContext = {
