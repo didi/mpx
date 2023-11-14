24707ce087519302f4bfc1e6a9badaefd447a1ba
anotherso1a <1181581742@qq.com>
2023-11-14 19:55:04 +0800

    feat: add preview image api

---
+++ packages/api-proxy/src/web/api/preview-image/index.js
@@ -0,0 +1,12 @@
+import Preview from "./Preview"
+
+let preview = null
+
+/**
+ * 预览图片
+ * @param {Object} options - 预览图片的配置项
+ */
+export const previewImage = (options) => {
+  if (!preview) preview = new Preview()
+  preview.show(options)
+}
