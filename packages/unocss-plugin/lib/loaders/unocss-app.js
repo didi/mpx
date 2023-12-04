4b6f9fe3a019ea6c83a0b6e5139c17180286c858
xuegan <xuegan@didiglobal.com>
2023-12-04 19:16:40 +0800

    fix: 修复unocss-plugin app-loader 在ts-watch模式下的报错问题

--- packages/unocss-plugin/lib/loaders/unocss-app.js
+++ packages/unocss-plugin/lib/loaders/unocss-app.js
@@ -1,4 +1,11 @@
+const tsWatchRunLoaderFilter = require('@mpxjs/webpack-plugin/lib/utils/ts-loader-watch-run-loader-filter')
+const path = require('path')
 module.exports = function (source) {
+  const pathExtname = path.extname(this.resourcePath)
+  if (!['.vue', '.mpx'].includes(pathExtname)) {
+    this.loaderIndex = tsWatchRunLoaderFilter(this.loaders, this.loaderIndex)
+    return source
+  }
   // 处理app引入
   return `
 ${source}
