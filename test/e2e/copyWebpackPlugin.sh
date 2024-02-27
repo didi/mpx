4de328a4b54f9ac7405020ead3874a1b3357f82a
xuegan <xuegan@didiglobal.com>
2024-02-27 17:38:46 +0800

    fix: 调整验证copyPlugin.sh脚本中的copy packages 文件夹逻辑，对packages中

--- test/e2e/copyWebpackPlugin.sh
+++ test/e2e/copyWebpackPlugin.sh
@@ -1,14 +1,8 @@
 echo "start copy webpackPlugin"
 
-rm -rf ./node_modules/@mpxjs/webpack-plugin/lib ./node_modules/@mpxjs/utils/src ./node_modules/@mpxjs/store/src ./node_modules/@mpxjs/pinia/src ./node_modules/@mpxjs/core/src ./node_modules/@mpxjs/fetch/src ./node_modules/@mpxjs/api-proxy/src
+#rm -rf ./node_modules/@mpxjs/webpack-plugin/lib ./node_modules/@mpxjs/utils/src ./node_modules/@mpxjs/store/src ./node_modules/@mpxjs/pinia/src ./node_modules/@mpxjs/core/src ./node_modules/@mpxjs/fetch/src ./node_modules/@mpxjs/api-proxy/src
 
 
-cp -r ../../../packages/webpack-plugin/lib/ ./node_modules/@mpxjs/webpack-plugin/lib/
-cp -r ../../../packages/utils/src/ ./node_modules/@mpxjs/utils/src/
-cp -r ../../../packages/store/src/ ./node_modules/@mpxjs/store/src/
-cp -r ../../../packages/pinia/src/ ./node_modules/@mpxjs/pinia/src/
-cp -r ../../../packages/core/src/ ./node_modules/@mpxjs/core/src/
-cp -r ../../../packages/fetch/src/ ./node_modules/@mpxjs/fetch/src/
-cp -r ../../../packages/api-proxy/src/ ./node_modules/@mpxjs/api-proxy/src/
+cp -r ../../../packages/* ./node_modules/@mpxjs/
 
 echo "end copy webpackPlugin"
