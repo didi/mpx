272be0a8c670cdad9b1c8ba51fb40c88d90bbe26
xuegan <xuegan@didiglobal.com>
2024-02-29 15:10:37 +0800

    fix: 调整e2e测试脚本

--- test/e2e/copyWebpackPlugin.sh
+++ test/e2e/copyWebpackPlugin.sh
@@ -1,8 +1,5 @@
 echo "start copy webpackPlugin"
 
-#rm -rf ./node_modules/@mpxjs/webpack-plugin/lib ./node_modules/@mpxjs/utils/src ./node_modules/@mpxjs/store/src ./node_modules/@mpxjs/pinia/src ./node_modules/@mpxjs/core/src ./node_modules/@mpxjs/fetch/src ./node_modules/@mpxjs/api-proxy/src
-
-
 cp -r ../../../packages/* ./node_modules/@mpxjs/
 
 echo "end copy webpackPlugin"
