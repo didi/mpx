3d0370d07c8716b735acd94df582eec4c46274ab
wangxiaokou <w-cuijuan@163.com>
2024-03-15 17:41:44 +0800

    index.js不分引用ENV_OBJ改动

--- packages/api-proxy/src/platform/api/canvas/index.js
+++ packages/api-proxy/src/platform/api/canvas/index.js
@@ -1,6 +1,4 @@
-import { getEnvObj, envError } from '../../../common/js'
-
-const ENV_OBJ = getEnvObj()
+import { ENV_OBJ, envError } from '../../../common/js'
 
 const createCanvasContext = ENV_OBJ.createCanvasContext || envError('createCanvasContext')
 
