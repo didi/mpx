0c5d041f921bc749c7f7600a22e409201dde29be
xuegan <xuegan@didiglobal.com>
2024-03-22 12:00:53 +0800

    fix:  中数据包含data和props

--- packages/core/src/platform/builtInMixins/setDataMixin.web.js
+++ packages/core/src/platform/builtInMixins/setDataMixin.web.js
@@ -5,24 +5,10 @@ export default function setDataMixin () {
     beforeCreate () {
       Object.defineProperty(this, 'data', {
         get () {
-          return this.$data
+          return Object.assign({}, this.$data, this.$props)
         },
         configurable: true
       })
-    },
-    methods: {
-      setData (data, callback) {
-        if (!isObject(data)) {
-          error(`The data entry type of the setData method must be object, The type of data ${data} is incorrect`)
-          return
-        }
-        Object.keys(data).forEach(key => {
-          this[key] = data[key]
-        })
-        if (callback && isFunction(callback)) {
-          this.$nextTick(callback.bind(this))
-        }
-      }
     }
   }
 }
