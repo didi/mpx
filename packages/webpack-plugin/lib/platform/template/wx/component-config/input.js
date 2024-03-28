8a377b4e5c2caabbb79bd6b8c6b4dad66bf5bf20
xuegan <xuegan@didiglobal.com>
2024-03-28 15:08:20 +0800

    fix lint error

--- packages/webpack-plugin/lib/platform/template/wx/component-config/input.js
+++ packages/webpack-plugin/lib/platform/template/wx/component-config/input.js
@@ -5,7 +5,6 @@ module.exports = function ({ print }) {
   const aliEventLog = print({ platform: 'ali', tag: TAG_NAME, isError: false, type: 'event' })
   const baiduPropLog = print({ platform: 'baidu', tag: TAG_NAME, isError: false })
   const baiduEventLog = print({ platform: 'baidu', tag: TAG_NAME, isError: false, type: 'event' })
-  const ttPropLog = print({ platform: 'bytedance', tag: TAG_NAME, isError: false })
   const ttEventLog = print({ platform: 'bytedance', tag: TAG_NAME, isError: false, type: 'event' })
   const jdPropLog = print({ platform: 'jd', tag: TAG_NAME, isError: false })
   const jdEventLog = print({ platform: 'jd', tag: TAG_NAME, isError: false, type: 'event' })
