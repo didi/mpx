468ddbec9c4041c4bfc5db77911095569a61cfe5
xiaolei <1017653702@qq.com>
2024-04-09 11:32:08 +0800

    [optimize]暂时去掉 mpxAttrs 大对象属性，目前使用不到

--- packages/webpack-plugin/lib/runtime-render/base-wxml.js
+++ packages/webpack-plugin/lib/runtime-render/base-wxml.js
@@ -25,7 +25,7 @@ function setCustomEle (el, options, meta) {
   if (tag && !eleAttrsMap[tag]) {
     eleAttrsMap[tag] = {}
     if (el.dynamic) {
-      attrKeys.push('slots', 'mpxAttrs')
+      attrKeys.push('slots')
     }
   }
   Object.assign(eleAttrsMap[tag], makeAttrsMap(attrKeys))
