249a82bab81c7edc1a5320fd5e922b023a71ff47
huajingwen@didiglobal.com <huajingwen@didiglobal.com>
2024-04-17 19:32:20 +0800

    feat: add style rules

--- packages/webpack-plugin/lib/platform/style/wx/index.js
+++ packages/webpack-plugin/lib/platform/style/wx/index.js
@@ -7,9 +7,62 @@ module.exports = function getSpec ({ warn, error }) {
     if (type === 'prop') { // css pro 不支持
       content = `CSS property ${prop} is not supported in ${platform} environment!`
     } else if (type === 'value' && unsupportedMap[prop]?.includes(value)) { // css value 不支持
-      content = `CSS property ${prop} does not support [${value}] value in ${platform} environment!`
+      content = `CSS property ${prop} does not support ['${value}'] value in ${platform} environment!`
     }
     isError ? error({ prop, content }) : warn({ prop, content })
+  }
+  
+  // 简写格式化
+  const textShadowMap = { // 仅支持 offset-x | offset-y | blur-radius | color 排序
+    textShadowOffset: ['width','height'],
+    textShadowRadius: 0,
+    textShadowColor: 0
+  }
+  
+  const borderMap = {  // 仅支持 width | style | color 这种排序
+    borderWidth: 0,
+    borderStyle: 0,
+    borderColor: 0
+  }
+  
+  const formatAbbreviation = ({ prop, value, keyMap }) => {
+    const values = value.trim().split(/\s+/)
+    let idx = 0
+    let keyIdx = 0
+    const cssMap = []
+    const props = Object.keys(keyMap)
+    let curProp
+    while (idx < values.length) { // 按值的个数循环赋值
+      curProp = props[keyIdx]
+      // 多个值的属性
+      if (Array.isArray(keyMap[curProp])) {
+        const curVal = keyMap[curProp]
+        if (values.length - idx < curVal.length) {
+          error({ curProp, content: `RN prop ${curProp} need ${curVal.length} value, but value only remain ${values.length - idx}` })
+          return cssMap.length ? cssMap : { prop, value }
+        }
+        const newVal = {}
+        curVal.forEach(subkey => { // Todo hjw 简写内不支持的 value 的校验提示
+          newVal[subkey] = values[idx]
+          idx += 1
+        })
+        cssMap.push({
+          prop: curProp,
+          value: newVal
+        })
+        keyIdx = idx - curVal.length + 1
+      }
+      // 单个值的属性
+      if (!keyMap[curProp]){
+        cssMap.push({
+          prop: curProp,
+          value: values[idx]
+        })
+        idx += 1
+        keyIdx += 1
+      }
+    }
+    return cssMap
   }
   
   const spec = {
@@ -20,38 +73,15 @@ module.exports = function getSpec ({ warn, error }) {
       {
         test: 'text-shadow',
         react ({ prop, value }) { // 仅支持 offset-x | offset-y | blur-radius | color 这种排序
-          console.log('css text-shadow', prop, value, 99)
-          const newValue = value.split(' ')
-          const newProp = ['textShadowOffset','textShadowRadius','textShadowColor']
-          return [
-            {
-              prop: newProp[0],
-              value: {
-                width: newValue[0],
-                height: newValue[1],
-              }
-            },
-            {
-              prop: newProp[1],
-              value: newValue[2],
-            },
-            {
-              prop: newProp[2],
-              value: newValue[3],
-            }
-          ]
+          return formatAbbreviation({ prop, value, keyMap: textShadowMap })
         }
       },
-      // {
-      //   test: 'pointer-events',
-      //   react ({ prop, value }) {
-      //     console.log('cdd background-color', prop, value, 999)
-      //     return {
-      //       prop,
-      //       value: '#000'
-      //     }
-      //   }
-      // },
+      {
+        test: 'border',
+        react ({ prop, value }) { // 仅支持 width | style | color 这种排序
+          return formatAbbreviation({ prop, value, keyMap: borderMap })
+        }
+      }
     ]
   }
   return spec
--- packages/webpack-plugin/lib/platform/style/wx/unsupported.js
+++ packages/webpack-plugin/lib/platform/style/wx/unsupported.js
@@ -2,7 +2,8 @@
 const UNSUPPORTED_PROP_ARR = ['box-sizing']
 // react CSS property 有不支持的 value 集合
 const UNSUPPORTED_PROP_VAL_ARR = {
-  'overflow': ['clip', 'auto']
+  'overflow': ['clip', 'auto'],
+  'border-style': ['none', 'hidden', 'double', 'groove', 'ridge', 'inset', 'outset']
 }
 
 
@@ -12,15 +13,15 @@ const UNSUPPORTED_PROP_VAL_ARR = {
  * @return {array}
  */
 module.exports = function ({ print }) {
-
+  
   const UnsupportedPropError = print({ platform: 'react', isError: true, type: 'prop' })
   const unsupportedPropExp = new RegExp('^(' + UNSUPPORTED_PROP_ARR.join('|') + ')$')
-
+  
   const unsupportedPropValExp = new RegExp('^(' + Object.keys(UNSUPPORTED_PROP_VAL_ARR).join('|') + ')$')
   const UnsupportedPropValError = print({ platform: 'react', isError: true, type: 'value', unsupportedMap: UNSUPPORTED_PROP_VAL_ARR })
-
-
-
+  
+  
+  
   return [
     {
       test: unsupportedPropExp,
