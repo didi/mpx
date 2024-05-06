208f8ad7942d425f2088e1d24504c1c49b7c6a4a
huajingwen@didiglobal.com <huajingwen@didiglobal.com>
2024-05-06 16:10:33 +0800

    feat: add flex direaction

--- packages/webpack-plugin/lib/react/style-helper.js
+++ packages/webpack-plugin/lib/react/style-helper.js
@@ -39,12 +39,16 @@ function getClassMap ({ content, filename, mode, srcMode }) {
   })
   root.walkRules(rule => {
     const classMapValue = {}
+    let isFlex = false, hasFlexDirection = false
     rule.walkDecls(({ prop, value }) => {
+      // console.log(isFlex, hasFlexDirection)
       let newData = rulesRunner({ prop, value })
       if (!newData.length) {
         newData = [newData]
       }
       newData.forEach(item => {
+        if (value === 'flex') isFlex = true
+        if (prop === 'flex-direction') hasFlexDirection = true
         prop = dash2hump(item.prop)
         value = item.value
         if (typeof item.value === 'object') {
@@ -56,6 +60,8 @@ function getClassMap ({ content, filename, mode, srcMode }) {
         }
         classMapValue[prop] = value
       })
+      // 定义flex布局且未定义方向时设置默认row
+      if (isFlex && !hasFlexDirection) classMapValue['flexDirection'] = formatValue('row')
     })
 
     const classMapKeys = []
