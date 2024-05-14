9b8dd58bb486731e43c300f5f4e5291ce4dc4333
xuegan <xuegan@didiglobal.com>
2024-05-14 15:01:30 +0800

    feat: 合并feat-react分支

--- packages/core/src/platform/patch/react/getDefaultOptions.ios.js
+++ packages/core/src/platform/patch/react/getDefaultOptions.ios.js
@@ -1,11 +1,12 @@
 import { useEffect, useSyncExternalStore, useRef, createElement, memo } from 'react'
 import * as reactNative from 'react-native'
 import { ReactiveEffect } from '../../../observer/effect'
-import { hasOwn, isFunction, noop, isObject, getByPath } from '@mpxjs/utils'
+import { set } from '../../../observer/reactive'
+import { hasOwn, isFunction, noop, isObject, error, getByPath } from '@mpxjs/utils'
 import MpxProxy from '../../../core/proxy'
 import { BEFOREUPDATE, UPDATED } from '../../../core/innerLifecycle'
 import mergeOptions from '../../../core/mergeOptions'
-import { queueJob } from '../../../observer/scheduler'
+import { queueJob, nextTick } from '../../../observer/scheduler'
 
 function getNativeComponent (tagName) {
   return getByPath(reactNative, tagName)
@@ -31,9 +32,29 @@ function createEffect (proxy, components) {
   }, () => queueJob(update), proxy.scope)
 }
 
+const datasetReg = /^data-(.+)$/
+
+function collectDataset (props) {
+  const dataset = {}
+  for (const key in props) {
+    if (hasOwn(props, key)) {
+      const matched = datasetReg.exec(key)
+      if (matched) {
+        dataset[matched[1]] = props[key]
+      }
+    }
+  }
+  return dataset
+}
+
 function createInstance ({ props, ref, type, rawOptions, currentInject, validProps, components }) {
   const instance = Object.create({
-    setData () {
+    setData (newData, callback) {
+      Object.keys(newData).forEach((key) => {
+        set(this, key, newData[key])
+      })
+      this.__render()
+      nextTick(callback)
     },
     __getProps () {
       const propsData = {}
@@ -47,6 +68,9 @@ function createInstance ({ props, ref, type, rawOptions, currentInject, validPro
       return propsData
     },
     __render () {
+      // eslint-disable-next-line symbol-description
+      this.__mpxProxy.stateVersion = Symbol()
+      this.__mpxProxy.onStoreChange && this.__mpxProxy.onStoreChange()
     },
     __injectedRender: currentInject.render || noop,
     __getRefsData () {
@@ -72,15 +96,41 @@ function createInstance ({ props, ref, type, rawOptions, currentInject, validPro
       }
       return result
     },
-    triggerEvent () {
+    triggerEvent (eventName, eventDetail) {
+      const handlerName = eventName.replace(/^./, matched => matched.toUpperCase()).replace(/-([a-z])/g, (match, p1) => p1.toUpperCase())
+      const handler = props && (props['bind' + handlerName] || props['catch' + handlerName] || props['capture-bind' + handlerName] || props['capture-catch' + handlerName])
+      if (handler && typeof handler === 'function') {
+        const timeStamp = +new Date()
+        const dataset = collectDataset(props)
+        const id = props.id || ''
+        const eventObj = {
+          type: eventName,
+          timeStamp,
+          target: {
+            id,
+            dataset,
+            targetDataset: dataset
+          },
+          currentTarget: {
+            id,
+            dataset
+          },
+          detail: eventDetail
+        }
+        handler.call(this, eventObj, '__customEvent')
+      }
     },
     selectComponent () {
+      error('selectComponent is not supported in react native, please use ref instead')
     },
     selectAllComponents () {
+      error('selectAllComponents is not supported in react native, please use ref instead')
     },
     createSelectorQuery () {
+      error('createSelectorQuery is not supported in react native, please use ref instead')
     },
     createIntersectionObserver () {
+      error('createIntersectionObserver is not supported in react native, please use ref instead')
     },
     ...rawOptions.methods
   })
--- packages/webpack-plugin/lib/template-compiler/gen-node-react.js
+++ packages/webpack-plugin/lib/template-compiler/gen-node-react.js
@@ -49,8 +49,7 @@ function genNode (node) {
         } else if (node.if && !node.ifProcessed) {
           exp += genIf(node)
         } else {
-          exp += `createElement(${node.isComponent || node.isBuiltIn ? `components[${node.is || s(node.tag)}]` : s(node.tag)}`
-          exp += `createElement(${node.isComponent || node.isBuiltIn ? `components[${s(node.tag)}]` : `getNativeComponent(${s(node.tag)})`}`
+          exp += `createElement(${node.isComponent || node.isBuiltIn ? `components[${node.is || s(node.tag)}]` : `getNativeComponent(${s(node.tag)})`}`
           if (node.attrsList.length) {
             exp += ',{'
             const attrExpMap = (node.exps || []).reduce((map, { exp, attrName }) => {
