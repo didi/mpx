42520cdc27d0d81ef543c6ab9682997e645ecd1d
hiyuki <674883329@qq.com>
2024-05-20 18:35:53 +0800

    WEB: fix sideEffect problem of effectScope.pause/resume in production mode.

--- packages/core/src/vuePlugin.js
+++ packages/core/src/vuePlugin.js
@@ -1,5 +1,60 @@
 import { walkChildren, parseSelector, error, hasOwn } from '@mpxjs/utils'
 import { createSelectorQuery, createIntersectionObserver } from '@mpxjs/api-proxy'
+import { EffectScope } from 'vue'
+import { PausedState } from '../../helper/const'
+
+const hackEffectScope = () => {
+  EffectScope.prototype.pause = function () {
+    if (this.active) {
+      let i, l
+      for (i = 0, l = this.effects.length; i < l; i++) {
+        const effect = this.effects[i]
+        // vue2.7中存在对于watcher实例方法的重写(doWatch)，因此无法通过修改Watcher.prototype统一实现pause和resume，只能逐个实例修改实现
+        if (!hasOwn(effect, 'pausedState')) {
+          effect.pausedState = PausedState.resumed
+          const rawUpdate = effect.update
+          effect.update = function () {
+            if (effect.pausedState !== PausedState.resumed) {
+              effect.pausedState = PausedState.dirty
+            } else {
+              rawUpdate.call(effect)
+            }
+          }
+        }
+        if (effect.pausedState !== PausedState.dirty) {
+          effect.pausedState = PausedState.paused
+        }
+      }
+      if (this.scopes) {
+        for (i = 0, l = this.scopes.length; i < l; i++) {
+          this.scopes[i].pause()
+        }
+      }
+    }
+  }
+
+  EffectScope.prototype.resume = function (ignoreDirty = false) {
+    if (this.active) {
+      let i, l
+      for (i = 0, l = this.effects.length; i < l; i++) {
+        const effect = this.effects[i]
+        if (hasOwn(effect, 'pausedState')) {
+          const lastPausedState = effect.pausedState
+          effect.pausedState = PausedState.resumed
+          if (!ignoreDirty && lastPausedState === PausedState.dirty) {
+            effect.update()
+          }
+        }
+      }
+      if (this.scopes) {
+        for (i = 0, l = this.scopes.length; i < l; i++) {
+          this.scopes[i].resume(ignoreDirty)
+        }
+      }
+    }
+  }
+}
+
 const datasetReg = /^data-(.+)$/
 
 function collectDataset (attrs) {
@@ -55,4 +110,5 @@ export default function install (Vue) {
   Vue.prototype.createIntersectionObserver = function (options) {
     return createIntersectionObserver(this, options)
   }
+  hackEffectScope()
 }
