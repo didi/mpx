791e1cbdb3352fa845495592797d72cd6b9714c1
yandadaFreedom <525966780@qq.com>
2024-05-28 18:31:30 +0800

    refactor: 修改showTabbar逻辑

--- packages/webpack-plugin/lib/runtime/components/react/mpx-scroll-view.tsx
+++ packages/webpack-plugin/lib/runtime/components/react/mpx-scroll-view.tsx
@@ -99,13 +99,13 @@ const _ScrollView = forwardRef((props: ScrollViewProps = {}, ref: React.Forwarde
     'scroll-with-animation': scrollWithAnimation,
     'refresher-enabled': refresherEnabled,
     'refresher-default-style': refresherDefaultStyle,
-    'refresher-background': refresherBackground
+    'refresher-background': refresherBackground,
+    'show-scrollbar': showScrollbar = true
   } = props;
   const [snapScrollTop, setSnapScrollTop] = useState(0);
   const [snapScrollLeft, setSnapScrollLeft] = useState(0);
   const [refreshing, setRefreshing] = useState(true);
   const [scrollEnabled, setScrollEnabled] = useState(true);
-  const [showScrollbar, setShowScrollbar] = useState(true);
   const layoutRef = useRef({})
   const scrollOptions = useRef({
     contentLength: 0,
@@ -159,15 +159,6 @@ const _ScrollView = forwardRef((props: ScrollViewProps = {}, ref: React.Forwarde
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [props['scroll-x'], props['scroll-y']]);
-
-  useEffect(() => {
-    if (props['show-scrollbar'] === undefined) {
-      setShowScrollbar(true)
-    } else{
-      setShowScrollbar(!!props['show-scrollbar'])
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [props['show-scrollbar']]);
   
   useEffect(() => {
     if (snapScrollTop || snapScrollLeft) {
