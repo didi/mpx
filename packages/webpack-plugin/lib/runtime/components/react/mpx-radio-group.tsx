9c129e98951e0b08f6ba1d79759d429fb041a8a1
WX-DongXing <dong.xing@outlook.com>
2024-06-20 15:17:08 +0800

    feat: 实现 checkbox 组件

---
+++ packages/webpack-plugin/lib/runtime/components/react/mpx-radio-group.tsx
@@ -0,0 +1,128 @@
+/**
+ * ✔ bindchange
+ */
+import React, {
+  JSX,
+  useRef,
+  forwardRef,
+  ReactNode,
+  Children,
+  cloneElement,
+  FunctionComponent,
+  isValidElement,
+  useState
+} from 'react'
+import {
+  View,
+  NativeSyntheticEvent,
+  StyleProp,
+  ViewStyle,
+  StyleSheet
+} from 'react-native'
+import useInnerProps, { getCustomEvent } from './getInnerListeners'
+import useNodesRef, { HandlerRef } from '../../useNodesRef'
+
+export interface radioGroupProps {
+  style?: StyleProp<ViewStyle>
+  'enable-offset'?: boolean
+  children: ReactNode
+  bindchange?: (evt: NativeSyntheticEvent<TouchEvent> | unknown) => void
+}
+
+const radioGroup = forwardRef<
+  HandlerRef<View, radioGroupProps>,
+  radioGroupProps
+>((props, ref): JSX.Element => {
+  const {
+    style = [],
+    'enable-offset': enableOffset,
+    children,
+    bindchange
+  } = props
+
+  const layoutRef = useRef({})
+
+  const [checkedValue, setCheckedValue] = useState<string>()
+
+  const defaultStyle = {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    ...StyleSheet.flatten(style)
+  }
+
+  const { nodeRef } = useNodesRef(props, ref, {
+    defaultStyle
+  })
+
+  const onLayout = () => {
+    nodeRef.current?.measure(
+      (
+        x: number,
+        y: number,
+        width: number,
+        height: number,
+        offsetLeft: number,
+        offsetTop: number
+      ) => {
+        layoutRef.current = { x, y, width, height, offsetLeft, offsetTop }
+      }
+    )
+  }
+
+  const onChange = (evt: NativeSyntheticEvent<TouchEvent>, value: string) => {
+    setCheckedValue(value)
+
+    bindchange &&
+      bindchange(
+        getCustomEvent(
+          'tap',
+          evt,
+          {
+            layoutRef,
+            detail: {
+              value
+            }
+          },
+          props
+        )
+      )
+  }
+
+  const wrapChildren = (children: ReactNode) => {
+    return Children.toArray(children).map((child) => {
+      if (!isValidElement(child)) return child
+
+      const displayName = (child.type as FunctionComponent)?.displayName
+
+      if (displayName === 'mpx-radio') {
+        const { value, checked } = child.props
+        return cloneElement(child, {
+          ...child.props,
+          checked: checkedValue ? checkedValue === value : !!checked,
+          _onChange: onChange
+        })
+      } else {
+        return cloneElement(child, {}, wrapChildren(child.props.children))
+      }
+    })
+  }
+
+  const innerProps = useInnerProps(
+    props,
+    {
+      ref: nodeRef,
+      style: defaultStyle,
+      ...(enableOffset ? { onLayout } : {})
+    },
+    ['enable-offset'],
+    {
+      layoutRef
+    }
+  )
+
+  return <View {...innerProps}>{wrapChildren(children)}</View>
+})
+
+radioGroup.displayName = 'mpx-radio-group'
+
+export default radioGroup
