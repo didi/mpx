15596af1bd263170da419964c872a5166fb30a0c
hiyuki <674883329@qq.com>
2024-07-02 20:09:54 +0800

    fix api-proxy

--- packages/api-proxy/src/platform/api/storage/index.js
+++ packages/api-proxy/src/platform/api/storage/index.js
@@ -1,6 +1,4 @@
-import { getEnvObj, envError } from '../../../common/js'
-
-const ENV_OBJ = getEnvObj()
+import { ENV_OBJ, envError } from '../../../common/js'
 
 const setStorage = ENV_OBJ.setStorage || envError('setStorage')
 
