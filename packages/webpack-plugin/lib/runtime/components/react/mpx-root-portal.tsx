a2453b823dfb13288b6f8f0a9ea41f01a22a141d
yandadaFreedom <525966780@qq.com>
2024-07-19 14:49:52 +0800

    feat: add root-portal组件

---
+++ packages/webpack-plugin/lib/runtime/components/react/mpx-root-portal.tsx
@@ -0,0 +1,21 @@
+/**
+ * ✔ enable
+ */
+import { ReactNode } from 'react'
+import { Portal } from '@ant-design/react-native'
+
+interface RootPortalProps {
+  enable?: boolean
+  children: ReactNode
+}
+const _RootPortal = (props: RootPortalProps) => {
+  const { children, enable = true } = props
+
+  return (
+    enable ? <Portal>{children}</Portal> : <>{children}</>
+  )
+}
+
+_RootPortal.displayName = 'mpx-root-portal'
+
+export default _RootPortal
