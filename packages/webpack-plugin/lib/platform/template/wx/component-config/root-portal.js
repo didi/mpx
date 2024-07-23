8ffbc66988a327a1da217f1d1a0410b653cd5be3
yandadaFreedom <525966780@qq.com>
2024-07-23 20:42:06 +0800

    feat: 支持web root-portal

--- packages/webpack-plugin/lib/platform/template/wx/component-config/root-portal.js
+++ packages/webpack-plugin/lib/platform/template/wx/component-config/root-portal.js
@@ -3,6 +3,10 @@ const TAG_NAME = 'root-portal'
 module.exports = function ({ print }) {
   return {
     test: TAG_NAME,
+    web (tag, { el }) {
+      el.isBuiltIn = true
+      return 'mpx-root-portal'
+    },
     ios (tag, { el }) {
       el.isBuiltIn = true
       return 'mpx-root-portal'
--- packages/webpack-plugin/lib/runtime/components/react/mpx-root-portal.tsx
+++ packages/webpack-plugin/lib/runtime/components/react/mpx-root-portal.tsx
@@ -2,18 +2,22 @@
  * ✔ enable
  */
 import { ReactNode } from 'react'
-import { Portal } from '@ant-design/react-native'
+import { Portal, View } from '@ant-design/react-native'
 
 interface RootPortalProps {
   enable?: boolean
   children: ReactNode
+  style?: Record<string, any>
 }
 const _RootPortal = (props: RootPortalProps) => {
-  const { children, enable = true } = props
-
-  return (
-    enable ? <Portal>{children}</Portal> : <>{children}</>
-  )
+  const { children, enable = true, style } = props
+  return enable ? (
+    <Portal>
+      <View style={style}>{children}</View>
+    </Portal>
+  ) : (
+    <View style={style}>{children}</View>
+  );
 }
 
 _RootPortal.displayName = 'mpx-root-portal'
