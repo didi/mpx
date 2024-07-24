e3138c80594629d5e95cb042b2065c83bc6d8088
yandadaFreedom <525966780@qq.com>
2024-07-24 10:08:07 +0800

    chore: 修改root-portal支持平台

--- packages/webpack-plugin/lib/platform/template/wx/component-config/root-portal.js
+++ packages/webpack-plugin/lib/platform/template/wx/component-config/root-portal.js
@@ -3,10 +3,6 @@ const TAG_NAME = 'root-portal'
 module.exports = function ({ print }) {
   return {
     test: TAG_NAME,
-    web (tag, { el }) {
-      el.isBuiltIn = true
-      return 'mpx-root-portal'
-    },
     ios (tag, { el }) {
       el.isBuiltIn = true
       return 'mpx-root-portal'
