8ae39d4274562f11cc4d972092eaf10aed7e6228
shangqunfeng <shangqunfeng@didiglobal.com>
2024-08-26 14:47:24 +0800

    fix(*): fix code

--- packages/webpack-plugin/lib/platform/template/wx/component-config/block.js
+++ packages/webpack-plugin/lib/platform/template/wx/component-config/block.js
@@ -5,14 +5,6 @@ module.exports = function () {
     test: TAG_NAME,
     web () {
       return 'template'
-    },
-    ios (tag, { el }) {
-      el.isBuiltIn = true
-      return 'mpx-block'
-    },
-    android (tag, { el }) {
-      el.isBuiltIn = true
-      return 'mpx-block'
     }
   }
 }
