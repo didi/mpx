4a6c7a9b0a12b17050b19e3bca632595b199b4f5
luyongfang <luyongfang@didichuxing.com>
2024-10-08 10:08:45 +0800

    fix: mask position & selected item position

--- packages/webpack-plugin/lib/runtime/components/react/mpx-picker-view.tsx
+++ packages/webpack-plugin/lib/runtime/components/react/mpx-picker-view.tsx
@@ -1,4 +1,4 @@
-import { View, ViewStyle } from 'react-native'
+import { View } from 'react-native'
 import React, { forwardRef, MutableRefObject, useState, useRef } from 'react'
 import useInnerProps, { getCustomEvent } from './getInnerListeners'
 import useNodesRef, { HandlerRef } from './useNodesRef' // 引入辅助函数
@@ -19,12 +19,19 @@ interface PickerViewProps {
   // 初始的defaultValue数组中的数字依次表示 picker-view 内的 picker-view-column 选择的第几项（下标从 0 开始），数字大于 picker-view-column 可选项长度时，选择最后一项。
   value?: Array<number>
   bindchange?: Function
-  style?: Object
+  style?: {
+    height?: number
+  }
 }
 
 interface PickerLayout {
   height: number,
   itemHeight: number
+}
+
+interface PosType {
+  height?: number,
+  top?: number
 }
 
 const styles: { [key: string]: Object } = {
@@ -44,7 +51,6 @@ const styles: { [key: string]: Object } = {
     width: "100%",
     zIndex: 100
   },
-
   maskBottom: {
     position: 'absolute',
     backgroundColor: "#fcfcfc",
@@ -59,7 +65,16 @@ const _PickerView = forwardRef<HandlerRef<View, PickerViewProps>, PickerViewProp
   const innerLayout = useRef({})
   const cloneRef = useRef(null)
   const wrapRef = useRef(null)
+  const fixPosition: PosType = {}
+  const maskPos: PosType = {}
   let [pickH, setPickH] = useState(0)
+  if (style?.height && pickH) {
+    const fixH = Math.floor((pickH - style.height) / 2)
+    fixPosition.top = -fixH
+    maskPos.height = pickH / 5 * 2 - fixH
+  } else {
+    maskPos.height = pickH / 5 * 2
+  }
 
   const { nodeRef } = useNodesRef<View, PickerViewProps>(props, ref, {})
 
@@ -74,20 +89,17 @@ const _PickerView = forwardRef<HandlerRef<View, PickerViewProps>, PickerViewProp
     const changeValue = value.slice()
     changeValue[columnIndex] = selIndex
     const eventData = getCustomEvent('change', {}, { detail: { value: changeValue, source: 'change' }, layoutRef: {} })
-    // console.log('-------------------onSelectChange:eventData', eventData)
     bindchange && bindchange(eventData)
   }
   
   const onWrapperLayout = (e) => {
     wrapRef.current?.measure((x: number, y: number, width: number, height: number, offsetLeft: number, offsetTop: number) => {
       const a = { x, y, width, height, offsetLeft, offsetTop }
-      console.log('-----onWrapperLayout', a)
     })
   }
 
   const getInnerLayout = (layout: MutableRefObject<{}>) => {
     innerLayout.current = layout.current
-    console.log('--------------getInnerLayout', innerLayout)
   }
 
   const innerProps = useInnerProps(props, {ref: nodeRef}, [], { layoutRef: innerLayout })
@@ -106,14 +118,14 @@ const _PickerView = forwardRef<HandlerRef<View, PickerViewProps>, PickerViewProp
       ...extraProps
     }
     return React.cloneElement(child, childProps)
-  }  
+  }
 
   const renderTopMask = () => {
-    return <View style={[styles.maskTop, { height: pickH / 5 * 2, top: 0, pointerEvents: "none"}]}></View>
+    return <View style={[styles.maskTop, { height: maskPos.height, top: 0, pointerEvents: "none"}]}></View>
   }
 
   const renderBottomMask = () => {
-    return <View style={[styles.maskBottom, { height: pickH / 5 * 2, bottom: 0, pointerEvents: "none"}]}></View>
+    return <View style={[styles.maskBottom, { height: maskPos.height, bottom: 0, pointerEvents: "none"}]}></View>
   }
 
   const renderSubChild = () => {
@@ -125,22 +137,14 @@ const _PickerView = forwardRef<HandlerRef<View, PickerViewProps>, PickerViewProp
       return cloneChild(children, 0)
     }
   }
-  // innerLayout.current.offsetTop 
-  console.log('----------mpx-picker-view: render', style)
-  return (<View style={[style, { position: 'relative' }]}  onLayout={onWrapperLayout} ref={wrapRef}>
+  
+  return (<View style={[style, { position: 'relative', overflow: 'hidden' }]}  onLayout={onWrapperLayout} ref={wrapRef}>
     {renderTopMask()}
-    <View style={[styles.wrapper]}>
+    <View style={[styles.wrapper, fixPosition]}>
       {renderSubChild()}
     </View>
     {renderBottomMask()}
   </View>)
-  /*
-  return (<View style={[{height: pickH}]}>
-    <View style={[styles.wrapper, { height: pickH }]}>
-      {renderSubChild()}
-    </View>
-  </View>)
-  */
 })
 
 _PickerView.displayName = 'mpx-picker-view';
