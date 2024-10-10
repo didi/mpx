091fa11ac346613efe1c176d6e63a7949d0dee3c
lareinayanyu <lareinayanyu@didiglobal.com>
2024-10-10 15:25:41 +0800

    feat: scroll-view组件样式统一处理

---
+++ packages/webpack-plugin/lib/runtime/components/react/common.tsx
@@ -0,0 +1,31 @@
+import { TextStyle } from 'react-native'
+import { Children, cloneElement } from 'react'
+import { VarContext } from './context'
+import { isText } from './utils'
+
+export interface WrapChildrenConfig {
+  hasVarDec: boolean
+  varContext?: Record<string, any>
+}
+
+interface TextConfig {
+  textStyle?: TextStyle
+  textProps?: Record<string, any>
+}
+
+export function wrapChildren (props: TextProps = {}, { hasVarDec, varContext }: WrapChildrenConfig, { textStyle, textProps } : TextConfig = {}) {
+  let { children } = props
+  if (textStyle || textProps) {
+    children = Children.map(children, (child) => {
+      if (isText(child)) {
+        const style = { ...textStyle, ...child.props.style }
+        return cloneElement(child, { ...textProps, style })
+      }
+      return child
+    })
+  }
+  if (hasVarDec && varContext) {
+    children = <VarContext.Provider value={varContext}>{children}</VarContext.Provider>
+  }
+  return children
+}
