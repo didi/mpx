66acf09d37f7a1df90c556605e7138171b43bda1
hiyuki <674883329@qq.com>
2024-10-17 15:16:32 +0800

    RN: support calc() && fix transform percent

--- packages/webpack-plugin/lib/runtime/components/react/mpx-text.tsx
+++ packages/webpack-plugin/lib/runtime/components/react/mpx-text.tsx
@@ -15,11 +15,12 @@ interface _TextProps extends TextProps {
   style?: TextStyle
   children?: ReactNode
   selectable?: boolean
+  'user-select'?: boolean
   'enable-var'?: boolean
   'external-var-context'?: Record<string, any>
-  'user-select'?: boolean
-  userSelect?: boolean
-  'disable-default-style'?: boolean
+  'parent-font-size'?: number
+  'parent-width'?: number
+  'parent-height'?: number
 }
 
 interface WrapChildrenConfig {
@@ -41,7 +42,10 @@ const _Text = forwardRef<HandlerRef<Text, _TextProps>, _TextProps>((props, ref):
     selectable,
     'enable-var': enableVar,
     'external-var-context': externalVarContext,
-    'user-select': userSelect
+    'user-select': userSelect,
+    'parent-font-size': parentFontSize,
+    'parent-width': parentWidth,
+    'parent-height': parentHeight
   } = props
 
   const layoutRef = useRef({})
@@ -50,7 +54,13 @@ const _Text = forwardRef<HandlerRef<Text, _TextProps>, _TextProps>((props, ref):
     normalStyle,
     hasVarDec,
     varContextRef
-  } = useTransformStyle(style, { enableVar, externalVarContext })
+  } = useTransformStyle(style, {
+    enableVar,
+    externalVarContext,
+    parentFontSize,
+    parentWidth,
+    parentHeight
+  })
 
   const { nodeRef } = useNodesRef<Text, _TextProps>(props, ref)
 
@@ -61,8 +71,11 @@ const _Text = forwardRef<HandlerRef<Text, _TextProps>, _TextProps>((props, ref):
     'children',
     'selectable',
     'user-select',
-    'useInherit',
-    'enable-offset'
+    'enable-var',
+    'external-var-context',
+    'parent-font-size',
+    'parent-width',
+    'parent-height'
   ], {
     layoutRef
   })
