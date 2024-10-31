07c6c285bd67ff0dfd2f2a234e51793eff8cd1ed
WX-DongXing <dong.xing@outlook.com>
2024-10-31 15:49:10 +0800

    feat: optimize image events mounting & remove useless

--- packages/webpack-plugin/lib/runtime/components/react/mpx-image/index.tsx
+++ packages/webpack-plugin/lib/runtime/components/react/mpx-image/index.tsx
@@ -11,12 +11,10 @@
  * ✔ DEFAULT_SIZE
  */
 import { useEffect, useMemo, useState, useRef, forwardRef } from 'react'
-
 import {
   Image as RNImage,
   View,
   ImageStyle,
-  ImageSourcePropType,
   ImageResizeMode,
   StyleSheet,
   NativeSyntheticEvent,
@@ -27,7 +25,7 @@ import {
 } from 'react-native'
 import useInnerProps, { getCustomEvent } from '../getInnerListeners'
 import useNodesRef, { HandlerRef } from '../useNodesRef'
-import { useLayout, useTransformStyle } from '../utils'
+import { SVG_REGEXP, useLayout, useTransformStyle } from '../utils'
 
 export type Mode =
   | 'scaleToFill'
@@ -44,8 +42,6 @@ export type Mode =
   | 'top right'
   | 'bottom left'
   | 'bottom right'
-
-export type SvgNumberProp = string | number | undefined
 
 export interface ImageProps {
   src?: string
@@ -64,17 +60,6 @@ export interface ImageProps {
 
 const DEFAULT_IMAGE_WIDTH = 320
 const DEFAULT_IMAGE_HEIGHT = 240
-// const REMOTE_SVG_REGEXP = /https?:\/\/.*\.(?:svg)/i
-
-// const styls = StyleSheet.create({
-//   suspense: {
-//     display: 'flex',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     width: '100%',
-//     height: '100%',
-//   },
-// })
 
 const cropMode: Mode[] = [
   'top',
@@ -100,14 +85,6 @@ const ModeMap = new Map<Mode, ImageResizeMode | undefined>([
 const isNumber = (value: DimensionValue) => typeof value === 'number'
 
 const relativeCenteredSize = (viewSize: number, imageSize: number) => (viewSize - imageSize) / 2
-
-// const Svg = lazy(() => import('./svg'))
-
-// const Fallback = (
-//   <View style={styls.suspense}>
-//     <Text>loading ...</Text>
-//   </View>
-// )
 
 const Image = forwardRef<HandlerRef<RNImage, ImageProps>, ImageProps>((props, ref): JSX.Element => {
   const {
@@ -151,21 +128,18 @@ const Image = forwardRef<HandlerRef<RNImage, ImageProps>, ImageProps>((props, re
 
   const { width, height } = normalStyle
 
-  const preSrc = useRef<string | undefined>()
-
-  const resizeMode: ImageResizeMode = ModeMap.get(mode) || 'stretch'
+  const isSvg = SVG_REGEXP.test(src)
+  // const isFillMode = mode === 'aspectFill'
   const isWidthFixMode = mode === 'widthFix'
   const isHeightFixMode = mode === 'heightFix'
   const isCropMode = cropMode.includes(mode)
-
-  const source: ImageSourcePropType = typeof src === 'string' ? { uri: src } : src
+  const resizeMode: ImageResizeMode = ModeMap.get(mode) || 'stretch'
 
   const [viewWidth, setViewWidth] = useState(isNumber(width) ? width : 0)
   const [viewHeight, setViewHeight] = useState(isNumber(height) ? height : 0)
   const [imageWidth, setImageWidth] = useState(0)
   const [imageHeight, setImageHeight] = useState(0)
   const [ratio, setRatio] = useState(0)
-  const [loaded, setLoaded] = useState(false)
 
   const fixedHeight = useMemo(() => {
     const fixed = viewWidth * ratio
@@ -204,25 +178,9 @@ const Image = forwardRef<HandlerRef<RNImage, ImageProps>, ImageProps>((props, re
   }, [mode, viewWidth, viewHeight, imageWidth, imageHeight])
 
   const onImageLoad = (evt: NativeSyntheticEvent<ImageLoadEventData>) => {
-    if (!bindload) return
-    if (typeof src === 'string') {
-      evt.persist()
-      RNImage.getSize(src, (width: number, height: number) => {
-        bindload(
-          getCustomEvent(
-            'load',
-            evt,
-            {
-              detail: { width, height },
-              layoutRef
-            },
-            props
-          )
-        )
-      })
-    } else {
-      const { width = 0, height = 0 } = RNImage.resolveAssetSource(src) || {}
-      bindload(
+    evt.persist()
+    RNImage.getSize(src, (width: number, height: number) => {
+      bindload!(
         getCustomEvent(
           'load',
           evt,
@@ -233,35 +191,25 @@ const Image = forwardRef<HandlerRef<RNImage, ImageProps>, ImageProps>((props, re
           props
         )
       )
-    }
+    })
   }
 
   const onImageError = (evt: NativeSyntheticEvent<ImageErrorEventData>) => {
-    binderror &&
-      binderror(
-        getCustomEvent(
-          'error',
-          evt,
-          {
-            detail: { errMsg: evt.nativeEvent.error },
-            layoutRef
-          },
-          props
-        )
+    binderror!(
+      getCustomEvent(
+        'error',
+        evt,
+        {
+          detail: { errMsg: evt.nativeEvent.error },
+          layoutRef
+        },
+        props
       )
+    )
   }
 
   useEffect(() => {
-    if (!isWidthFixMode && !isHeightFixMode && !isCropMode) {
-      setLoaded(true)
-      return
-    }
-
-    const changed = preSrc.current !== src
-    preSrc.current = src
-    changed && setLoaded(false)
-
-    if (typeof src === 'string') {
+    if (!isSvg && (isWidthFixMode || isHeightFixMode || isCropMode)) {
       RNImage.getSize(src, (width: number, height: number) => {
         if (isWidthFixMode || isHeightFixMode) {
           setRatio(width === 0 ? 0 : height / width)
@@ -270,73 +218,42 @@ const Image = forwardRef<HandlerRef<RNImage, ImageProps>, ImageProps>((props, re
           setImageWidth(width)
           setImageHeight(height)
         }
-        changed && setLoaded(true)
       })
-    } else {
-      const { width = 0, height = 0 } = RNImage.resolveAssetSource(src) || {}
-      if (isWidthFixMode || isHeightFixMode) {
-        setRatio(width === 0 ? 0 : height / width)
-      }
-      if (isCropMode) {
-        setImageWidth(width)
-        setImageHeight(height)
-      }
-      changed && setLoaded(true)
     }
-  }, [isWidthFixMode, isHeightFixMode, isCropMode, src])
+  }, [src, isSvg, isWidthFixMode, isHeightFixMode, isCropMode])
 
-  const innerProps = useInnerProps(props, {
-    ref: nodeRef,
-    style: {
-      ...normalStyle,
-      ...layoutStyle,
-      ...(isHeightFixMode && { width: fixedWidth }),
-      ...(isWidthFixMode && { height: fixedHeight })
+  const innerProps = useInnerProps(
+    props,
+    {
+      ref: nodeRef,
+      style: {
+        ...normalStyle,
+        ...layoutStyle,
+        ...(isHeightFixMode && { width: fixedWidth }),
+        ...(isWidthFixMode && { height: fixedHeight })
+      },
+      ...layoutProps
     },
-    ...layoutProps
-  },
-  [],
-  {
-    layoutRef
-  }
+    [],
+    {
+      layoutRef
+    }
   )
-
-  // if (typeof src === 'string' && REMOTE_SVG_REGEXP.test(src)) {
-  //   return (
-  //     <Suspense fallback={Fallback} {...innerProps}>
-  //       <View {...innerProps}>
-  //         <Svg src={src} style={style} width={width as SvgNumberProp} height={height as SvgNumberProp} />
-  //       </View>
-  //     </Suspense>
-  //   )
-  // }
-
-  // if (svg) {
-  //   return (
-  //     <Suspense fallback={Fallback}>
-  //       <View {...innerProps}>
-  //         <Svg local src={src} style={style} width={width as SvgNumberProp} height={height as SvgNumberProp} />
-  //       </View>
-  //     </Suspense>
-  //   )
-  // }
 
   return (
     <View {...innerProps}>
-      {
-        loaded && <RNImage
-          source={source}
-          resizeMode={resizeMode}
-          onLoad={onImageLoad}
-          onError={onImageError}
-          style={{
-            ...StyleSheet.absoluteFillObject,
-            width: isCropMode ? imageWidth : '100%',
-            height: isCropMode ? imageHeight : '100%',
-            ...(isCropMode && cropModeStyle)
-          }}
-        />
-      }
+      <RNImage
+        source={{ uri: src }}
+        resizeMode={resizeMode}
+        onLoad={bindload && onImageLoad}
+        onError={binderror && onImageError}
+        style={{
+          ...StyleSheet.absoluteFillObject,
+          width: isCropMode ? imageWidth : '100%',
+          height: isCropMode ? imageHeight : '100%',
+          ...(isCropMode && cropModeStyle)
+        }}
+      />
     </View>
   )
 })
