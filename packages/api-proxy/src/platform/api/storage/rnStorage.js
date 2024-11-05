08c7215298d9d4544d5e48fb660d91ff79fc9753
WX-DongXing <dong.xing@outlook.com>
2024-11-05 17:41:54 +0800

    feat: remove AsyncStorage

--- packages/api-proxy/src/platform/api/storage/rnStorage.js
+++ packages/api-proxy/src/platform/api/storage/rnStorage.js
@@ -1,6 +1,6 @@
 // import AsyncStorage from '@react-native-async-storage/async-storage'
 import { envError, successHandle, failHandle, defineUnsupportedProps } from '../../../common/js'
-import { hasOwn } from '@mpxjs/utils'
+// import { hasOwn } from '@mpxjs/utils'
 function setStorage (options = {}) {
   // const { key, data, success, fail, complete } = options
   // let obj = {}
@@ -27,102 +27,102 @@ function setStorage (options = {}) {
 const setStorageSync = envError('setStorageSync')
 
 function getStorage (options = {}) {
-  const { key, success, fail, complete } = options
-  if (!key) {
-    const result = {
-      errMsg: 'getStorage:fail parameter error: parameter.key should be String instead of Undefined;'
-    }
-    failHandle(result, fail, complete)
-    return
-  }
-  AsyncStorage.getItem(key, (err, res) => {
-    if (err || !res) {
-      const result = {
-        errMsg: `getStorage:fail ${err || 'data not found'}`
-      }
-      failHandle(result, fail, complete)
-      return
-    }
-    let item
-    let data = null
-    try {
-      item = JSON.parse(res)
-    } catch (e) {
-    }
-    if (item && typeof item === 'object' && hasOwn(item, 'data')) {
-      data = item.data
-    }
-    const result = {
-      errMsg: 'getStorage:ok',
-      data
-    }
-    successHandle(result, success, complete)
-  })
+  // const { key, success, fail, complete } = options
+  // if (!key) {
+  //   const result = {
+  //     errMsg: 'getStorage:fail parameter error: parameter.key should be String instead of Undefined;'
+  //   }
+  //   failHandle(result, fail, complete)
+  //   return
+  // }
+  // AsyncStorage.getItem(key, (err, res) => {
+  //   if (err || !res) {
+  //     const result = {
+  //       errMsg: `getStorage:fail ${err || 'data not found'}`
+  //     }
+  //     failHandle(result, fail, complete)
+  //     return
+  //   }
+  //   let item
+  //   let data = null
+  //   try {
+  //     item = JSON.parse(res)
+  //   } catch (e) {
+  //   }
+  //   if (item && typeof item === 'object' && hasOwn(item, 'data')) {
+  //     data = item.data
+  //   }
+  //   const result = {
+  //     errMsg: 'getStorage:ok',
+  //     data
+  //   }
+  //   successHandle(result, success, complete)
+  // })
 }
 
 const getStorageSync = envError('getStorageSync')
 
 function getStorageInfo (options = {}) {
-  const { success, fail, complete } = options
-  AsyncStorage.getAllKeys((err, keys) => {
-    if (err) {
-      const result = {
-        errMsg: `getStorage:fail ${err}`
-      }
-      failHandle(result, fail, complete)
-      return
-    }
-    const result = {
-      keys,
-      errMsg: 'getStorageInfo:ok'
-    }
-    defineUnsupportedProps(result, ['currentSize', 'limitSize'])
-    successHandle(result, success, complete)
-  })
+  // const { success, fail, complete } = options
+  // AsyncStorage.getAllKeys((err, keys) => {
+  //   if (err) {
+  //     const result = {
+  //       errMsg: `getStorage:fail ${err}`
+  //     }
+  //     failHandle(result, fail, complete)
+  //     return
+  //   }
+  //   const result = {
+  //     keys,
+  //     errMsg: 'getStorageInfo:ok'
+  //   }
+  //   defineUnsupportedProps(result, ['currentSize', 'limitSize'])
+  //   successHandle(result, success, complete)
+  // })
 }
 
 const getStorageInfoSync = envError('getStorageInfoSync')
 
 function removeStorage (options = {}) {
-  const { key, success, fail, complete } = options
-  AsyncStorage.removeItem(key, (err) => {
-    if (err) {
-      const result = {
-        errMsg: `removeStorage:fail ${err}`
-      }
-      failHandle(result, fail, complete)
-      return
-    }
-    const result = {
-      errMsg: 'removeStorage:ok'
-    }
-    successHandle(result, success, complete)
-  })
+  // const { key, success, fail, complete } = options
+  // AsyncStorage.removeItem(key, (err) => {
+  //   if (err) {
+  //     const result = {
+  //       errMsg: `removeStorage:fail ${err}`
+  //     }
+  //     failHandle(result, fail, complete)
+  //     return
+  //   }
+  //   const result = {
+  //     errMsg: 'removeStorage:ok'
+  //   }
+  //   successHandle(result, success, complete)
+  // })
 }
 
 function removeStorageSync (key) {
-  AsyncStorage.removeItem(key)
+  // AsyncStorage.removeItem(key)
 }
 
 function clearStorage (options = {}) {
-  const { success, fail, complete } = options
-  AsyncStorage.clear((err) => {
-    if (err) {
-      const result = {
-        errMsg: `clearStorage:fail ${err}`
-      }
-      failHandle(result, fail, complete)
-      return
-    }
-    const result = {
-      errMsg: 'clearStorage:ok'
-    }
-    successHandle(result, success, complete)
-  })
+  // const { success, fail, complete } = options
+  // AsyncStorage.clear((err) => {
+  //   if (err) {
+  //     const result = {
+  //       errMsg: `clearStorage:fail ${err}`
+  //     }
+  //     failHandle(result, fail, complete)
+  //     return
+  //   }
+  //   const result = {
+  //     errMsg: 'clearStorage:ok'
+  //   }
+  //   successHandle(result, success, complete)
+  // })
 }
 
 function clearStorageSync () {
-  AsyncStorage.clear()
+  // AsyncStorage.clear()
 }
 
 export {
