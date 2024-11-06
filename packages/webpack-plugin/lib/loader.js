f4b69506d281087b30b7ef29cabe55e1ca04f577
xuegan <xuegan@didiglobal.com>
2024-11-06 13:59:24 +0800

    fix: eslint error

--- packages/webpack-plugin/lib/loader.js
+++ packages/webpack-plugin/lib/loader.js
@@ -127,7 +127,7 @@ module.exports = function (content) {
         callback(err, {
           componentPlaceholder,
           componentGenerics,
-          usingComponentsInfo,
+          usingComponentsInfo
         })
       }
       try {
