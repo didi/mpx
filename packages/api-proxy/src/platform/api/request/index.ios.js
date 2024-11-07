7c102b6be4cc539d556985ac0dd92ac780255fb8
wangcuijuan <wangcuijuan@didiglobal.com>
2024-11-07 11:54:38 +0800

    先提交一版

--- packages/api-proxy/src/platform/api/request/index.ios.js
+++ packages/api-proxy/src/platform/api/request/index.ios.js
@@ -1,1 +1,18 @@
-export * from './index.web'
+import { request as requestFun } from './index.web'
+import qs from 'qs'
+import { Platform } from 'react-native'
+const request = function (options) {
+  if (parseInt(Platform.Version, 10) >= 17) {
+    Object.assign(options, {
+      paramsSerializer: (params) =>
+        qs.stringify(params, {
+          format: 'RFC3986'
+        })
+    })
+  }
+  return requestFun(options)
+}
+
+export {
+  request
+}
