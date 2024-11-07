8cce874f341a688f12129fd5024db2a2b8df3e31
xuegan <xuegan@didiglobal.com>
2024-11-07 21:51:11 +0800

    feat: 修改注释

--- packages/webpack-plugin/lib/index.js
+++ packages/webpack-plugin/lib/index.js
@@ -1814,7 +1814,7 @@ try {
           }
           createData.resource = addQuery(createData.resource, { mpx: MPX_PROCESSED_FLAG }, true)
         }
-
+        // mpxStyleOptions 为 mpx style 文件的标识，避免 Vue 文件插入 styleCompiler 后导致 vue scoped 样式隔离失效
         if (isWeb(mpx.mode) && queryObj.mpxStyleOptions) {
           const firstLoader = loaders[0] ? toPosix(loaders[0].loader) : ''
           const isPitcherRequest = firstLoader.includes('node_modules/vue-loader/lib/loaders/pitcher')
@@ -1831,7 +1831,6 @@ try {
               mpxStyleLoaderIndex = index
             }
           })
-          // mpxStyleOptions 为 mpx style 文件的标识，避免 Vue 文件插入 styleCompiler 后导致 vue scoped 样式隔离失效
           if (mpxStyleLoaderIndex === -1) {
             let loaderIndex = -1
             if (cssLoaderIndex > -1 && vueStyleLoaderIndex === -1) {
