4cb164fe82c9f6356b75aa3822f902e9baaa857e
xuegan <xuegan@didiglobal.com>
2024-11-12 16:44:45 +0800

    feat: 修改 mode resolver 逻辑

--- packages/webpack-plugin/lib/resolver/AddModePlugin.js
+++ packages/webpack-plugin/lib/resolver/AddModePlugin.js
@@ -34,20 +34,17 @@ module.exports = class AddModePlugin {
       // 当前资源没有后缀名或者路径不符合fileConditionRules规则时，直接返回
       if (!extname || !matchCondition(resourcePath, this.fileConditionRules)) return callback()
       const queryObj = parseQuery(request.query || '?')
+      const queryInfix = queryObj.infix
       queryObj.mode = mode
-      queryObj.infix = `${queryObj.infix || ''}.${mode}`
+      queryObj.infix = `${queryInfix || ''}.${mode}`
       obj.query = stringifyQuery(queryObj)
       obj.path = addInfix(resourcePath, mode, extname)
       obj.relativePath = request.relativePath && addInfix(request.relativePath, mode, extname)
-      // callback 中判断 mode = android | harmony，若无结果则二次 doResolve ios
       resolver.doResolve(target, Object.assign({}, request, obj), 'add mode: ' + mode, resolveContext, (err, result) => {
         if (this.defaultMode && !result) {
-          queryObj.mode = defaultMode
-          queryObj.infix = `${queryObj.infix || ''}.${defaultMode}`
-          obj.mode = defaultMode
+          queryObj.infix = `${queryInfix || ''}.${defaultMode}`
           obj.query = stringifyQuery(queryObj)
           obj.path = addInfix(resourcePath, defaultMode, extname)
-          obj.relativePath = request.relativePath && addInfix(request.relativePath, defaultMode, extname)
           resolver.doResolve(target, Object.assign({}, request, obj), 'add mode: ' + this.defaultMode, resolveContext, (err, result) => {
             callback(err, result)
           })
