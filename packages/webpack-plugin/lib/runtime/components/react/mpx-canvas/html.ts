2ed7e7cc42cf9828ffdbc0a3758e1aaa4dce2907
lareinayanyu <lareinayanyu@didiglobal.com>
2024-11-20 22:19:15 +0800

    chore: 修改webview通信

--- packages/webpack-plugin/lib/runtime/components/react/mpx-canvas/html.ts
+++ packages/webpack-plugin/lib/runtime/components/react/mpx-canvas/html.ts
@@ -54,136 +54,116 @@ function autoScaleCanvas(canvas) {
 }
 window.autoScaleCanvas = autoScaleCanvas;
 </script>
-<script>var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
+<script>
+
+var WEBVIEW_TARGET = '@@WEBVIEW_TARGET';
+
+var ID = function () {
+  return Math.random().toString(32).slice(2);
 };
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-var WEBVIEW_TARGET = "@@WEBVIEW_TARGET";
-var ID = function () { return Math.random().toString(32).slice(2); };
+
 var flattenObjectCopyValue = function (flatObj, srcObj, key) {
-    var value = srcObj[key];
-    if (typeof value === "function") {
-        return;
-    }
-    if (typeof value === "object" && value instanceof Node) {
-        return;
-    }
-    flatObj[key] = flattenObject(value);
+  var value = srcObj[key];
+  if (typeof value === 'function') {
+    return;
+  }
+  if (typeof value === 'object' && value instanceof Node) {
+    return;
+  }
+  flatObj[key] = flattenObject(value);
 };
+
 var flattenObject = function (object) {
-    if (typeof object !== "object" || object === null) {
-        return object;
-    }
-     // 处理 TypedArray
-    if (object instanceof Uint8ClampedArray) {
-        return Array.from(object);
-    }
-    var flatObject = {};
-    for (var key in object) {
-        flattenObjectCopyValue(flatObject, object, key);
-    }
-    for (var key in Object.getOwnPropertyNames(object)) {
-        flattenObjectCopyValue(flatObject, object, key);
-    }
-    return flatObject;
+  if (typeof object !== 'object' || object === null) {
+    return object;
+  }
+  // Handle TypedArray
+  if (object instanceof Uint8ClampedArray) {
+    return Array.from(object);
+  }
+  var flatObject = {};
+  for (var key in object) {
+    flattenObjectCopyValue(flatObject, object, key);
+  }
+  for (var key in Object.getOwnPropertyNames(object)) {
+    flattenObjectCopyValue(flatObject, object, key);
+  }
+  return flatObject;
 };
-var AutoScaledCanvas = (function () {
-    function AutoScaledCanvas(element) {
-        this.element = element;
-    }
-    AutoScaledCanvas.prototype.toDataURL = function () {
-        var _a;
-        var args = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            args[_i] = arguments[_i];
-        }
-        return (_a = this.element).toDataURL.apply(_a, args);
-    };
-    AutoScaledCanvas.prototype.autoScale = function () {
-        if (this.savedHeight !== undefined) {
-            this.element.height = this.savedHeight;
-        }
-        if (this.savedWidth !== undefined) {
-            this.element.width = this.savedWidth;
-        }
-        window.autoScaleCanvas(this.element);
-    };
-    Object.defineProperty(AutoScaledCanvas.prototype, "width", {
-        get: function () {
-            return this.element.width;
-        },
-        set: function (value) {
-            this.savedWidth = value;
-            this.autoScale();
-            return value;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(AutoScaledCanvas.prototype, "height", {
-        get: function () {
-            return this.element.height;
-        },
-        set: function (value) {
-            this.savedHeight = value;
-            this.autoScale();
-            return value;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    return AutoScaledCanvas;
-}());
+
+var AutoScaledCanvas = function (element) {
+  this.element = element;
+};
+
+AutoScaledCanvas.prototype.toDataURL = function () {
+  return this.element.toDataURL.apply(this.element, arguments);
+};
+
+AutoScaledCanvas.prototype.autoScale = function () {
+  if (this.savedHeight !== undefined) {
+    this.element.height = this.savedHeight;
+  }
+  if (this.savedWidth !== undefined) {
+    this.element.width = this.savedWidth;
+  }
+  window.autoScaleCanvas(this.element);
+};
+
+Object.defineProperty(AutoScaledCanvas.prototype, 'width', {
+  get: function () {
+    return this.element.width;
+  },
+  set: function (value) {
+    this.savedWidth = value;
+    this.autoScale();
+    return value;
+  },
+});
+
+Object.defineProperty(AutoScaledCanvas.prototype, 'height', {
+  get: function () {
+    return this.element.height;
+  },
+  set: function (value) {
+    this.savedHeight = value;
+    this.autoScale();
+    return value;
+  },
+});
 var toMessage = function (result) {
-    if (result instanceof Blob) {
-        return {
-            type: "blob",
-            payload: btoa(result),
-            meta: {},
-        };
-    }
-    if (result instanceof Object) {
-        if (!result[WEBVIEW_TARGET]) {
-            var id = ID();
-            result[WEBVIEW_TARGET] = id;
-            targets[id] = result;
-        }
-        return {
-            type: "json",
-            payload: flattenObject(result),
-            args: toArgs(flattenObject(result)),
-            meta: {
-                target: result[WEBVIEW_TARGET],
-                constructor: result.__constructorName__ || result.constructor.name,
-            },
-        };
+  if (result instanceof Blob) {
+    return {
+      type: 'blob',
+      payload: btoa(result),
+      meta: {},
+    };
+  }
+  if (result instanceof Object) {
+    if (!result[WEBVIEW_TARGET]) {
+      var id = ID();
+      result[WEBVIEW_TARGET] = id;
+      targets[id] = result;
     }
     return {
-        type: "json",
-        payload: typeof result === 'string' ? result : JSON.stringify(result),
-        meta: {},
+      type: 'json',
+      payload: flattenObject(result),
+      args: toArgs(flattenObject(result)),
+      meta: {
+        target: result[WEBVIEW_TARGET],
+        constructor: result.__constructorName__ || result.constructor.name,
+      },
     };
+  }
+  return {
+    type: 'json',
+    payload: typeof result === 'string' ? result : JSON.stringify(result),
+    meta: {},
+  };
 };
 var toArgs = function (result) {
     var args = [];
     for (var key in result) {
-        if (result[key] !== undefined && key !== "@@WEBVIEW_TARGET") {
+        if (result[key] !== undefined && key !== '@@WEBVIEW_TARGET') {
             args.push(result[key]);
         }
     }
@@ -191,151 +171,172 @@ var toArgs = function (result) {
 };
 
 var createObjectsFromArgs = function (args) {
-    var _a;
     for (var index = 0; index < args.length; index += 1) {
         var currentArg = args[index];
         if (currentArg && currentArg.className !== undefined) {
             var className = currentArg.className, classArgs = currentArg.classArgs;
             // new ImageData，第一个参数需要是 Uint8ClampedArray
-            var object = new ((_a = constructors[className]).bind.apply(_a, __spreadArray([void 0], classArgs, false)))();
+            var object = new (Function.prototype.bind.apply(constructors[className], [null].concat(classArgs)))();
             args[index] = object;
         }
     }
     return args;
 };
 
-var canvas = document.createElement("canvas");
+var canvas = document.createElement('canvas');
 var autoScaledCanvas = new AutoScaledCanvas(canvas);
+
 var targets = {
-    canvas: autoScaledCanvas,
-    context2D: canvas.getContext("2d"),
+  canvas: autoScaledCanvas,
+  context2D: canvas.getContext('2d'),
 };
+
 var constructors = {
-    CanvasGradient: CanvasGradient,
-    Image: Image,
-    ImageData: ImageData,
-    Uint8ClampedArray: Uint8ClampedArray,
+  CanvasGradient: CanvasGradient,
+  Image: Image,
+  ImageData: ImageData,
+  Uint8ClampedArray: Uint8ClampedArray,
 };
 
 Image.bind =
-    Image.bind ||
-        function () {
-            return Image;
-        };
+  Image.bind ||
+  function () {
+    return Image;
+  };
+
 ImageData.bind =
-    ImageData.bind ||
-        function () {
-            return ImageData;
-        };
+  ImageData.bind ||
+  function () {
+    return ImageData;
+  };
 Uint8ClampedArray.bind =
-    Uint8ClampedArray.bind ||
-        function () {
-            return Uint8ClampedArray;
-        };  
+  Uint8ClampedArray.bind ||
+  function () {
+    return Uint8ClampedArray;
+  };
+
 var populateRefs = function (arg) {
-    if (arg && arg.__ref__) {
-        return targets[arg.__ref__];
-    }
-    return arg;
+  if (arg && arg.__ref__) {
+    return targets[arg.__ref__];
+  }
+  return arg;
 };
 document.body.appendChild(canvas);
-function handleMessage(_a) {
-    var _b, _c;
-    var id = _a.id, type = _a.type, payload = _a.payload;
-    switch (type) {
-        case "exec": {
-            var target = payload.target, method = payload.method, args = payload.args;
-            var result = (_b = targets[target])[method].apply(_b, args.map(populateRefs));
-            var message = toMessage(result);
-            if (typeof result === "object" && !message.meta.constructor) {
-                for (var constructorName in constructors) {
-                    if (result instanceof constructors[constructorName]) {
-                        message.meta.constructor = constructorName;
-                    }
-                }
-            }
-            window.ReactNativeWebView.postMessage(JSON.stringify(__assign({ id: id }, message)));
-            break;
+
+var mergeObjects = function (target, source) {
+  for (var key in source) {
+    if (source.hasOwnProperty(key)) {
+      target[key] = source[key];
+    }
+  }
+  return target;
+};
+
+function handleMessage(message) {
+  var id = message.id,
+      type = message.type,
+      payload = message.payload;
+
+  switch (type) {
+    case 'exec': {
+      var target = payload.target,
+          method = payload.method,
+          args = payload.args;
+      var result = targets[target][method].apply(targets[target], args.map(populateRefs));
+      var msg = toMessage(result);
+
+      if (typeof result === 'object' && !msg.meta.constructor) {
+        for (var constructorName in constructors) {
+          if (result instanceof constructors[constructorName]) {
+            msg.meta.constructor = constructorName;
+          }
         }
-        case "set": {
-            var target = payload.target, key = payload.key, value = payload.value;
-            targets[target][key] = populateRefs(value);
-            break;
-        }
-        case "construct": {
-            var constructor = payload.constructor, target = payload.id, _d = payload.args, args = _d === void 0 ? [] : _d;
-            var newArgs = createObjectsFromArgs(args);
-            var object = void 0;
-            try {
-                object = new ((_c = constructors[constructor]).bind.apply(_c, __spreadArray([void 0], newArgs, false)))();
-            }
-            catch (error) {
-                throw new Error("Error while constructing ".concat(constructor, " ").concat(error.message));
-            }
-            object.__constructorName__ = constructor;
-            var message = toMessage({});
-            targets[target] = object;
-            window.ReactNativeWebView.postMessage(JSON.stringify(__assign({ id: id }, message)));
-            break;
-        }
-              case "listen": {
-      const { types, target } = payload;
-      for (const eventType of types) {
-        targets[target].addEventListener(eventType, (e) => {
-          const message = toMessage({
-            type: "event",
-            payload: {
-              type: e.type,
-              target: {
-                ...flattenObject(targets[target]),
-                [WEBVIEW_TARGET]: target,
-              },
-            },
-          });
-          window.ReactNativeWebView.postMessage(
-            JSON.stringify({ id, ...message }),
-          );
-        });
       }
+      window.ReactNativeWebView.postMessage(JSON.stringify(mergeObjects({ id: id }, msg)));
       break;
     }
+    case 'set': {
+      var target = payload.target,
+          key = payload.key,
+          value = payload.value;
+      targets[target][key] = populateRefs(value);
+      break;
+    }
+        case 'construct': {
+            var constructor = payload.constructor,
+          target = payload.id,
+          args = payload.args || [];
+            var newArgs = createObjectsFromArgs(args);
+            var object;
+            try {
+                object = new (Function.prototype.bind.apply(constructors[constructor], [null].concat(newArgs)))();
+            }
+            catch (error) {
+                throw new Error('Error while constructing '.concat(constructor, ' ').concat(error.message));
+            }
+            object.__constructorName__ = constructor;
+            var msg = toMessage({});
+            targets[target] = object;
+            window.ReactNativeWebView.postMessage(JSON.stringify(mergeObjects({ id: id }, msg)));
+            break;
+        }
+            case 'listen': {
+      var types = payload.types,
+          target = payload.target;
+    for (var i = 0; i < types.length; i++) {
+    var eventType = types[i];
+    targets[target].addEventListener(eventType, function (e) {
+          const message = toMessage({
+            type: 'event',
+            payload: {
+                type: e.type,
+                target: mergeObjects(flattenObject(targets[target]), {
+                [WEBVIEW_TARGET]: target,
+            }),
+            },
+          });
+      window.ReactNativeWebView.postMessage(
+        JSON.stringify(mergeObjects({ id: id }, message))
+      );
+    });
+  }
+  break;
+}
     }
 }
 var handleError = function (err, message) {
-    window.ReactNativeWebView.postMessage(JSON.stringify({
-        id: message.id,
-        type: "error",
-        payload: {
-            message: err.message,
-            stack: err.stack,
-        },
-    }));
-    document.removeEventListener("message", handleIncomingMessage);
+  window.ReactNativeWebView.postMessage(JSON.stringify({
+    id: message.id,
+    type: 'error',
+    payload: {
+      message: err.message,
+      stack: err.stack,
+    },
+  }));
+  document.removeEventListener('message', handleIncomingMessage);
 };
+
 function handleIncomingMessage(e) {
-    var data = JSON.parse(e.data);
-    if (Array.isArray(data)) {
-        for (var _i = 0, data_1 = data; _i < data_1.length; _i++) {
-            var message = data_1[_i];
-            try {
-                handleMessage(message);
-            }
-            catch (err) {
-                handleError(err, message);
-            }
-        }
+  var data = JSON.parse(e.data);
+  if (Array.isArray(data)) {
+    for (var i = 0; i < data.length; i++) {
+      try {
+        handleMessage(data[i]);
+      } catch (err) {
+        handleError(err, data[i]);
+      }
     }
-    else {
-        try {
-            handleMessage(data);
-        }
-        catch (err) {
-            handleError(err, data);
-        }
+  } else {
+    try {
+      handleMessage(data);
+    } catch (err) {
+      handleError(err, data);
     }
+  }
 }
-window.addEventListener("message", handleIncomingMessage);
-document.addEventListener("message", handleIncomingMessage);
+
+window.addEventListener('message', handleIncomingMessage);
+document.addEventListener('message', handleIncomingMessage);
 </script>
   
 
