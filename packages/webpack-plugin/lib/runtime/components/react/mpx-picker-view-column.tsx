86d6c7bca0db90ad9ce5a9c4d1d9903f788ad5d8
wangshunnn <wangshunnn@gmail.com>
2024-11-25 20:44:06 +0800

    chore: update

--- packages/webpack-plugin/lib/runtime/components/react/mpx-picker-view-column.tsx
+++ packages/webpack-plugin/lib/runtime/components/react/mpx-picker-view-column.tsx
@@ -42,7 +42,7 @@ const _PickerViewColumn = forwardRef<HandlerRef<ScrollView & View, ColumnProps>,
     'enable-var': enableVar,
     'external-var-context': externalVarContext
   } = props
-  console.log(`[mpx-picker-view-column-${columnIndex}] render`, initialIndex)
+
   const {
     normalStyle,
     hasVarDec,
