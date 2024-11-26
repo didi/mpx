cd99717b44d038262e1be1589947a679e5660f4b
lareinayanyu <lareinayanyu@didiglobal.com>
2024-11-26 18:35:49 +0800

    chore: 优化postMessage调用次数

--- packages/webpack-plugin/lib/runtime/components/react/mpx-canvas/Bus.ts
+++ packages/webpack-plugin/lib/runtime/components/react/mpx-canvas/Bus.ts
@@ -9,6 +9,7 @@ export default class Bus {
   _messageListeners: { [key: string]: (message: Message) => void } = {}
   _queue: Message[] = []
   _send: (message: Message | Message[]) => void
+  _timeoutId: NodeJS.Timeout | null = null; // 用于存储定时器 ID
   constructor (send: (message: Message | Message[]) => void) {
     this._send = send
   }
@@ -20,7 +21,8 @@ export default class Bus {
       }
 
       if (!this._paused) {
-        this._send(message)
+        this._queue.push(message)
+        this.startBatching()
       } else {
         this._queue.push(message)
       }
@@ -47,5 +49,15 @@ export default class Bus {
     this._paused = false
     this._send(this._queue)
     this._queue = []
+  }
+
+  startBatching (): void {
+    if (this._timeoutId) return
+
+    this._timeoutId = setTimeout(() => {
+      this._send(this._queue)
+      this._queue = []
+      this._timeoutId = null
+    }, 10)
   }
 }
