fab3d3407a13a758932b170cbf853fa2b0720e8e
huajingwen@didiglobal.com <huajingwen@didiglobal.com>
2024-11-28 20:08:04 +0800

    del log

--- packages/webpack-plugin/lib/platform/style/wx/index.js
+++ packages/webpack-plugin/lib/platform/style/wx/index.js
@@ -460,9 +460,7 @@ module.exports = function getSpec ({ warn, error }) {
       values = values.splice(0, 3)
     }
     const cssMap = []
-    // 复合属性不支持单个css var（css var可以接收单个值可以是复合值，复合值运行时不处理，这里前置提示一下）
     if (values.length === 1 && cssVariableExp.test(value)) {
-      error(`Property ${prop} in ${selector} is abbreviated property and does not support a single CSS var`)
       return { prop, value }
     }
     const lastOne = values[values.length - 1]
