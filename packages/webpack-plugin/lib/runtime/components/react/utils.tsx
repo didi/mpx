ecfb6cbe81dae4a267e4b9ce4e9597fae918abd8
shangqunfeng <shangqunfeng@didiglobal.com>
2024-11-29 14:58:30 +0800

    feat(image): image opt component.

--- packages/webpack-plugin/lib/runtime/components/react/utils.tsx
+++ packages/webpack-plugin/lib/runtime/components/react/utils.tsx
@@ -10,6 +10,7 @@ import type { AnyFunc, ExtendedFunctionComponent } from './types/common'
 export const TEXT_STYLE_REGEX = /color|font.*|text.*|letterSpacing|lineHeight|includeFontPadding|writingDirection/
 export const PERCENT_REGEX = /^\s*-?\d+(\.\d+)?%\s*$/
 export const URL_REGEX = /^\s*url\(["']?(.*?)["']?\)\s*$/
+export const SVG_REGEXP = /https?:\/\/.*\.(?:svg)/i
 export const BACKGROUND_REGEX = /^background(Image|Size|Repeat|Position)$/
 export const TEXT_PROPS_REGEX = /ellipsizeMode|numberOfLines/
 export const DEFAULT_FONT_SIZE = 16
@@ -610,3 +611,4 @@ export function pickStyle (styleObj: Record<string, any> = {}, pickedKeys: Array
     return acc
   }, {})
 }
+g
