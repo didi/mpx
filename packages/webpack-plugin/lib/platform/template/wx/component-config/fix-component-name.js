97a759f7cf103e37e897bb2066a4a45e91d3a608
lareinayanyu <lareinayanyu@didiglobal.com>
2024-12-03 12:08:04 +0800

    chore: 优化重复逻辑

--- packages/webpack-plugin/lib/platform/template/wx/component-config/fix-component-name.js
+++ packages/webpack-plugin/lib/platform/template/wx/component-config/fix-component-name.js
@@ -1,21 +1,25 @@
 const { isOriginTag, isBuildInTag } = require('../../../../utils/dom-tag-config')
 
 module.exports = function () {
+  const handleComponentTag = (el, data) => {
+    const newTag = `mpx-com-${el.tag}`
+    const usingComponents = data.usingComponents || []
+    // 当前组件名与原生tag或内建tag同名，对组件名进行转义
+    // json转义见：platform/json/wx/index.js fixComponentName
+    if (usingComponents.includes(newTag)) {
+      el.tag = newTag
+    }
+    return el
+  }
+
   return {
     waterfall: true,
     skipNormalize: true,
-    supportedModes: ['web'],
+    supportedModes: ['web', 'ios', 'android'],
     test: (input) => isOriginTag(input) || isBuildInTag(input),
-    // 输出web时对组件名进行转义避免与原生tag和内建tag冲突
-    web (el, data) {
-      const newTag = `mpx-com-${el.tag}`
-      const usingComponents = data.usingComponents || []
-      // 当前组件名与原生tag或内建tag同名，对组件名进行转义
-      // json转义见：platform/json/wx/index.js fixComponentName
-      if (usingComponents.includes(newTag)) {
-        el.tag = newTag
-      }
-      return el
-    }
+    // 所有平台使用相同的处理逻辑
+    web: handleComponentTag,
+    ios: handleComponentTag,
+    android: handleComponentTag
   }
 }
