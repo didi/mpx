aa14c818b880c421af69d901a3a1f86f62d70435
hiyuki <674883329@qq.com>
2024-12-10 15:04:04 +0800

    refactor patch add file

---
+++ packages/core/src/platform/patch/lifecycle/index.web.js
@@ -0,0 +1,53 @@
+const COMPONENT_HOOKS = [
+  'beforeCreate',
+  'created',
+  'beforeMount',
+  'mounted',
+  'beforeUpdate',
+  'updated',
+  'activated',
+  'deactivated',
+  'beforeDestroy',
+  'destroyed',
+  'errorCaptured',
+  'serverPrefetch'
+]
+
+const PAGE_HOOKS = [
+  ...COMPONENT_HOOKS,
+  'onLoad',
+  'onReady',
+  'onShow',
+  'onHide',
+  'onUnload',
+  'onPullDownRefresh',
+  'onReachBottom',
+  'onPageScroll',
+  'onAddToFavorites',
+  'onShareAppMessage',
+  'onShareTimeline',
+  'onResize',
+  'onTabItemTap',
+  'onSaveExitState'
+]
+
+const APP_HOOKS = [
+  ...COMPONENT_HOOKS,
+  'onLaunch',
+  'onShow',
+  'onHide',
+  'onError',
+  'onPageNotFound',
+  'onUnhandledRejection',
+  'onThemeChange',
+  'onSSRAppCreated',
+  'onAppInit'
+]
+
+export const LIFECYCLE = {
+  APP_HOOKS,
+  PAGE_HOOKS,
+  COMPONENT_HOOKS
+}
+
+export const pageMode = ''
