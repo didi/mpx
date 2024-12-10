d5f8c6e9290efb6d8822d1bd3efdd3c145cecfca
lareinayanyu <lareinayanyu@didiglobal.com>
2024-12-10 19:26:23 +0800

    fix: pendingProps取值

--- packages/webpack-plugin/lib/template-compiler/compiler.js
+++ packages/webpack-plugin/lib/template-compiler/compiler.js
@@ -2063,16 +2063,22 @@ function processWrapTextReact (el, options, meta) {
   if (parentTag !== 'mpx-text' && parentTag !== 'Text' && parentTag !== 'wxs') {
     const wrapper = createASTElement('mpx-simple-text')
     wrapper.isBuiltIn = true
-    const dataSetAttrs = []
+    const inheritAttrs = []
     parent.attrsList.forEach(({ name, value }) => {
       if (/^data-/.test(name)) {
-        dataSetAttrs.push({
+        inheritAttrs.push({
           name,
           value
         })
       }
+      if (/^id$/.test(name)) {
+        inheritAttrs.push({
+          name: 'parentId',
+          value
+        })
+      }
     })
-    addAttrs(wrapper, dataSetAttrs)
+    addAttrs(wrapper, inheritAttrs)
     replaceNode(el, wrapper, true)
     addChild(wrapper, el)
     processBuiltInComponents(wrapper, meta)
