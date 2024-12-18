abf1b6f84c2eed8a4d9d802f9d1e428bf0a8dd04
mater <bxh8640@gmail.com>
2024-12-18 19:11:56 +0800

    feat: preset

--- packages/unocss-plugin/lib/index.js
+++ packages/unocss-plugin/lib/index.js
@@ -1,5 +1,5 @@
 import * as path from 'path'
-import minimatch from 'minimatch'
+import { minimatch } from 'minimatch'
 import * as unoConfig from '@unocss/config'
 import * as core from '@unocss/core'
 import * as mpxConfig from '@mpxjs/webpack-plugin/lib/config.js'
