0a0a39918117f88b5351dd3674516ebe286354d0
wangcuijuan <wangcuijuan@didiglobal.com>
2024-12-19 21:26:46 +0800

    补充文件

---
+++ packages/api-proxy/src/platform/api/image/index.ios.js
@@ -0,0 +1,41 @@
+import { envError, defineUnsupportedProps, successHandle, failHandle } from '../../../common/js'
+import { Image } from 'react-native'
+
+const previewImage = envError('previewImage')
+
+const compressImage = envError('compressImage')
+
+const getImageInfo = function (options = {}) {
+  const { src, success, fail, complete } = options
+  if (src === undefined) {
+    const result = {
+      errMsg: 'getImageInfo:fail parameter error: parameter.src should be String instead of Undefined;',
+      errno: 1001
+    }
+    failHandle(result, fail, complete)
+    return
+  }
+  if (src === '') {
+    return
+  }
+  Image.getSize(src, (width, height) => {
+    const result = {
+      errMsg: 'getImageInfo:ok',
+      width,
+      height
+    }
+    defineUnsupportedProps(result, ['path', 'orientation', 'type'])
+    successHandle(result, success, complete)
+  }, (err) => {
+    const result = {
+      errMsg: 'getImageInfo:fail download image fail. reason: ' + err
+    }
+    failHandle(result, fail, complete)
+  })
+}
+
+export {
+  previewImage,
+  compressImage,
+  getImageInfo
+}
