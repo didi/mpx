7b34d473e7097ed2a2ef29c6b92ba2d96fb6d892
wangcuijuan <wangcuijuan@didiglobal.com>
2024-12-19 21:26:15 +0800

    补充getImageInfo

--- packages/api-proxy/src/platform/api/image/index.web.js
+++ packages/api-proxy/src/platform/api/image/index.web.js
@@ -1,5 +1,5 @@
 import Preview from './Preview'
-import { isBrowser, throwSSRWarning, envError } from '../../../common/js'
+import { isBrowser, throwSSRWarning, envError, defineUnsupportedProps, successHandle, failHandle } from '../../../common/js'
 
 let preview = null
 
@@ -15,9 +15,49 @@ const previewImage = (options) => {
   if (!preview) preview = new Preview()
   preview.show(options)
 }
+
 const compressImage = envError('compressImage')
+
+const getImageInfo = function (options = {}) {
+  const { src, success, fail, complete } = options
+
+  if (src === undefined) {
+    const result = {
+      errMsg: 'getImageInfo:fail parameter error: parameter.src should be String instead of Undefined;',
+      errno: 1001
+    }
+    failHandle(result, fail, complete)
+    return
+  }
+  if (src === '') {
+    return
+  }
+
+  const img = new Image()
+  img.src = src
+
+  img.onload = function () {
+    const width = img.width
+    const height = img.height
+    const result = {
+      errMsg: 'getImageInfo:ok',
+      width,
+      height
+    }
+    defineUnsupportedProps(result, ['path', 'orientation', 'type'])
+    successHandle(result, success, complete)
+  }
+
+  img.onerror = function () {
+    const result = {
+      errMsg: 'getImageInfo:fail download image fail. '
+    }
+    failHandle(result, fail, complete)
+  }
+}
 
 export {
   previewImage,
-  compressImage
+  compressImage,
+  getImageInfo
 }
