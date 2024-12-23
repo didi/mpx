b5e03c41c7817a3941555d3f58ee283bee50a9aa
mater <bxh8640@gmail.com>
2024-12-23 17:10:37 +0800

    fix: divide support

--- packages/unocss-base/preset-rn/rules/divide.js
+++ packages/unocss-base/preset-rn/rules/divide.js
@@ -1,19 +1,6 @@
 import { divides } from '@unocss/preset-wind/rules'
-import { ruleCallback } from '../../utils/index.js'
+import { transformEmptyRule } from '../../utils/index.js'
 
-const unSupport = ['s', 'e', 'bs', 'be', 'is', 'ie', 'block', 'inline']
-
-const divideRules = divides.map(v => {
-  const [regex, matcher, ...another] = v
-  return [
-    regex,
-    (...args) => {
-      const [[, direction]] = args
-      if (unSupport.includes(direction)) return ruleCallback(...args)
-      return matcher(...args)
-    },
-    ...another
-  ]
-})
+const divideRules = transformEmptyRule(divides)
 
 export { divideRules as divides }
