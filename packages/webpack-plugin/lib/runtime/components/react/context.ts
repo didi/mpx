5c03178172c022ae69923ad6e61ad918456c0e2a
wangcuijuan <wangcuijuan@didiglobal.com>
2024-12-30 11:55:37 +0800

    解决合并冲突丢失的代码

--- packages/webpack-plugin/lib/runtime/components/react/context.ts
+++ packages/webpack-plugin/lib/runtime/components/react/context.ts
@@ -32,7 +32,6 @@ export interface IntersectionObserver {
     throttleMeasure: () => void
   }
 }
-
 export interface PortalManagerContextValue {
   mount: (key: number, children: React.ReactNode) => void
   update: (key: number, children: React.ReactNode) => void
@@ -45,6 +44,10 @@ export interface PortalContextValue {
   update: (key: number, children: React.ReactNode, pageId?: number|null) => void
   unmount: (key: number, pageId?: number|null) => void
   manager?: PortalManagerContextValue
+}
+
+export interface ScrollViewContextValue {
+   gestureRef: React.RefObject<any> | null
 }
 
 export const MovableAreaContext = createContext({ width: 0, height: 0 })
@@ -65,7 +68,11 @@ export const IntersectionObserverContext = createContext<IntersectionObserver | 
 
 export const RouteContext = createContext<number | null>(null)
 
+export const SwiperContext = createContext({})
+
 export const KeyboardAvoidContext = createContext<KeyboardAvoidContextValue | null>(null)
+
+export const ScrollViewContext = createContext<ScrollViewContextValue>({ gestureRef: null })
 
 export const PortalContext = createContext<PortalContextValue>(null as any)
 
