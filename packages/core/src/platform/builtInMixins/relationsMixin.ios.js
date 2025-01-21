41a84156cd513607ac8d95eee0d4767ac50baa24
xiaolei <1017653702@qq.com>
2025-01-21 13:49:32 +0800

    [fix]edge case

--- packages/core/src/platform/builtInMixins/relationsMixin.ios.js
+++ packages/core/src/platform/builtInMixins/relationsMixin.ios.js
@@ -26,28 +26,30 @@ export default function relationsMixin (mixinType) {
           const relations = this.__mpxProxy.options.relations
           const relationContext = this.__relation
           const currentPath = this.__componentPath
-          Object.keys(relations).forEach((path) => {
-            const relation = relations[path]
-            const relationType = relation.type
-            if ((relationType === 'parent' || relationType === 'ancestor') && relationContext[path]) {
-              const target = relationContext[path]
-              const targetRelation = target.__mpxProxy.options.relations?.[currentPath]
-              if (targetRelation && targetRelation.type === relationTypeMap[relationType] && target.__componentPath === path) {
-                if (type === 'linked') {
-                  this.__mpxLinkRelationNodes(target, currentPath)
-                } else if (type === 'unlinked') {
-                  this.__mpxRemoveRelationNodes(target, currentPath)
+          if (relations && relationContext) {
+            Object.keys(relations).forEach((path) => {
+              const relation = relations[path]
+              const relationType = relation.type
+              if ((relationType === 'parent' || relationType === 'ancestor') && relationContext[path]) {
+                const target = relationContext[path]
+                const targetRelation = target.__mpxProxy.options.relations?.[currentPath]
+                if (targetRelation && targetRelation.type === relationTypeMap[relationType] && target.__componentPath === path) {
+                  if (type === 'linked') {
+                    this.__mpxLinkRelationNodes(target, currentPath)
+                  } else if (type === 'unlinked') {
+                    this.__mpxRemoveRelationNodes(target, currentPath)
+                  }
+                  if (typeof targetRelation[type] === 'function') {
+                    targetRelation[type].call(target, this)
+                  }
+                  if (typeof relation[type] === 'function') {
+                    relation[type].call(this, target)
+                  }
+                  this.__relationNodesMap[path] = [target]
                 }
-                if (typeof targetRelation[type] === 'function') {
-                  targetRelation[type].call(target, this)
-                }
-                if (typeof relation[type] === 'function') {
-                  relation[type].call(this, target)
-                }
-                this.__relationNodesMap[path] = [target]
               }
-            }
-          })
+            })
+          }
         },
         __mpxLinkRelationNodes (target, path) {
           target.__relationNodesMap[path] = target.__relationNodesMap[path] || [] // 父级绑定子级
