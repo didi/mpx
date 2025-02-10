6a3e3fc6e322283c363c80af8db7d0c7ba7b93c7
mackwang <mackwang@didichuxing.com>
2025-02-10 18:33:16 +0800

    fix: __mpx_mode__ type add harmony

--- packages/core/@types/global.d.ts
+++ packages/core/@types/global.d.ts
@@ -1,5 +1,5 @@
 // declaration for mpx mode
-declare let __mpx_mode__: 'wx' | 'ali' | 'swan' | 'qq' | 'tt' | 'web' | 'dd' | 'qa' | 'jd' | 'android' | 'ios'
+declare let __mpx_mode__: 'wx' | 'ali' | 'swan' | 'qq' | 'tt' | 'web' | 'dd' | 'qa' | 'jd' | 'android' | 'ios' | 'harmony'
 
 // declaration for mpx env
 declare let __mpx_env__: string
