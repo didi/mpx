11a4e81c88d73194d68db22f5b96d3c2012596a9
luyongfang <luyongfang@didichuxing.com>
2025-02-20 19:01:45 +0800

    修复非循环swiper最后快速滑动出边界的问题

--- packages/webpack-plugin/lib/runtime/components/react/mpx-swiper.tsx
+++ packages/webpack-plugin/lib/runtime/components/react/mpx-swiper.tsx
@@ -497,7 +497,8 @@ const SwiperWrapper = forwardRef<HandlerRef<View, SwiperProps>, SwiperProps>((pr
       let isCriticalItem = false
       // 真实滚动到的偏移量坐标
       let moveToTargetPos = 0
-      const currentOffset = translation < 0 ? offset.value - preMarginShared.value : offset.value + preMarginShared.value
+      const tmp = !circularShared.value ? 0 :  preMarginShared.value
+      const currentOffset = translation < 0 ? offset.value - tmp : offset.value + tmp
       const computedIndex = Math.abs(currentOffset) / step.value
       const moveToIndex = translation < 0 ? Math.ceil(computedIndex) : Math.floor(computedIndex)
       // 实际应该定位的索引值
