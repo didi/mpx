0d0489415ea944af204b80e614e6cf830173b4c4
wangshunnn <wangshunnn@gmail.com>
2025-02-20 21:19:43 +0800

    feat: support shiki twoslash

--- docs-vitepress/.vitepress/theme/index.js
+++ docs-vitepress/.vitepress/theme/index.js
@@ -1,7 +1,9 @@
 import { h } from "vue"
 import DefaultTheme from "vitepress/theme"
+import TwoslashFloatingVue from "@shikijs/vitepress-twoslash/client"
 import HomepageLayout from "./layouts/HomepageLayout.vue"
 
+import '@shikijs/vitepress-twoslash/style.css'
 import "virtual:group-icons.css"
 import "./styles/index.css"
 import "./styles/switchAppearance.css"
@@ -10,5 +12,8 @@ export default {
     ...DefaultTheme,
     Layout() {
         return h(HomepageLayout)
+    },
+    enhanceApp({ app }) {
+        app.use(TwoslashFloatingVue)
     },
 }
