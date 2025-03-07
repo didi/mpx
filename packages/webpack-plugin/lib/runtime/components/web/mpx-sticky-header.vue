a0d0c4a5d057d39cef7f5ade46e3cc603083be26
lareinayanyu <lareinayanyu@didiglobal.com>
2025-03-07 22:16:49 +0800

    feat: add v2

--- packages/webpack-plugin/lib/runtime/components/web/mpx-sticky-header.vue
+++ packages/webpack-plugin/lib/runtime/components/web/mpx-sticky-header.vue
@@ -1,65 +1,76 @@
 <script>
-import { getCustomEvent } from './getInnerListeners'
+  import { getCustomEvent } from './getInnerListeners'
 
-export default {
-  name: 'mpx-sticky-header',
-  inject: ['scrollOffset', 'scrollViewRect'],
-  props: {
-    'offsetTop': {
-      type: Number,
-      default: 0
-    }
-  },
-  data () {
-    return {
-      headerTop: 0,
-      isStickOnTop: false,
-      headerRect: {}
-    }
-  },
-  computed: {
-    _scrollOffset () {
-      return -this.scrollOffset?.get() || 0
+  export default {
+    name: 'mpx-sticky-header',
+    inject: ['scrollOffset', 'scrollViewRect'],
+    props: {
+      'offsetTop': {
+        type: Number,
+        default: 0
+      }
     },
-    _scrollViewRect () {
-      return this.scrollViewRect?.get() || {}
-    }
-  },
-  mounted () {
-    this.headerRect = this.$el.getBoundingClientRect()
-    this.headerTop = this.headerRect.top - (this._scrollViewRect.top || 0) - this.offsetTop
-  },
-  watch: {
-    _scrollOffset: {
-      handler (newScrollOffset) {
-        const newIsStickOnTop = newScrollOffset > this.headerTop
-        
-        if (newIsStickOnTop !== this.isStickOnTop) {
-          this.isStickOnTop = newIsStickOnTop
-          this.$emit('stickontopchange', getCustomEvent('stickontopchange', {
-            isStickOnTop: newIsStickOnTop
-          }, this))
+    data() {
+      return {
+        headerTop: 0,
+        isStickOnTop: false,
+        headerRect: {},
+        isInit: false
+      }
+    },
+    computed: {
+      _scrollOffset() {
+        return -this.scrollOffset?.get() || 0
+      },
+      _scrollViewRect() {
+        return this.scrollViewRect?.get() || {}
+      }
+    },
+    mounted() {
+      this.headerRect = this.$el.getBoundingClientRect()
+      this.headerTop = this.headerRect.top - (this._scrollViewRect.top || 0) - this.offsetTop
+    },
+    watch: {
+      _scrollOffset: {
+        handler(newScrollOffset) {
+          const newIsStickOnTop = newScrollOffset > this.headerTop
+          if (newIsStickOnTop !== this.isStickOnTop) {
+            this.isStickOnTop = newIsStickOnTop
+            this.$emit('stickontopchange', getCustomEvent('stickontopchange', {
+              isStickOnTop: newIsStickOnTop
+            }, this))
+          }
+        },
+        immediate: true
+      },
+      _scrollViewRect: {
+        handler(rect = {}) {
+          this.headerTop = this.headerRect.top - (rect.top || 0) - this.offsetTop
+        },
+      },
+      headerTop: {
+        handler() {
+          const newIsStickOnTop = this._scrollOffset > this.headerTop
+          if (newIsStickOnTop !== this.isStickOnTop) {
+            this.isStickOnTop = newIsStickOnTop
+            this.$emit('stickontopchange', getCustomEvent('stickontopchange', {
+              isStickOnTop: newIsStickOnTop
+            }, this))
+          }
         }
-      },
-      immediate: true
+      }
     },
-    _scrollViewRect: {
-      handler (rect = {}) {
-        this.headerTop = this.headerRect.top - (rect.top || 0) - this.offsetTop
-      },
-    }
-  },
-  render (h) {
-    const style = {
-      width: '100%',
-      boxSizing: 'border-box',
-      position: 'relative',
-      transform: this.isStickOnTop ? `translateY(${this._scrollOffset - this.headerTop + this.offsetTop}px)` : 'none'
-    }
+    render(h) {
+      const style = {
+        width: '100%',
+        boxSizing: 'border-box',
+        position: 'relative',
+        transform: this.isStickOnTop ? `translateY(${this._scrollOffset - this.headerTop + this.offsetTop}px)` : 'none'
+      }
 
-    return h('div', {
-      style
-    }, this.$slots.default)
+      return h('div', {
+        style
+      }, this.$slots.default)
+    }
   }
-}
 </script>
