c2be66db2cbd83cbc3d2c612c0d1cbbf73045c3e
WX-DongXing <dong.xing@outlook.com>
2025-03-07 19:31:28 +0800

    feat: remove supported rule

--- packages/webpack-plugin/lib/platform/template/wx/component-config/input.js
+++ packages/webpack-plugin/lib/platform/template/wx/component-config/input.js
@@ -92,7 +92,7 @@ module.exports = function ({ print }) {
         qa: qaPropLog
       },
       {
-        test: /^(placeholder-style|placeholder-class|always-embed|hold-keyboard|safe-password-.+)$/,
+        test: /^(placeholder-class|always-embed|hold-keyboard|safe-password-.+)$/,
         ios: iosPropLog,
         android: androidPropLog
       }
