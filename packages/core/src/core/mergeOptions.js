afb6588d3b96f145d5c397f915c7057672f234f5
wangshunnn <wangshunnn@gmail.com>
2025-03-17 21:53:00 +0800

    feat: support provide/inject mixins

--- packages/core/src/core/mergeOptions.js
+++ packages/core/src/core/mergeOptions.js
@@ -2,6 +2,7 @@ import { getConvertRule } from '../convertor/convertor'
 import builtInKeysMap from '../platform/patch/builtInKeysMap'
 import { implemented } from './implement'
 import {
+  isFunction,
   isObject,
   aliasReplace,
   makeMap,
@@ -215,12 +216,14 @@ function mergeMixins (parent, child) {
       mergeHooks(parent, child, key)
     } else if (/^(data|dataFn)$/.test(key)) {
       mergeDataFn(parent, child, key)
-    } else if (/^(computed|properties|props|methods|proto|options|relations|initData)$/.test(key)) {
+    } else if (/^(computed|properties|props|methods|proto|options|relations|initData|inject)$/.test(key)) {
       mergeShallowObj(parent, child, key)
     } else if (/^(watch|observers|pageLifetimes|events)$/.test(key)) {
       mergeToArray(parent, child, key)
     } else if (/^behaviors|externalClasses$/.test(key)) {
       mergeArray(parent, child, key)
+    } else if (/^provide$/.test(key)) {
+      mergedProvide(parent, child, key)
     } else if (key !== 'mixins' && key !== 'mpxCustomKeysForBlend') {
       // 收集非函数的自定义属性，在Component创建的页面中挂载到this上，模拟Page创建页面的表现，swan当中component构造器也能自动挂载自定义数据，不需要框架模拟挂载
       if (curType === 'blend' && typeof child[key] !== 'function' && !builtInKeysMap[key] && __mpx_mode__ !== 'swan') {
@@ -273,6 +276,22 @@ function mergeDataFn (parent, child, key) {
         const from = childVal.call(this)
         return Object.assign(to, from)
       }
+    }
+  }
+}
+
+function mergedProvide (parent, child, key) {
+  const parentVal = parent[key]
+  const childVal = child[key]
+  if (!parentVal) {
+    parent[key] = childVal
+  } else if (!childVal) {
+    parent[key] = parentVal
+  } else {
+    parent[key] = function mergedProvide () {
+      const to = isFunction(parentVal) ? parentVal.call(this) : parentVal
+      const from = isFunction(childVal) ? childVal.call(this) : childVal
+      return Object.assign(to, from)
     }
   }
 }
