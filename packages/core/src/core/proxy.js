7fbe07c31e283ee7986c64157ab10dc386ed014b
lareinayanyu <lareinayanyu@didiglobal.com>
2025-03-17 17:35:35 +0800

    fix: ali getPageId报错

--- packages/core/src/core/proxy.js
+++ packages/core/src/core/proxy.js
@@ -318,7 +318,7 @@ export default class MpxProxy {
           selectAllComponents: this.target.selectAllComponents.bind(this.target),
           createSelectorQuery: this.target.createSelectorQuery ? this.target.createSelectorQuery.bind(this.target) : envObj.createSelectorQuery.bind(envObj),
           createIntersectionObserver: this.target.createIntersectionObserver ? this.target.createIntersectionObserver.bind(this.target) : envObj.createIntersectionObserver.bind(envObj),
-          getPageId: this.target.getPageId.bind(this.target)
+          getPageId: this.target.getPageId ? this.target.getPageId.bind(this.target) : noop
         }
       ])
       if (!isObject(setupResult)) {
