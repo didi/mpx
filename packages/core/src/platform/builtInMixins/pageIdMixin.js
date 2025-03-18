de83dea448be4df442683eab5279a4e3bd3eb3ec
lareinayanyu <lareinayanyu@didiglobal.com>
2025-03-18 11:35:28 +0800

    fix: lint error

--- packages/core/src/platform/builtInMixins/pageIdMixin.js
+++ packages/core/src/platform/builtInMixins/pageIdMixin.js
@@ -1,9 +1,9 @@
-export default function pageStatusMixin () {
+export default function pageIdMixin (mixinType) {
   if (__mpx_mode__ === 'ali') {
       return {
         methods: {
           getPageId () {
-            if (this.__mpxProxy.options.__type__ === 'component') {
+            if (mixinType === 'component') {
               return this.$page.$id
             } else {
               return this.$id
