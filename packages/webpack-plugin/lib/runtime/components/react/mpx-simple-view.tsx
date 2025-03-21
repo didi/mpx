94974ac29730ab7f301ed31ba54f14c448bb2414
lareinayanyu <lareinayanyu@didiglobal.com>
2025-03-21 19:48:36 +0800

    feat: add simple view

--- packages/webpack-plugin/lib/runtime/components/react/mpx-simple-view.tsx
+++ packages/webpack-plugin/lib/runtime/components/react/mpx-simple-view.tsx
@@ -2,10 +2,13 @@ import { View, ViewProps } from 'react-native'
 import { createElement, forwardRef } from 'react'
 import { HandlerRef } from './useNodesRef'
 import useInnerProps from './getInnerListeners'
+import { extendObject } from './utils'
 
 const _View2 = forwardRef<HandlerRef<View, ViewProps>, ViewProps>((props: ViewProps, ref) => {
   const innerProps = useInnerProps(props, {}, [], {})
-  return createElement(View, innerProps)
+  return createElement(View, extendObject({}, innerProps, {
+    children: props.children
+  }))
 })
 
 _View2.displayName = 'MpxSimpleView'
