dd98dc8f2ca1581d9a4ae96ca5b36f8fce80a001
lareinayanyu <lareinayanyu@didiglobal.com>
2025-03-21 23:48:27 +0800

    feat: 支持nodeRef

--- packages/webpack-plugin/lib/runtime/components/react/mpx-simple-view.tsx
+++ packages/webpack-plugin/lib/runtime/components/react/mpx-simple-view.tsx
@@ -1,11 +1,17 @@
 import { View, ViewProps } from 'react-native'
-import { createElement, forwardRef } from 'react'
-import { HandlerRef } from './useNodesRef'
+import { createElement, forwardRef, useRef } from 'react'
+import useNodesRef, { HandlerRef } from './useNodesRef'
 import useInnerProps from './getInnerListeners'
 import { extendObject } from './utils'
 
 const _View2 = forwardRef<HandlerRef<View, ViewProps>, ViewProps>((props: ViewProps, ref) => {
-  const innerProps = useInnerProps(props, {}, [], {})
+  const nodeRef = useRef(null)
+  useNodesRef(props, ref, nodeRef, {
+    style: props.style || {}
+  })
+  const innerProps = useInnerProps(props, {
+    ref: nodeRef
+  }, [], {})
   return createElement(View, extendObject({}, innerProps, {
     children: props.children
   }))
