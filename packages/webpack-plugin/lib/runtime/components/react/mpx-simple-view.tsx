2ac8a0fdd54d77e7a1b5be2bf5ccb8b4beb8fc24
lareinayanyu <lareinayanyu@didiglobal.com>
2025-03-24 11:05:53 +0800

    chore: 简单view支持layoutRef

--- packages/webpack-plugin/lib/runtime/components/react/mpx-simple-view.tsx
+++ packages/webpack-plugin/lib/runtime/components/react/mpx-simple-view.tsx
@@ -2,16 +2,23 @@ import { View, ViewProps } from 'react-native'
 import { createElement, forwardRef, useRef } from 'react'
 import useNodesRef, { HandlerRef } from './useNodesRef'
 import useInnerProps from './getInnerListeners'
-import { extendObject } from './utils'
+import { extendObject, useLayout } from './utils'
 
 const _View2 = forwardRef<HandlerRef<View, ViewProps>, ViewProps>((props: ViewProps, ref) => {
   const nodeRef = useRef(null)
+
+  const { layoutRef } = useLayout({ props, nodeRef })
+
   useNodesRef(props, ref, nodeRef, {
     style: props.style || {}
   })
+
   const innerProps = useInnerProps(props, {
     ref: nodeRef
-  }, [], {})
+  }, [], {
+    layoutRef
+  })
+
   return createElement(View, extendObject({}, innerProps, {
     children: props.children
   }))
