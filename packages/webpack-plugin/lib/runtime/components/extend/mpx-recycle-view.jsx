6ec3a9ca4470b1d7ab09811d434ef4c9791a7200
lareinayanyu <lareinayanyu@didiglobal.com>
2025-03-26 17:03:43 +0800

    chore: 增强recycle-view.

--- packages/webpack-plugin/lib/runtime/components/extend/mpx-recycle-view.jsx
+++ packages/webpack-plugin/lib/runtime/components/extend/mpx-recycle-view.jsx
@@ -1,52 +1,57 @@
 import React, { forwardRef } from 'react';
 import {
   StyleSheet,
-  Text,
-  View,
   SafeAreaView,
   SectionList,
   StatusBar,
 } from 'react-native';
 
-  const Item = global.__mpxOptionsMap._e0b4ebfc
+const getGenericComponent = ({props, ref, generichash, generickey}) => {
+  const GenericComponent = global.__mpxGenericsMap[generichash](generickey)
+  return <GenericComponent ref={ref} {...props}/>
+}
+const Item = forwardRef((props, ref) => {
+   const { generichash, genericrecycleItem } = props
+   return getGenericComponent({props, ref, generichash, generickey: genericrecycleItem})
+  })
 
-const App = (props = {}, ref) => {
-  const { listData } = props
+const SectionHeader = forwardRef((props, ref) => {
+  const { generichash, genericsectionHeader } = props
+  return getGenericComponent({props, ref, generichash, generickey: genericsectionHeader})
+  })
+
+const SectionFooter = forwardRef((props, ref) => {
+  const { generichash, genericsectionFooter } = props
+  return getGenericComponent({props, ref, generichash, generickey: genericsectionFooter})
+})
+
+const ListHeader = forwardRef((props, ref) => {
+  const { generichash, genericlistHeader } = props
+   return getGenericComponent({props, ref, generichash, generickey: genericlistHeader})
+  })
+
+const ListFooter= forwardRef((props, ref) => {
+  const { generichash, genericlistFooter } = props
+  return getGenericComponent({props, ref, generichash, generickey: genericlistFooter})
+  })
+
+
+const RecycleView = forwardRef((props = {}, ref) => {
+  const { style, height, width, listData, generichash, genericrecycleItem, genericsectionHeader, genericsectionFooter, genericlistHeader, genericlistFooter, 'enable-sticky': enableSticky = false} = props
   return (
-    <SafeAreaView style={styles.container}>
       <SectionList
+        style={[{ height, width }, style]}
         sections={listData}
-        stickySectionHeadersEnabled={true}
+        stickySectionHeadersEnabled={enableSticky}
         keyExtractor={(item, index) => item + index}
-        renderItem={({item}) => <Item currentItem={item} />}
-        renderSectionHeader={({ section: { title } }) => (
-          <Text style={styles.header}>{title}</Text>
-        )}
+        renderItem={({item}) => <Item currentItem={item} generichash={generichash} genericrecycleItem={genericrecycleItem}/>}
+        renderSectionHeader={(data) => <SectionHeader data={data.section} generichash={generichash} genericsectionHeader={genericsectionHeader}/>}
+        renderSectionFooter={(data) => <SectionFooter data={data.section} generichash={generichash} genericsectionFooter={genericsectionFooter}/>}
+        ListHeaderComponent={<ListHeader {...props} generichash={generichash} genericlistHeader={genericlistHeader}/>}
+        ListFooterComponent={<ListFooter {...props} generichash={generichash} genericlistFooter={genericlistFooter}/>}
       />
-    </SafeAreaView>
   )
 
-}
+})
 
-const styles = StyleSheet.create({
-  container: {
-    height: 500,
-    paddingTop: StatusBar.currentHeight,
-    marginHorizontal: 16,
-    backgroundColor: 'red'
-  },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-  },
-  header: {
-    fontSize: 32,
-    backgroundColor: '#fff',
-  },
-  title: {
-    fontSize: 24,
-  },
-});
-
-export default App;
+export default RecycleView
