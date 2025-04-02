2a935c8eff4bcbf5d7b34b887c73f824c2a6b3fa
lareinayanyu <lareinayanyu@didiglobal.com>
2025-04-02 10:23:01 +0800

    feat: text组件支持is-simple

--- packages/webpack-plugin/lib/runtime/components/react/mpx-simple-view.tsx
+++ packages/webpack-plugin/lib/runtime/components/react/mpx-simple-view.tsx
@@ -1,22 +1,15 @@
 import { View, ViewProps, TextStyle } from 'react-native'
-import { createElement, forwardRef, useRef } from 'react'
-import useNodesRef, { HandlerRef } from './useNodesRef'
+import { createElement, forwardRef } from 'react'
+import { HandlerRef } from './useNodesRef'
 import { extendObject, splitProps, splitStyle, wrapChildren } from './utils'
 
 const _View2 = forwardRef<HandlerRef<View, ViewProps>, ViewProps>((simpleViewProps: ViewProps, ref) => {
-  const nodeRef = useRef(null)
-
   const { textProps, innerProps: props = {} } = splitProps(simpleViewProps)
 
   const { textStyle, innerStyle = {} } = splitStyle(props.style || {})
 
-  useNodesRef(props, ref, nodeRef, {
-    style: innerStyle || {}
-  })
-
   return createElement(View, extendObject({}, props, {
-    style: innerStyle,
-    ref: nodeRef
+    style: innerStyle
   }), wrapChildren(
     props,
     {
