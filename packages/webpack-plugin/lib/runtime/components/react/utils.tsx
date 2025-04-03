3de276ba200a6640f25486b27687e3d2150f2825
shangqunfeng <shangqunfeng@didiglobal.com>
2025-04-03 16:51:09 +0800

    feat(*): fix assign

--- packages/webpack-plugin/lib/runtime/components/react/utils.tsx
+++ packages/webpack-plugin/lib/runtime/components/react/utils.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useCallback, useMemo, useRef, ReactNode, ReactElement, isValidElement, useContext, useState, Dispatch, SetStateAction, Children, cloneElement } from 'react'
+import { useEffect, useCallback, useMemo, useRef, ReactNode, ReactElement, isValidElement, useContext, useState, Dispatch, SetStateAction, Children, cloneElement, createElement } from 'react'
 import { LayoutChangeEvent, TextStyle, ImageProps, Image } from 'react-native'
 import { isObject, isFunction, isNumber, hasOwn, diffAndCloneA, error, warn } from '@mpxjs/utils'
 import { VarContext, ScrollViewContext, RouteContext } from './context'
@@ -642,7 +642,7 @@ export function renderImage (
   enableFastImage = false
 ) {
   const Component: React.ComponentType<ImageProps | FastImageProps> = enableFastImage ? FastImage : Image
-  return <Component {...imageProps} />
+  return createElement(Component, imageProps)
 }
 
 export function pickStyle (styleObj: Record<string, any> = {}, pickedKeys: Array<string>, callback?: (key: string, val: number | string) => number | string) {
