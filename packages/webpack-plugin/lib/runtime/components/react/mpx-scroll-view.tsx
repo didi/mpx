911bf36737c050993c59be9e5ed0b223a6d68c37
lareinayanyu <lareinayanyu@didiglobal.com>
2025-04-10 15:05:54 +0800

    chore: 修改wrapChildren props 合并

--- packages/webpack-plugin/lib/runtime/components/react/mpx-scroll-view.tsx
+++ packages/webpack-plugin/lib/runtime/components/react/mpx-scroll-view.tsx
@@ -722,7 +722,7 @@ const _ScrollView = forwardRef<HandlerRef<ScrollView & View, ScrollViewProps>, S
           ScrollViewContext.Provider,
           { value: contextValue },
           wrapChildren(
-            extendObject(props, { children: otherContent }),
+            extendObject({}, props, { children: otherContent }),
             {
               hasVarDec,
               varContext: varContextRef.current,
