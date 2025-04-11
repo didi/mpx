4f409dce957cf377e5b2bbb985cfab171282971d
lareinayanyu <lareinayanyu@didiglobal.com>
2025-04-11 11:02:10 +0800

    fix: lint;

--- packages/webpack-plugin/lib/runtime/components/extend/mpx-recycle-view.jsx
+++ packages/webpack-plugin/lib/runtime/components/extend/mpx-recycle-view.jsx
@@ -1,39 +1,38 @@
-import React, { forwardRef } from 'react';
-import { SectionList, FlatList } from 'react-native';
-import useInnerProps from '../react/getInnerListeners';
+import React, { forwardRef, useRef } from 'react'
+import { SectionList, FlatList } from 'react-native'
+import useInnerProps from '../react/getInnerListeners'
 import useNodesRef from '../react/useNodesRef'
-import { extendObject, getCustomEvent } from '../react/utils'
+import { extendObject, getCustomEvent, useLayout, useTransformStyle } from '../react/utils'
 
-const getGenericComponent = ({props, ref, generichash, generickey}) => {
+const getGenericComponent = ({ props, ref, generichash, generickey }) => {
   const GenericComponent = global.__mpxGenericsMap[generichash](generickey)
   return <GenericComponent ref={ref} {...props}/>
 }
 
 const Item = forwardRef((props, ref) => {
-   const { generichash, genericrecycleItem } = props
-   return getGenericComponent({props, ref, generichash, generickey: genericrecycleItem})
-  })
+  const { generichash, genericrecycleItem } = props
+  return getGenericComponent({ props, ref, generichash, generickey: genericrecycleItem })
+})
 
 const SectionHeader = forwardRef((props, ref) => {
   const { generichash, genericsectionHeader } = props
-  return getGenericComponent({props, ref, generichash, generickey: genericsectionHeader})
-  })
+  return getGenericComponent({ props, ref, generichash, generickey: genericsectionHeader })
+})
 
 const SectionFooter = forwardRef((props, ref) => {
   const { generichash, genericsectionFooter } = props
-  return getGenericComponent({props, ref, generichash, generickey: genericsectionFooter})
+  return getGenericComponent({ props, ref, generichash, generickey: genericsectionFooter })
 })
 
 const ListHeader = forwardRef((props, ref) => {
   const { generichash, genericlistHeader } = props
-   return getGenericComponent({props, ref, generichash, generickey: genericlistHeader})
-  })
+  return getGenericComponent({ props, ref, generichash, generickey: genericlistHeader })
+})
 
-const ListFooter= forwardRef((props, ref) => {
+const ListFooter = forwardRef((props, ref) => {
   const { generichash, genericlistFooter } = props
-  return getGenericComponent({props, ref, generichash, generickey: genericlistFooter})
-  })
-
+  return getGenericComponent({ props, ref, generichash, generickey: genericlistFooter })
+})
 
 const RecycleView = forwardRef((props = {}, ref) => {
   const {
@@ -50,6 +49,12 @@ const RecycleView = forwardRef((props = {}, ref) => {
     genericsectionFooter,
     genericlistHeader,
     genericlistFooter,
+    style = {},
+    'enable-var': enableVar,
+    'external-var-context': externalVarContext,
+    'parent-font-size': parentFontSize,
+    'parent-width': parentWidth,
+    'parent-height': parentHeight,
     'enable-sticky': enableSticky = false,
     'enable-back-to-top': enableBackToTop = false,
     'lower-threshold': lowerThreshold = 50,
@@ -65,23 +70,31 @@ const RecycleView = forwardRef((props = {}, ref) => {
     setHeight
   } = useTransformStyle(style, { enableVar, externalVarContext, parentFontSize, parentWidth, parentHeight })
 
-  const { layoutRef, layoutStyle, layoutProps } = useLayout({ props: extendObject({}, props, { 'enable-offset': true }), hasSelfPercent, setWidth, setHeight, nodeRef: scrollViewRef })
-  
+  const { layoutRef, layoutStyle, layoutProps } = useLayout({ props, hasSelfPercent, setWidth, setHeight, nodeRef: scrollViewRef })
+
   useNodesRef(props, ref, scrollViewRef, { style })
 
-  function onRefresh() {
+  function onRefresh () {
     const { bindrefresherrefresh } = props
     bindrefresherrefresh &&
       bindrefresherrefresh(
         getCustomEvent('refresherrefresh', {}, { layoutRef }, props)
-    )
+      )
   }
 
-  function onEndReached() {
+  function onEndReached () {
     const { bindscrolltolower } = props
     bindscrolltolower &&
       bindscrolltolower(
         getCustomEvent('scrolltolower', {}, { layoutRef }, props)
+      )
+  }
+
+  function onScroll (event) {
+    const { bindscroll } = props
+    bindscroll &&
+      bindscroll(
+        getCustomEvent('scroll', event.nativeEvent, { layoutRef }, props)
       )
   }
 
@@ -123,25 +136,24 @@ const RecycleView = forwardRef((props = {}, ref) => {
   ], { layoutRef })
 
   return (
-    type === 'section' ?
-      <SectionList
+    type === 'section'
+      ? <SectionList
         {...innerProps}
         style={[{ height, width }, style, layoutStyle]}
         sections={listData}
         keyExtractor={(item, index) => item + index}
-        renderItem={({item}) => <Item currentItem={item} generichash={generichash} genericrecycleItem={genericrecycleItem}/>}
+        renderItem={({ item }) => <Item currentItem={item} generichash={generichash} genericrecycleItem={genericrecycleItem}/>}
         renderSectionHeader={(data) => <SectionHeader data={data.section} generichash={generichash} genericsectionHeader={genericsectionHeader}/>}
         renderSectionFooter={(data) => <SectionFooter data={data.section} generichash={generichash} genericsectionFooter={genericsectionFooter}/>}
         ListHeaderComponent={<ListHeader {...props} generichash={generichash} genericlistHeader={genericlistHeader}/>}
         ListFooterComponent={<ListFooter {...props} generichash={generichash} genericlistFooter={genericlistFooter}/>}
       />
-    :
-      <FlatList
+      : <FlatList
         {...innerProps}
         style={[{ height, width }, style, layoutStyle]}
         data={listData}
         keyExtractor={(item, index) => item + index}
-        renderItem={({item}) => <Item currentItem={item} generichash={generichash} genericrecycleItem={genericrecycleItem}/>}
+        renderItem={({ item }) => <Item currentItem={item} generichash={generichash} genericrecycleItem={genericrecycleItem}/>}
         ListHeaderComponent={<ListHeader {...props} generichash={generichash} genericlistHeader={genericlistHeader}/>}
         ListFooterComponent={<ListFooter {...props} generichash={generichash} genericlistFooter={genericlistFooter}/>}
       />
