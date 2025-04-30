8f0597b4074a4cf97ce13da189fe1b7edd37535e
xiaolei <1017653702@qq.com>
2025-04-30 16:38:18 +0800

    [update]add loading & errorPage

--- packages/webpack-plugin/lib/runtime/components/react/AsyncContainer.tsx
+++ packages/webpack-plugin/lib/runtime/components/react/AsyncContainer.tsx
@@ -1,18 +1,29 @@
 import { ComponentType, ReactNode, Component, Fragment, Suspense, ErrorInfo } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
     padding: 20,
     backgroundColor: '#fff'
   },
-  text: {
+  loadingImage: {
+    width: 100,
+    height: 100,
+    marginTop: 220,
+    alignSelf: 'center'
+  },
+  buttonText: {
+    color: '#fff',
     fontSize: 16,
-    textAlign: 'center',
-    color: '#333'
+    fontWeight: '500',
+    textAlign: 'center'
+  },
+  errorImage: {
+    marginTop: 80,
+    width: 220,
+    aspectRatio: 1,
+    alignSelf: 'center'
   },
   errorText: {
     fontSize: 16,
@@ -20,15 +31,18 @@ const styles = StyleSheet.create({
     color: '#333',
     marginBottom: 20
   },
-  reloadButton: {
-    backgroundColor: '#9E9E9E',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-    marginTop: 10
+  retryButton: {
+    backgroundColor: '#fff',
+    paddingVertical: 15,
+    borderRadius: 30,
+    marginTop: 40,
+    borderWidth: 1,
+    borderColor: '#FF5F00',
+    marginLeft: 20,
+    marginRight: 20
   },
-  buttonText: {
-    color: '#fff',
+  retryButtonText: {
+    color: '#FF5F00',
     fontSize: 16,
     fontWeight: '500',
     textAlign: 'center'
@@ -53,14 +67,13 @@ interface ComponentError extends Error {
   request?: string
 }
 
-interface DefaultLoadingProps {
-  text?: string
-}
-
-const DefaultLoading = ({ text = '加载中...' }: DefaultLoadingProps) => {
+const DefaultLoading = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{text}</Text>
+      <Image
+        style={styles.loadingImage}
+        source={{ uri: 'https://dpubstatic.udache.com/static/dpubimg/439jiCVOtNOnEv9F2LaDs_loading.gif' }}
+        resizeMode='contain'></Image>
     </View>
   )
 }
@@ -72,13 +85,18 @@ interface DefaultFallbackProps {
 const DefaultFallback = ({ onReload }: DefaultFallbackProps) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.errorText}>网络无法连接</Text>
+      <Image
+        source={{ uri: 'https://dpubstatic.udache.com/static/dpubimg/Vak5mZvezPpKV5ZJI6P9b_drn-fallbak.png' }}
+        style={styles.errorImage}
+        resizeMode="contain"
+      />
+      <Text style={styles.errorText}>网络出了点问题，请查看网络环境</Text>
       <TouchableOpacity
-        style={styles.reloadButton}
+        style={styles.retryButton}
         onPress={onReload}
         activeOpacity={0.7}
       >
-        <Text style={styles.buttonText}>重新加载</Text>
+        <Text style={styles.retryButtonText}>点击重试</Text>
       </TouchableOpacity>
     </View>
   )
