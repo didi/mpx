479e379f0f18077559800cb7c210272c0a6449c9
Soon Wang <wangshunnn@gmail.com>
2025-05-16 15:40:20 +0800

    feat: init reactivity package

---
+++ packages/reactivity/tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  splitting: false,
+  clean: true,
+  treeshake: true,
+  minify: true,
+  outDir: "dist",
+  outExtension: ({ format }) => ({
+    js: `.${format}.js`,
+  }),
+})
