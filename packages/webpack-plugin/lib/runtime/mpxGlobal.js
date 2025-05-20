5b541a90edb573c314eea9e636aae344046d2db8
xuegan <xuegan@didiglobal.com>
2025-05-20 14:27:43 +0800

    feat: 调整mpxGlobal

--- packages/webpack-plugin/lib/runtime/mpxGlobal.js
+++ packages/webpack-plugin/lib/runtime/mpxGlobal.js
@@ -1,8 +1,1 @@
-const globalObj = typeof globalThis === 'object'
-  ? globalThis
-  : (() => {
-    // eslint-disable-next-line no-new-func
-    try { return this || new Function('return this')() } catch (e) { return window }
-  })()
-
-module.exports = Object.create(globalObj)
+module.exports = Object.create(global)
