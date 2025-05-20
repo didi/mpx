0f12bb8d50f1f035f394348f3d5ae627de679c59
xuegan <xuegan@didiglobal.com>
2025-05-20 10:59:35 +0800

    fix: 修复mpxGlobal proxy 代理问题

--- packages/webpack-plugin/lib/runtime/mpxGlobal.js
+++ packages/webpack-plugin/lib/runtime/mpxGlobal.js
@@ -5,13 +5,4 @@ const globalObj = typeof globalThis === 'object'
     try { return this || new Function('return this')() } catch (e) { return window }
   })()
 
-const proxyGlobal = new Proxy(Object.create(globalObj), {
-  set (target, prop, value) {
-    // 同步修改到真实全局对象
-    Reflect.set(globalObj, prop, value)
-    Reflect.set(target, prop, value)
-    return true
-  }
-})
-
-module.exports = proxyGlobal
+module.exports = Object.create(globalObj)
