b83f73ad795c5aa3aa17a27c8a4d8cb8fdcd313f
WX-DongXing <dong.xing@outlook.com>
2025-06-03 10:38:24 +0800

    fix: fix the issue where the input value is not updated when changed

--- packages/webpack-plugin/lib/runtime/components/react/mpx-input.tsx
+++ packages/webpack-plugin/lib/runtime/components/react/mpx-input.tsx
@@ -221,7 +221,7 @@ const Input = forwardRef<HandlerRef<TextInput, FinalInputProps>, FinalInputProps
       tmpValue.current = parsed
       setInputValue(parsed)
     }
-  }, [value])
+  }, [value, inputValue])
 
   useEffect(() => {
     if (selectionStart > -1) {
