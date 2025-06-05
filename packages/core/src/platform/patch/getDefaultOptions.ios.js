e6bf119b6ac34becd4a84a9a93270fa1ed5d6646
xuegan <xuegan@didiglobal.com>
2025-06-05 21:18:43 +0800

    v2.10.6-beta.2

--- packages/core/src/platform/patch/getDefaultOptions.ios.js
+++ packages/core/src/platform/patch/getDefaultOptions.ios.js
@@ -461,8 +461,8 @@ function getLayoutData (headerHeight) {
   }
 }
 
-export function PageWrapperHOC (WrappedComponent) {
-  return function PageWrapperCom ({ navigation, route, pageConfig = {}, ...props }) {
+export function PageWrapperHOC (WrappedComponent, pageConfig = {}) {
+  return function PageWrapperCom ({ navigation, route, ...props }) {
     const rootRef = useRef(null)
     const keyboardAvoidRef = useRef(null)
     const intersectionObservers = useRef({})
@@ -691,11 +691,7 @@ export function getDefaultOptions ({ type, rawOptions = {}, currentInject }) {
   }
 
   if (type === 'page') {
-    return (props) =>
-      createElement(PageWrapperHOC(defaultOptions), {
-        pageConfig: currentInject.pageConfig,
-        ...props
-      })
+    return PageWrapperHOC(defaultOptions, currentInject.pageConfig)
   }
   return defaultOptions
 }
