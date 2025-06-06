5627dd5dfe7b9df19bb372f58f6817ceac1bb076
human <humanhuman@global.com>
2025-06-06 11:00:34 +0800

    去掉stackActions的引用

--- packages/core/src/platform/env/navigationHelper.ios.js
+++ packages/core/src/platform/env/navigationHelper.ios.js
@@ -1,5 +1,5 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { NavigationContainer, StackActions } from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
 import PortalHost from '@mpxjs/webpack-plugin/lib/runtime/components/react/dist/mpx-portal/portal-host'
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
@@ -7,7 +7,6 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler'
 export {
   createNativeStackNavigator,
   NavigationContainer,
-  StackActions,
   GestureHandlerRootView,
   PortalHost,
   SafeAreaProvider,
