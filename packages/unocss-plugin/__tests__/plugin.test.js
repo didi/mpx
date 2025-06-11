7c6ab7391df0cb3a4f9f2d0d1a0e7aec4466cc7d
chenwanyue <527202897@qq.com>
2025-06-11 11:41:59 +0800

    feat: 优化test demo&删除本地文件

--- packages/unocss-plugin/__tests__/plugin.test.js
+++ packages/unocss-plugin/__tests__/plugin.test.js
@@ -1,7 +1,7 @@
 import MpxUnocssPlugin from '../lib/index'
 import { describe, expect, test } from 'vitest'
 import { getRawSource } from '../lib/source.js'
-import {  e as cssEscape } from '@unocss/core'
+import { e as cssEscape } from '@unocss/core'
 import { mpEscape } from '../lib/transform.js'
 import presetMpx from '@mpxjs/unocss-base'
 // const { presetLegacyCompat } = require('@unocss/preset-legacy-compat')
@@ -21,8 +21,11 @@ describe('test plugin', async () => {
   }
   const uno = await plugin.createContext(mockCompilation, mode)
   const parseTemplate = plugin.getTemplateParser(uno)
-  test('test-template', async () => {
-    const source = getRawSource('<view class="translate-[-50%,-50%] text-12px" />')
+  async function testTemplate(content, generateOptions={
+    preflights: false,
+    safelist: false
+  }) {
+    const source = getRawSource(content)
     const classmap = {}
     const { newsource } = parseTemplate(source, (className) => {
       if (!className) {
@@ -31,18 +34,19 @@ describe('test plugin', async () => {
       classmap[className] = true
       return mpEscape(cssEscape(className), plugin.options.escapeMap)
     })
+    // 测试模板是否转义
     expect(newsource.source()).toMatchSnapshot()
     const classList = Object.keys(classmap)
+    // 测试类名是否正确识别
     expect(classList).toMatchSnapshot()
-
-    const generateOptions = {
-      preflights: false,
-      safelist: false,
-      minify: this.minify
-    }
     const list = new Set(classList)
     const unoFileContent = await plugin.generateStyle(uno, classList, generateOptions)
     console.log('unoFileContent==', list, unoFileContent)
+    // 测试css输出是否正确
     expect(unoFileContent).toMatchSnapshot()
+  }
+  test('test-template', async () => {
+    await testTemplate(`<view class="translate-[-50%,-50%] text-12px bg-#fff/10" />`)
+    await testTemplate(`<view wx:class="{{['translate-[-50%]',{'text-12px': false},'bg-#fff/10']}}" />`)
   })
 })
