b7e569a1b0d01ca6c3e151913a3f8503a1c7d870
human <humanhuman@global.com>
2025-06-18 10:22:13 +0800

    fix: 加上android判定

--- packages/core/src/platform/builtInMixins/styleHelperMixin.ios.js
+++ packages/core/src/platform/builtInMixins/styleHelperMixin.ios.js
@@ -3,7 +3,7 @@ import { Dimensions, StyleSheet } from 'react-native'
 
 let { width, height } = Dimensions.get('screen')
 // TODO 临时适配折叠屏场景适配
-const isLargeFoldableLike = (height / width < 1.5) && (width > 600)
+const isLargeFoldableLike = (__mpx_mode__ === 'android') && (height / width < 1.5) && (width > 600)
 if (isLargeFoldableLike) width = width / 2
 
 Dimensions.addEventListener('change', ({ screen }) => {
