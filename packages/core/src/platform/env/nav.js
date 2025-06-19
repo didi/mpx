d6be3b7d12d596487919a4a2356293690bcc68f9
shulandmimi <sshuang141@163.com>
2025-06-19 20:51:28 +0800

    feat: 支持通过 stack top config 控制回退按钮的展示

--- packages/core/src/platform/env/nav.js
+++ packages/core/src/platform/env/nav.js
@@ -96,14 +96,16 @@ export function innerNav ({ props, navigation }) {
   const safeAreaTop = useSafeAreaInsets()?.top || 0
   // 假设是栈导航，获取栈的长度
   const stackLength = navigation.getState()?.routes?.length
-  // 用于外部注册打开RN容器之前的栈长度
-  const beforeStackLength = Mpx.config?.rnConfig?.beforeStackLength || 0
+  const stackTopConfig = Mpx.config?.rnConfig?.stackTopConfig || {}
 
   // 回退按钮与图标
-  const backElement = stackLength + beforeStackLength > 1
+  const backElement = stackLength > 1 || stackTopConfig.show
   ? createElement(ReactNative.TouchableOpacity, {
     style: [styles.backButton],
-    onPress: () => { navigation.goBack() }
+    onPress: () => {
+      navigation.goBack()
+      stackTopConfig.listener?.()
+    }
   }, createElement(ReactNative.Image, {
     source: { uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADwAAABICAYAAACqT5alAAAA2UlEQVR4nO3bMQrCUBRE0Yla6AYEN2nnBrTL+izcitW3MRDkEUWSvPzJvfCqgMwhZbAppWhNbbIHzB1g9wATERFRVyvpkj1irlpJ5X326D7WHh1hbdFD2CLpLmmftm7kfsEe09aNHFiBrT+wAlt/YAW2/sAKbP2BFdj6Ayuwy+ufz6XPL893krZ//O6iu2n4LT8kndLWTRTo4EC7BDo40C6BDg60S6CDA+0S6OBAuwQ6uNWiD2nrJmoIfU7cNWkR2hbb1UfbY7uuWhGWiIg+a/iHuHmA3QPs3gu4JW9Gan+OJAAAAABJRU5ErkJggg==' },
     // 回退按钮的颜色与设置的title文案颜色一致
