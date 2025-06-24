aff5d1992f7615119155a873ae65499e9287eabd
lareinayanyu <lareinayanyu@didiglobal.com>
2025-06-24 20:39:49 +0800

    fix: lint

--- packages/webpack-plugin/lib/platform/template/wx/component-config/movable-view.js
+++ packages/webpack-plugin/lib/platform/template/wx/component-config/movable-view.js
@@ -2,9 +2,6 @@ const TAG_NAME = 'movable-view'
 
 module.exports = function ({ print }) {
   const aliEventLog = print({ platform: 'ali', tag: TAG_NAME, isError: false, type: 'event' })
-  const androidEventLog = print({ platform: 'android', tag: TAG_NAME, isError: false, type: 'event' })
-  const harmonyEventLog = print({ platform: 'harmony', tag: TAG_NAME, isError: false, type: 'event' })
-  const iosEventLog = print({ platform: 'ios', tag: TAG_NAME, isError: false, type: 'event' })
   const qaPropLog = print({ platform: 'qa', tag: TAG_NAME, isError: false })
   const androidPropLog = print({ platform: 'android', tag: TAG_NAME, isError: false })
   const harmonyPropLog = print({ platform: 'harmony', tag: TAG_NAME, isError: false })
