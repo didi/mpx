9b53171f0be20856bb84107014ecb33d9c5987b0
shulandmimi <sshuang141@163.com>
2025-06-30 15:54:44 +0800

    feat: 简化 stack top back 配置

--- packages/core/src/platform/env/nav.js
+++ packages/core/src/platform/env/nav.js
@@ -95,17 +95,22 @@ export function innerNav ({ pageConfig, navigation }) {
   const safeAreaTop = useSafeAreaInsets()?.top || 0
   // 假设是栈导航，获取栈的长度
   const stackLength = navigation.getState()?.routes?.length
-  const stackTopConfig = Mpx.config?.rnConfig?.stackTopConfig || {}
+  const onStackTopBack = Mpx.config?.rnConfig?.onStackTopBack
+  const isHandleStackTopBack = typeof onStackTopBack === 'function'
 
   // 回退按钮与图标
-  const backElement = stackLength > 1 || stackTopConfig.show
+  const backElement = stackLength > 1 || isHandleStackTopBack
   ? createElement(TouchableOpacity, {
     style: [styles.backButton],
     onPress: () => {
-      navigation.goBack()
-      if (stackLength <= 1 && stackTopConfig.show && typeof stackTopConfig.listener === 'function') {
-        stackTopConfig.listener?.()
+      if (stackLength <= 1) {
+        if (isHandleStackTopBack) {
+          onStackTopBack()
+        }
+        return
       }
+
+      navigation.goBack()
     }
   }, createElement(Image, {
     source: { uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADwAAABICAYAAACqT5alAAAA2UlEQVR4nO3bMQrCUBRE0Yla6AYEN2nnBrTL+izcitW3MRDkEUWSvPzJvfCqgMwhZbAppWhNbbIHzB1g9wATERFRVyvpkj1irlpJ5X326D7WHh1hbdFD2CLpLmmftm7kfsEe09aNHFiBrT+wAlt/YAW2/sAKbP2BFdj6Ayuwy+ufz6XPL893krZ//O6iu2n4LT8kndLWTRTo4EC7BDo40C6BDg60S6CDA+0S6OBAuwQ6uNWiD2nrJmoIfU7cNWkR2hbb1UfbY7uuWhGWiIg+a/iHuHmA3QPs3gu4JW9Gan+OJAAAAABJRU5ErkJggg==' },
