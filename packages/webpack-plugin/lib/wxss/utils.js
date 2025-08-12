754eada1335ae9e0e41479c3d935e5c932f9bb20
xuegan <xuegan@didiglobal.com>
2025-08-12 11:29:09 +0800

    fix: 修复输出RN时style block内单独@improt css文件时报错问题

--- packages/webpack-plugin/lib/wxss/utils.js
+++ packages/webpack-plugin/lib/wxss/utils.js
@@ -1052,10 +1052,8 @@ function getModuleCode (
         `@import url(${url});`
       )}${printedParam.length > 0 ? `, ${printedParam}` : ''}]);\n`
     } else {
-      // 符合css后缀名的文件经过mpx处理后会带上相应的后缀防止使用 WebPack 的默认解析规则，此时 require/import 相应路径时，导出的不是一段 css 代码了，事实上是一个文件路径。
       const printedParam = printParams(media, dedupe, supports, layer)
-      const otherParams = printedParam.length > 0 ? printedParam : ''
-      beforeCode += `___CSS_LOADER_EXPORT___.push([module.id, '@import "' + ${item.importName} + '";', ${JSON.stringify(otherParams)} ]);\n`
+      beforeCode += `___CSS_LOADER_EXPORT___.i(${item.importName}${printedParam.length > 0 ? `, ${printedParam}` : ''});\n`
     }
   }
 
