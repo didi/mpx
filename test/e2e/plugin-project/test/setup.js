b322a4bc6a9aa65493e469d04332d8846c946890
mackwang112 <mackwang112@gmail.com>
2025-09-01 17:09:09 +0800

    fix: unit

--- test/e2e/plugin-project/test/setup.js
+++ test/e2e/plugin-project/test/setup.js
@@ -1,3 +1,4 @@
 global.__mpx_mode__ = 'wx'
 // eslint-disable-next-line camelcase
 global.__mpx_dynamic_runtime__ = undefined
+global.mpxGlobal = require('@mpxjs/webpack-plugin/lib/runtime/mpxGlobal.js')
