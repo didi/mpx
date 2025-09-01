6ce735a0143cae1056b6ea1ffdffdc76697a33cd
mackwang112 <mackwang112@gmail.com>
2025-09-01 16:30:40 +0800

    fix: unit

--- test/e2e/plugin-project/copyWebpackPlugin.sh
+++ test/e2e/plugin-project/copyWebpackPlugin.sh
@@ -1,7 +1,9 @@
 echo "start copy webpackPlugin"
 
 rm -rf ./node_modules/@mpxjs/webpack-plugin/lib
+rm -rf ./node_modules/@mpxjs/core/src
 
 scp -r ../../../packages/webpack-plugin/lib/ ./node_modules/@mpxjs/webpack-plugin/lib/
+scp -r ../../../packages/core/src/ ./node_modules/@mpxjs/core/src/
 
 echo "end copy webpackPlugin"
