3186e4c851f2ffcc119f3f61932aebc6d874c7d2
yandadaFreedom <lareinayanyu@didiglobal.com>
2025-09-04 15:37:36 +0800

    chore: rebuild

--- packages/webpack-plugin/lib/runtime/components/react/mpx-view.tsx
+++ packages/webpack-plugin/lib/runtime/components/react/mpx-view.tsx
@@ -289,7 +289,7 @@ function backgroundSize (imageProps: ImageProps, preImageInfo: PreImageInfo, ima
       imageProps.resizeMode = 'stretch'
       if (type === 'linear' && (!layoutWidth || !layoutHeight)) {
         // ios 上 linear 组件只要重新触发渲染，在渲染过程中外层容器 width 或者 height 被设置为 0，通过设置 % 的方式会渲染不出来，即使后面再更新为正常宽高也渲染不出来
-        // 所以 hack 手动先将 linear 宽高也设置为 0，后面再更新为正确的数值或 %
+        // 所以 hack 手动先将 linear 宽高也设置为 0，后面再更新为正确的数值或 %。
         dimensions = {
           width: 0,
           height: 0
