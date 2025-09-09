1aa213e82ba0c7f28f373e18c889b208a55c166b
xuegan <xuegan@didiglobal.com>
2025-09-09 17:37:21 +0800

    fix: video progress callback error

--- packages/webpack-plugin/lib/runtime/components/web/mpx-video.vue
+++ packages/webpack-plugin/lib/runtime/components/web/mpx-video.vue
@@ -239,7 +239,10 @@
 
         this._player.on('progress', (e) => {
           const eNode = e.target
-          const buffered = (eNode?.buffered?.end(0)) / (eNode?.duration)
+          let buffered = 0
+          if (eNode?.buffered && eNode.buffered.length > 0) {
+            buffered = (eNode.buffered.end(0)) / (eNode?.duration)
+          }
           this.$emit('progress', inheritEvent('progress', e, { buffered: buffered * 100 }))
         })
 
