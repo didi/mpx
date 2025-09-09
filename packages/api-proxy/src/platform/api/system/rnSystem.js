5e5ad5a27c3c2b82a52f87b079735617a96c3146
wangcuijuan <wangcuijuan@didiglobal.com>
2025-09-09 10:43:50 +0800

    修正eslint

--- packages/api-proxy/src/platform/api/system/rnSystem.js
+++ packages/api-proxy/src/platform/api/system/rnSystem.js
@@ -11,11 +11,11 @@ const getWindowInfo = function () {
   let safeArea = {}
   const { top = 0, left = 0, bottom = 0, right = 0 } = insets
   const layout = navigation.layout || {}
+  const screenHeight = __mpx_mode__ === 'ios' ? dimensionsScreen.height : dimensionsScreen.height - layout.bottomVirtualHeight // 解决安卓开启屏幕内三建导航安卓把安全区计算进去后产生的影响
+  const screenWidth = __mpx_mode__ === 'ios' ? dimensionsScreen.width : dimensionsScreen.width - right
   const layoutHeight = layout.height || 0
   const layoutWidth = layout.width || 0
   const windowHeight = layoutHeight || screenHeight
-  const screenHeight = __mpx_mode__ === 'ios' ? dimensionsScreen.height : dimensionsScreen.height - layout.bottomVirtualHeight // 解决安卓开启屏幕内三建导航安卓把安全区计算进去后产生的影响
-  const screenWidth = __mpx_mode__ === 'ios' ? dimensionsScreen.width : dimensionsScreen.width - right
   try {
     safeArea = {
       left,
