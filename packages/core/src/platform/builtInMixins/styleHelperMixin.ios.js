cb07f5c04dfc7db87b1d5b166c1ef152cce21656
zhouchaozczc <undefined@didiglobal.com>
2025-09-28 14:56:32 +0800

    fix: 修复rn wx:show 问题
    只添加padding、margin，无法覆盖 padding-top 等情况

--- packages/core/src/platform/builtInMixins/styleHelperMixin.ios.js
+++ packages/core/src/platform/builtInMixins/styleHelperMixin.ios.js
@@ -237,7 +237,15 @@ export default function styleHelperMixin () {
             height: 0,
             width: 0,
             padding: 0,
+            paddingTop: 0,
+            paddingRight: 0,
+            paddingBottom: 0,
+            paddingLeft: 0,
             margin: 0,
+            marginTop: 0,
+            marginRight: 0,
+            marginBottom: 0,
+            marginLeft: 0,
             overflow: 'hidden'
           })
         }
