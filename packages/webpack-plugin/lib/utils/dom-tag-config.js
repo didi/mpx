37941816155c1c976d69a790c0c9545f1fdb2923
wangxiaokou <w-cuijuan@163.com>
2025-10-20 19:33:42 +0800

    改掉web下的tab-bar

--- packages/webpack-plugin/lib/utils/dom-tag-config.js
+++ packages/webpack-plugin/lib/utils/dom-tag-config.js
@@ -77,7 +77,7 @@ const isBuildInWebTag = makeMap(
   'mpx-swiper-item,mpx-video,mpx-button,mpx-progress,' +
   'mpx-swiper,mpx-view,mpx-checkbox-group,mpx-movable-area,mpx-radio-group,' +
   'mpx-switch,mpx-web-view,mpx-checkbox,mpx-movable-view,mpx-radio,' +
-  'mpx-form,mpx-navigator,mpx-rich-text,mpx-tab-bar,' +
+  'mpx-form,mpx-navigator,mpx-rich-text,' +
   'mpx-icon,mpx-picker-view-column,mpx-scroll-view,mpx-text'
 )
 
