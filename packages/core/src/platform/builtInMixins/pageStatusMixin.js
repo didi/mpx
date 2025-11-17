4630b43a3339ced681dd19e4d0d871b9b8ff30d0
wangcuijuan <wangcuijuan@didiglobal.com>
2025-11-17 19:21:48 +0800

    修复commit提到的问题

--- packages/core/src/platform/builtInMixins/pageStatusMixin.js
+++ packages/core/src/platform/builtInMixins/pageStatusMixin.js
@@ -14,15 +14,16 @@ export default function pageStatusMixin (mixinType) {
         this.__mpxProxy.callHook(ONRESIZE, [e])
       },
       onLoad (query) {
+        const loadParams = Object.assign({}, query)
         if (__mpx_mode__ === 'wx') {
           // 此处单独处理微信与其他端保持一致，传入onload的参数都是经过decodeURIComponent处理过的
           if (isObject(query)) {
             for (const key in query) {
-              query[key] = decodeURIComponent(query[key])
+              loadParams[key] = decodeURIComponent(query[key])
             }
           }
         }
-        this.__mpxProxy.callHook(ONLOAD, [query])
+        this.__mpxProxy.callHook(ONLOAD, [loadParams])
       }
     }
     if (__mpx_mode__ === 'ali') {
