b6a753ccfc58ed93460204869b73ad699f5008b8
luyongfang <luyongfang@didichuxing.com>
2025-11-17 11:11:38 +0800

    修复安卓部分机型onFinalize拿到的值不对

--- packages/webpack-plugin/lib/runtime/components/react/mpx-swiper.tsx
+++ packages/webpack-plugin/lib/runtime/components/react/mpx-swiper.tsx
@@ -791,6 +791,10 @@ const SwiperWrapper = forwardRef<HandlerRef<View, SwiperProps>, SwiperProps>((pr
         }
         preAbsolutePos.value = e[strAbso]
       })
+      .onEnd((e: GestureStateChangeEvent<PanGestureHandlerEventPayload>) => {
+        // 修复某些安卓机型小米 onFinalize拿到的absolute值不正确的问题, onUpdate并不是最终的值
+        preAbsolutePos.value = e[strAbso]
+      })
       .onFinalize((e: GestureStateChangeEvent<PanGestureHandlerEventPayload>) => {
         'worklet'
         if (touchfinish.value) return
