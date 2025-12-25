b6b789c261a8081ad1b0c5b9e8f2731946c21c38
wenwen <huajingwen@didiglobal.com>
2025-12-25 11:38:19 +0800

    platform wx del animation

--- packages/webpack-plugin/lib/platform/style/wx/index.js
+++ packages/webpack-plugin/lib/platform/style/wx/index.js
@@ -3,7 +3,7 @@ const { parseValues } = require('../../../utils/string')
 
 module.exports = function getSpec ({ warn, error }) {
   // React Native 双端都不支持的 CSS property
-  const unsupportedPropExp = /^(white-space|text-overflow|animation|font-variant-caps|font-variant-numeric|font-variant-east-asian|font-variant-alternates|font-variant-ligatures|background-position|caret-color)$/
+  const unsupportedPropExp = /^(white-space|text-overflow|font-variant-caps|font-variant-numeric|font-variant-east-asian|font-variant-alternates|font-variant-ligatures|background-position|caret-color)$/
   const unsupportedPropMode = {
     // React Native ios 不支持的 CSS property
     ios: /^(vertical-align)$/,
