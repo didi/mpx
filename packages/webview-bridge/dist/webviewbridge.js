d3b856b73a5bd247bf283cd4a0a6e6e7d64f4d17
wangxiaokou <w-cuijuan@163.com>
2026-07-14 14:42:49 +0800

    补充-1版本的逻辑

--- packages/webview-bridge/dist/webviewbridge.js
+++ packages/webview-bridge/dist/webviewbridge.js
@@ -133,7 +133,7 @@
     env = systemUA.indexOf('QQ') > -1 ? 'qq' : 'wx';
   } else if (systemUA.indexOf('swan/') > -1) {
     env = 'swan';
-  } else if (systemUA.indexOf('toutiao') > -1) {
+  } else if (systemUA.toLocaleLowerCase().indexOf('toutiao') > -1) {
     env = 'tt';
   } else {
     env = 'web';
