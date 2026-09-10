34ebc050d4406e2076ccd2fad2258b4ead9ead17
xuegan <xuegan@didiglobal.com>
2026-09-10 20:46:45 +0800

    test(core): simplify RN initial params tests

--- packages/core/__tests__/common/createApp.ios.spec.js
+++ packages/core/__tests__/common/createApp.ios.spec.js
@@ -1,48 +1,17 @@
-import createApp from '../../src/platform/createApp.ios'
-import transferOptions from '../../src/core/transferOptions'
-import Mpx from '../../src/index'
-import { error } from '@mpxjs/utils'
+global.__mpx_mode__ = 'ios'
 
-jest.mock('../../src/core/transferOptions', () => ({
-  __esModule: true,
-  default: jest.fn()
-}))
+const createApp = require('../../src/platform/createApp.ios').default
+const transferOptions = require('../../src/core/transferOptions')
+const Mpx = require('../../src/index')
+const { error } = require('@mpxjs/utils')
 
-jest.mock('../../src/platform/patch/builtInKeysMap', () => ({
-  __esModule: true,
-  default: {}
-}))
+jest.mock('../../src/core/transferOptions', () => jest.fn())
 
-jest.mock('@mpxjs/utils', () => ({
-  makeMap: (keys) => keys.reduce((map, key) => {
-    map[key] = true
-    return map
-  }, {}),
-  spreadProp: (options) => options,
-  getFocusedNavigation: jest.fn(),
-  hasOwn: (target, key) => Object.prototype.hasOwnProperty.call(target, key),
-  callWithErrorHandling: jest.fn(),
+jest.mock('@mpxjs/utils', () => Object.assign({}, jest.requireActual('@mpxjs/utils'), {
   error: jest.fn()
-}), { virtual: true })
-
-jest.mock('../../src/convertor/mergeLifecycle', () => ({
-  mergeLifecycle: () => ({
-    app: ['onLaunch', 'onShow', 'onHide', 'onError', 'onUnhandledRejection']
-  })
 }))
 
-jest.mock('../../src/platform/patch/lifecycle/index', () => ({
-  LIFECYCLE: {}
-}))
-
-jest.mock('../../src/index', () => {
-  const Mpx = function () {}
-  Mpx.config = { rnConfig: {} }
-  return {
-    __esModule: true,
-    default: Mpx
-  }
-})
+jest.mock('../../src/index', () => ({ config: { rnConfig: {} }, prototype: {} }))
 
 jest.mock('../../src/observer/reactive', () => ({
   reactive: (value) => value
@@ -62,11 +31,7 @@ jest.mock('react', () => ({
   useEffect: jest.fn()
 }), { virtual: true })
 
-jest.mock('react-native', () => ({
-  AppState: {
-    addEventListener: jest.fn()
-  }
-}), { virtual: true })
+jest.mock('react-native', () => ({}), { virtual: true })
 
 jest.mock('../../src/platform/export/inject', () => ({
   initAppProvides: jest.fn()
@@ -85,78 +50,36 @@ jest.mock('../../src/platform/env/navigationHelper', () => ({
 jest.mock('@mpxjs/webpack-plugin/lib/runtime/components/react/dist/mpx-nav', () => 'MpxNav', { virtual: true })
 
 describe('RN createApp initial params', () => {
-  const globalKeys = [
-    '__mpxAppCbs',
-    '__mpxAppHotLaunched',
-    '__mpxAppLaunched',
-    '__mpxAppOnLaunch',
-    '__mpxEnterOptions',
-    '__mpxInitialRouteKey',
-    '__mpxInitialRunParams',
-    '__mpxLaunchOptions',
-    '__mpxOptionsMap',
-    '__mpxPageConfig',
-    '__mpxPageConfigsMap',
-    '__mpxPagesMap',
-    '__navigationHelper',
-    'getApp',
-    'getCurrentPages',
-    'setAppHide',
-    'setAppShow'
-  ]
+  const onLaunch = jest.fn()
 
   beforeEach(() => {
-    global.__mpxAppCbs = {
-      show: [],
-      hide: [],
-      error: [],
-      rejection: []
-    }
-    global.__mpxPageConfig = {}
-    global.__mpxPageConfigsMap = {
-      'pages/index': {}
-    }
-    global.__navigationHelper = {}
-    Mpx.config.rnConfig = {}
-  })
-
-  afterEach(() => {
     jest.clearAllMocks()
-    globalKeys.forEach((key) => {
-      delete global[key]
-    })
-  })
-
-  it('uses initialState for a non-first Screen instead of Navigator and Screen defaults', () => {
-    const initialParams = { a: 1 }
-    const onLaunch = jest.fn()
-    const onStateChange = jest.fn()
-    const HomePage = () => null
-    HomePage.displayName = 'HomePage'
-    const IndexPage = () => null
-    IndexPage.displayName = 'IndexPage'
+    global.__mpxOptionsMap = {}
+    global.__mpxPageConfig = {}
+    global.__mpxPageConfigsMap = {}
+    Mpx.config.rnConfig = {}
     transferOptions.mockReturnValue({
       rawOptions: { onLaunch },
       currentInject: {
         moduleId: 'app',
         firstPage: 'pages/home',
         pagesMap: {
-          'pages/home': HomePage,
-          'pages/index': IndexPage
+          'pages/home': () => null,
+          'pages/index': () => null
         }
       }
     })
-    Mpx.config.rnConfig = {
-      parseAppProps: () => ({
-        initialRouteName: 'pages/index',
-        initialParams
-      }),
-      onStateChange
-    }
+  })
 
+  function renderApp (initialRouteName, initialParams) {
+    Mpx.config.rnConfig.parseAppProps = () => ({ initialRouteName, initialParams })
     createApp({})
-    const tree = global.__mpxOptionsMap.app({})
-    const navigationContainer = tree.props.children[0]
+    return global.__mpxOptionsMap.app({}).props.children[0]
+  }
+
+  it('uses initialState for a non-first Screen instead of Navigator and Screen defaults', () => {
+    const initialParams = { a: 1 }
+    const navigationContainer = renderApp('pages/index', initialParams)
     const stackNavigator = navigationContainer.props.children[0]
     const indexScreen = stackNavigator.props.children[1]
 
@@ -166,31 +89,12 @@ describe('RN createApp initial params', () => {
         params: initialParams
       }]
     })
-    expect(stackNavigator.props.children.map(screen => screen.props.name)).toEqual([
-      'pages/home',
-      'pages/index'
-    ])
     expect(stackNavigator.props).not.toHaveProperty('initialRouteName')
     expect(indexScreen.props).not.toHaveProperty('initialParams')
-    expect(global.__mpxInitialRouteKey).toBeUndefined()
-    expect(global.__mpxInitialRunParams).toBeUndefined()
 
-    const resetRouteParams = Object.assign({}, indexScreen.props.initialParams, { b: 2 })
-    expect(resetRouteParams).toEqual({ b: 2 })
-
-    const state = {
-      index: 0,
-      routes: [{
-        key: 'pages/index-1',
-        name: 'pages/index',
-        params: initialParams
-      }]
-    }
     global.__mpxAppOnLaunch({
-      getState: () => state
+      getState: () => Object.assign({ index: 0 }, navigationContainer.props.initialState)
     })
-
-    expect(onStateChange).toHaveBeenCalledWith(state)
     expect(onLaunch).toHaveBeenCalledWith(expect.objectContaining({
       path: 'pages/index',
       query: initialParams,
@@ -199,39 +103,15 @@ describe('RN createApp initial params', () => {
   })
 
   it('reports and falls back when the initial route is not registered', () => {
-    const initialParams = { fromMissing: true }
-    const IndexPage = () => null
-    IndexPage.displayName = 'IndexPage'
-    transferOptions.mockReturnValue({
-      rawOptions: {},
-      currentInject: {
-        moduleId: 'app',
-        firstPage: 'pages/index',
-        pagesMap: {
-          'pages/index': IndexPage
-        }
-      }
-    })
-    Mpx.config.rnConfig = {
-      parseAppProps: () => ({
-        initialRouteName: 'pages/missing',
-        initialParams
-      })
-    }
-
-    createApp({})
-    const tree = global.__mpxOptionsMap.app({})
-    const navigationContainer = tree.props.children[0]
-    const stackNavigator = navigationContainer.props.children[0]
+    const navigationContainer = renderApp('pages/missing', { fromMissing: true })
 
     expect(error).toHaveBeenCalledTimes(1)
-    expect(error).toHaveBeenCalledWith('The initial page [pages/missing] is not registered in the application. Mpx will fall back to the first page [pages/index].')
+    expect(error).toHaveBeenCalledWith('The initial page [pages/missing] is not registered in the application. Mpx will fall back to the first page [pages/home].')
     expect(navigationContainer.props.initialState).toEqual({
       routes: [{
-        name: 'pages/index',
+        name: 'pages/home',
         params: {}
       }]
     })
-    expect(stackNavigator.props).not.toHaveProperty('initialRouteName')
   })
 })
